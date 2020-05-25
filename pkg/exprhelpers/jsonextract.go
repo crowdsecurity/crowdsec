aad2e1421eed65c3be2acc3d7a5f861c6b673894
Thibault bui Koechlin <thibault@crowdsec.net>
2020-05-25 11:35:32 +0200

    fix printf

--- pkg/exprhelpers/jsonextract.go
+++ pkg/exprhelpers/jsonextract.go
@@ -32,6 +32,6 @@ func JsonExtract(jsblob string, target string) string {
 	}
 	fullpath := strings.Split(target, ".")
 
-	log.Printf("path -> %+v", fullpath)
+	log.Debugf("extract path %+v", fullpath)
 	return JsonExtractLib(jsblob, fullpath...)
 }
