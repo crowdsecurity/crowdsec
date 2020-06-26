77af2afb83c9f3e0e74115e86c2681362e8cc85a
AlteredCoder <AlteredCoder>
2020-06-26 17:13:17 +0200

    update

--- scripts/post_install_deb.sh
+++ scripts/post_install_deb.sh
@@ -1,5 +1,6 @@
 systemctl daemon-reload
 cscli update
+/usr/local/bin/cswizard -i
 cscli install collection crowdsecurity/linux
 cscli install scenario crowdsecurity/ssh-bf
 systemctl start crowdsec
