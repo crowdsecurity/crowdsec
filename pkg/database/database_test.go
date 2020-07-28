bec5dd810e2658badc872401679fda32bcdad88f
Thibault bui Koechlin <thibault@crowdsec.net>
2020-07-28 15:43:11 +0200

    up

--- pkg/database/database_test.go
+++ pkg/database/database_test.go
@@ -159,39 +159,9 @@ var _ = ginkgo.Describe("TestWrites", func() {
 		})
 	})
 
-	// It("test something", func(){
-	//     ...
-	// })
 })
 
 func TestSql(t *testing.T) {
 	gomega.RegisterFailHandler(ginkgo.Fail)
 	ginkgo.RunSpecs(t, "TestWrites")
 }
-
-// // a successful case
-// func TestInsertDatabase(t *testing.T) {
-
-// 	db, mock, err := sqlmock.New()
-// 	if err != nil {
-// 		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
-// 	}
-// 	defer db.Close()
-
-// 	fakeCtx := Context{Db: db}
-
-// 	mock.ExpectBegin()
-// 	mock.ExpectExec("UPDATE products").WillReturnResult(sqlmock.NewResult(1, 1))
-// 	mock.ExpectExec("INSERT INTO product_viewers").WithArgs(2, 3).WillReturnResult(sqlmock.NewResult(1, 1))
-// 	mock.ExpectCommit()
-
-// 	// now we execute our method
-// 	if err = recordStats(db, 2, 3); err != nil {
-// 		t.Errorf("error was not expected while updating stats: %s", err)
-// 	}
-
-// 	// we make sure that all expectations were met
-// 	if err := mock.ExpectationsWereMet(); err != nil {
-// 		t.Errorf("there were unfulfilled expectations: %s", err)
-// 	}
-// }
