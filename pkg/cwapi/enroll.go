d23512e9c6c25fdd447ae54906ceacec8dec9d97
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2020-07-30 15:30:02 +0200

    improve logging (#164)
    Co-authored-by: AlteredCoder <AlteredCoder>

--- pkg/cwapi/enroll.go
+++ pkg/cwapi/enroll.go
@@ -9,14 +9,15 @@ import (
 func (ctx *ApiCtx) Enroll(userID string) error {
 	toPush := map[string]string{"user_id": userID}
 	jsonResp := &ApiResp{}
+	errResp := &ApiResp{}
 
-	resp, err := ctx.Http.New().Post(ctx.EnrollPath).BodyJSON(&toPush).ReceiveSuccess(jsonResp)
+	resp, err := ctx.Http.New().Post(ctx.EnrollPath).BodyJSON(&toPush).Receive(jsonResp, errResp)
 	if err != nil {
 		return fmt.Errorf("api enroll: HTTP request creation failed: %s", err)
 	}
 
 	if resp.StatusCode != 200 {
-		return fmt.Errorf("api enroll: user '%s' return bad HTTP code (%d)", userID, resp.StatusCode)
+		return fmt.Errorf("api enroll: user '%s' return bad HTTP code (%d): %s", userID, resp.StatusCode, errResp.Message)
 	}
 	if jsonResp.Message == "" || jsonResp.Message != "OK" || jsonResp.StatusCode != 200 {
 		return fmt.Errorf("api user enroll failed")
