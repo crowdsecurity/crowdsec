4126c225c36f678a97dc7625323c7232a30a43e4
Thibault bui Koechlin <thibault@crowdsec.net>
2020-09-09 11:25:52 +0200

    trash the old structures to force convergence to the generated models

--- pkg/types/event.go
+++ pkg/types/event.go
@@ -1,10 +1,10 @@
 package types
 
 import (
-	"net"
 	"time"
 
 	"github.com/antonmedv/expr/vm"
+	"github.com/crowdsecurity/crowdsec/pkg/models"
 )
 
 const (
@@ -28,37 +28,38 @@ type Event struct {
 	/* output of enrichment */
 	Enriched map[string]string `json:"Enriched,omitempty" yaml:"Enriched,omitempty"`
 	/* Overflow */
-	Overflow      Alert     `yaml:"Alert,omitempty"`
-	Time          time.Time `json:"Time,omitempty"` //parsed time `json:"-"` ``
-	StrTime       string    `yaml:"StrTime,omitempty"`
-	MarshaledTime string    `yaml:"MarshaledTime,omitempty"`
-	Process       bool      `yaml:"Process,omitempty"` //can be set to false to avoid processing line
+	Overflow      RuntimeAlert `yaml:"Alert,omitempty"`
+	Time          time.Time    `json:"Time,omitempty"` //parsed time `json:"-"` ``
+	StrTime       string       `yaml:"StrTime,omitempty"`
+	MarshaledTime string       `yaml:"MarshaledTime,omitempty"`
+	Process       bool         `yaml:"Process,omitempty"` //can be set to false to avoid processing line
 	/* Meta is the only part that will make it to the API - it should be normalized */
 	Meta map[string]string `json:"Meta,omitempty" yaml:"Meta,omitempty"`
 }
 
-type Decision struct {
-	Origin string /*cscli,crowdsec,...*/
-	Type   string /*ban,slow,captcha,whatever*/
-	Scope  string /*ip,range,username,toto,lol*/
-	Target string /*in case we need extra info for the connector ?*/
-	/*only relevant if the scope is an IP or a range*/
-	StartIP uint32
-	EndIP   uint32
+// type Decision struct {
+// 	//Origin string /*cscli,crowdsec,...*/
+// 	//Type   string /*ban,slow,captcha,whatever*/
+// 	//Scope  string /*ip,range,username,toto,lol*/
+// 	//Target string /*in case we need extra info for the connector ?*/
+// 	/*only relevant if the scope is an IP or a range*/
+// 	//StartIP uint32
+// 	//EndIP   uint32
 
-	Duration time.Duration /*expiration of ban*/
+// 	//Duration time.Duration /*expiration of ban*/
 
-	Message  string /*long human reason of the ban 'ban AS1234' */
-	Scenario string /*the type of scenario that led to ban*/
-}
+// 	//Message  string /*long human reason of the ban 'ban AS1234' */
+// 	//Scenario string /*the type of scenario that led to ban*/
+// }
 
 //EventSequence is used to represent the summarized version of events that lead to overflow
-type EventSequence struct {
-	Time time.Time
-	//Serialized string            //the serialized dict evt.Meta
-	Meta map[string]string //the evt.Meta
-}
+// type EventSequence struct {
+// 	Time time.Time
+// 	//Serialized string            //the serialized dict evt.Meta
+// 	Meta map[string]string //the evt.Meta
+// }
 
+//Move in leakybuckets
 const (
 	Undefined = ""
 	Ip        = "Ip"
@@ -68,11 +69,12 @@ const (
 
 // golang fuckin lacks generics ...
 // tags are used only for unit tests
-type ScopeData struct {
-	Scope string `yaml:"scope"`
-	Value string `yaml:"value"`
-}
+// type ScopeData struct {
+// 	Scope string `yaml:"scope"`
+// 	Value string `yaml:"value"`
+// }
 
+//Move in leakybuckets
 type ScopeType struct {
 	Scope         string `yaml:"scope_type"`
 	Filter        string `yaml:"filter"`
@@ -80,41 +82,47 @@ type ScopeType struct {
 }
 
 //Source is the generic representation of a source ip implicated in events / overflows. It contains both information extracted directly from logs and enrichment
-type Source struct {
-	//tags here are used for unit tests
-	ScopeData                    ScopeData `yaml:"scope_data"`
-	Ip                           net.IP    `yaml:"ip"` //shorthand for scope=ip&value=<X>
-	Range                        net.IPNet `yaml:"range"`
-	AutonomousSystemNumber       string    `yaml:"as_number"`
-	AutonomousSystemOrganization string    `yaml:"as_org"`
-	Country                      string    `yaml:"country"`
-	Latitude                     float64   `yaml:"latitude"`
-	Longitude                    float64   `yaml:"longitude"`
-}
+// type Source struct {
+// 	//tags here are used for unit tests
+// 	/*merge?*/ScopeData                    ScopeData `yaml:"scope_data"`
+// 	Ip                           net.IP    `yaml:"ip"` //shorthand for scope=ip&value=<X>
+// 	Range                        net.IPNet `yaml:"range"`
+// 	AutonomousSystemNumber       string    `yaml:"as_number"`
+// 	AutonomousSystemOrganization string    `yaml:"as_org"`
+// 	Country                      string    `yaml:"country"`
+// 	Latitude                     float64   `yaml:"latitude"`
+// 	Longitude                    float64   `yaml:"longitude"`
+// }
 
-type Alert struct {
-	/*bucket mgmt*/
-	Mapkey    string //related to bucket mgmt, it's the partition identifier of a bucket
-	Bucket_id string //The 'runtime' bucket-name (mostly for debug), ie. `sunny-flower`
-
-	/*actual overflow*/
-	Scenario    string    `yaml:"scenario"` //The unique name of the scenario, ie. ssh_bruteforce_multi-user
-	Message     string    //Human-friendly label (to be displayed)
-	EventsCount int       `yaml:"events_count"` //Number of events between first occurence and ban
-	StartAt     time.Time //first event (usually bucket creation time)
-	StopAt      time.Time //last event (usually bucket overflow time)
-	Capacity    int
-	LeakSpeed   time.Duration
+type RuntimeAlert struct {
+	Mapkey      string
+	BucketId    string
 	Whitelisted bool
-	Simulated   bool
-	//Reprocess, when true, will make the overflow being processed again as a fresh log would
-	Reprocess bool
-	/*Events that constituted the actual overflow*/
-	Events []EventSequence //When adapted, a unique list of string representing the individual events that lead to the overflow
-	/*Associated decisions*/
-	Decisions []Decision
-	/*all the source implicated in said overflow*/
-	Sources map[string]Source `yaml:"sources"`
-	/*defined by user/bucket configuration*/
-	Labels map[string]string
+	Reprocess   bool
+	Sources     map[string]models.Source
+	APIAlerts   []models.Alert
 }
+
+// type Alert struct {
+// 	/*bucket mgmt*/
+// 	// Mapkey    string //related to bucket mgmt, it's the partition identifier of a bucket
+// 	// Bucket_id string //The 'runtime' bucket-name (mostly for debug), ie. `sunny-flower`
+
+// 	/*actual overflow*/
+// 	// Scenario    string    `yaml:"scenario"` //The unique name of the scenario, ie. ssh_bruteforce_multi-user
+// 	//Message     string    //Human-friendly label (to be displayed)
+// 	//EventsCount int       `yaml:"events_count"` //Number of events between first occurence and ban
+// 	//StartAt     time.Time //first event (usually bucket creation time)
+// 	//StopAt      time.Time //last event (usually bucket overflow time)
+// 	//Capacity    int
+// 	//LeakSpeed   time.Duration
+// 	//Simulated bool
+// 	//Reprocess, when true, will make the overflow being processed again as a fresh log would
+// 	/*Events that constituted the actual overflow*/
+// 	//Events []EventSequence //When adapted, a unique list of string representing the individual events that lead to the overflow
+// 	/*Associated decisions*/
+// 	//Decisions []Decision
+// 	/*all the source implicated in said overflow*/
+// 	/*defined by user/bucket configuration*/
+// 	Labels map[string]string
+// }
