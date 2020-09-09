457f658e5b312d3facfe9f0be3480998c2417ba1
Thibault bui Koechlin <thibault@crowdsec.net>
2020-09-09 10:36:14 +0200

    cleanup old tachymeter stuff

--- cmd/crowdsec/output.go
+++ cmd/crowdsec/output.go
@@ -5,8 +5,6 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	"time"
-
 	leaky "github.com/crowdsecurity/crowdsec/pkg/leakybucket"
 	"github.com/crowdsecurity/crowdsec/pkg/parser"
 	"github.com/crowdsecurity/crowdsec/pkg/types"
@@ -14,10 +12,6 @@ import (
 
 func runOutput(input chan types.Event, overflow chan types.Event, holders []leaky.BucketFactory, buckets *leaky.Buckets,
 	poctx parser.UnixParserCtx, ponodes []parser.Node) error {
-	var (
-		//action string
-		start time.Time
-	)
 
 LOOP:
 	for {
@@ -29,9 +23,6 @@ LOOP:
 			//if global simulation -> everything is simulation unless told otherwise
 			if cConfig.SimulationCfg != nil && cConfig.SimulationCfg.Simulation {
 				event.Overflow.Simulated = true
-			}
-			if cConfig.Profiling {
-				start = time.Now()
 			}
 
 			if event.Overflow.Reprocess {
@@ -54,14 +45,10 @@ LOOP:
 			}
 
 			if event.Overflow.Scenario == "" && event.Overflow.Mapkey != "" {
-				//log.Infof("Deleting expired entry %s", event.Overflow.MapKey)
 				buckets.Bucket_map.Delete(event.Overflow.Mapkey)
 			} else {
 				log.Warningf("overflow : %+v", event)
 			}
-		}
-		if cConfig.Profiling {
-			outputStat.AddTime(time.Since(start))
 		}
 	}
 	return nil
