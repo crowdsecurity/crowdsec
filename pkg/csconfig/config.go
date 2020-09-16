43cac1651f7f04e825f10d68fba8e72e0ae0a129
Thibault bui Koechlin <thibault@crowdsec.net>
2020-09-16 15:15:35 +0200

    up db cfg

--- pkg/csconfig/config.go
+++ pkg/csconfig/config.go
@@ -30,7 +30,9 @@ type DatabaseCfg struct {
 	User     string `yaml:"user"`
 	Password string `yaml:"password"`
 	DbName   string `yaml:"db_name"`
-	Uri      string `yaml:"uri"`
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	DbPath   string `yaml:"db_path"`
 	Type     string `yaml:"type"`
 }
 
