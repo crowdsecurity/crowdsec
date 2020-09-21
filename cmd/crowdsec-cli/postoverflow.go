934e024b5b0c1c8042bdba6c38750ce56bb64479
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2020-09-21 17:30:06 +0200

    Refactor cscli hub (#239)

---
+++ cmd/crowdsec-cli/postoverflow.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/crowdsecurity/crowdsec/pkg/cwhub"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/spf13/cobra"
+)
+
+func NewPostOverflowCmd() *cobra.Command {
+	var cmdPostOverflow = &cobra.Command{
+		Use:   "postoverflow [action] [config]",
+		Short: "Install/Remove/Upgrade/Inspect postoverflow(s) from hub",
+		Long: `
+		Install/Remove/Upgrade/Inspect postoverflow(s) from the CrowdSec Hub.
+
+In order to download latest versions of configuration, 
+you should [update cscli](./cscli_update.md).
+
+[action] must be install/upgrade or remove.
+
+[config_name] must be a valid config name from [Crowdsec Hub](https://hub.crowdsec.net).
+
+As a reminder, postoverflows are parsing configuration that will occur after the overflow (before a decision is applied)
+`,
+		Example: `cscli postoverflow [action] [config_name]`,
+		Args:    cobra.MinimumNArgs(1),
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if csConfig.Cscli == nil {
+				return fmt.Errorf("you must configure cli before interacting with hub")
+			}
+
+			if err := setHubBranch(); err != nil {
+				return fmt.Errorf("error while setting hub branch: %s", err)
+			}
+			return nil
+		},
+		PersistentPostRun: func(cmd *cobra.Command, args []string) {
+			if cmd.Name() == "inspect" {
+				return
+			}
+			log.Infof("Run 'systemctl reload crowdsec' for the new configuration to be effective.")
+		},
+	}
+
+	var cmdPostOverflowInstall = &cobra.Command{
+		Use:     "postoverflow [config]",
+		Short:   "Install given postoverflow",
+		Long:    `Fetch and install given postoverflow from hub`,
+		Example: `cscli postoverflow install crowdsec/xxx`,
+		Args:    cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cwhub.GetHubIdx(csConfig.Cscli); err != nil {
+				log.Fatalf("failed to get Hub index : %v", err)
+			}
+			for _, name := range args {
+				InstallItem(name, cwhub.PARSERS_OVFLW)
+			}
+		},
+	}
+	cmdPostOverflowInstall.PersistentFlags().BoolVarP(&downloadOnly, "download-only", "d", false, "Only download packages, don't enable")
+	cmdPostOverflowInstall.PersistentFlags().BoolVar(&forceInstall, "force", false, "Force install : Overwrite tainted and outdated files")
+	cmdPostOverflow.AddCommand(cmdPostOverflowInstall)
+
+	var cmdPostOverflowRemove = &cobra.Command{
+		Use:     "remove [config]",
+		Short:   "Remove given postoverflow",
+		Long:    `remove given postoverflow`,
+		Example: `cscli postoverflow remove crowdsec/xxx`,
+		Args:    cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cwhub.GetHubIdx(csConfig.Cscli); err != nil {
+				log.Fatalf("Failed to get Hub index : %v", err)
+			}
+
+			if removeAll {
+				RemoveMany(cwhub.PARSERS_OVFLW, "")
+			} else {
+				for _, name := range args {
+					RemoveMany(cwhub.PARSERS_OVFLW, name)
+				}
+			}
+		},
+	}
+	cmdPostOverflowRemove.PersistentFlags().BoolVar(&purgeRemove, "purge", false, "Delete source file in ~/.cscli/hub/ too")
+	cmdPostOverflowRemove.PersistentFlags().BoolVar(&removeAll, "all", false, "Delete all the files in selected scope")
+	cmdPostOverflow.AddCommand(cmdPostOverflowRemove)
+
+	var cmdPostOverflowUpgrade = &cobra.Command{
+		Use:     "upgrade [config]",
+		Short:   "Upgrade given postoverflow",
+		Long:    `Fetch and Upgrade given postoverflow from hub`,
+		Example: `cscli postoverflow upgrade crowdsec/xxx`,
+		Args:    cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cwhub.GetHubIdx(csConfig.Cscli); err != nil {
+				log.Fatalf("Failed to get Hub index : %v", err)
+			}
+			if upgradeAll {
+				UpgradeConfig(cwhub.PARSERS_OVFLW, "")
+			} else {
+				for _, name := range args {
+					UpgradeConfig(cwhub.PARSERS_OVFLW, name)
+				}
+			}
+		},
+	}
+	cmdPostOverflowUpgrade.PersistentFlags().BoolVarP(&upgradeAll, "download-only", "d", false, "Only download packages, don't enable")
+	cmdPostOverflowUpgrade.PersistentFlags().BoolVar(&forceUpgrade, "force", false, "Force install : Overwrite tainted and outdated files")
+	cmdPostOverflow.AddCommand(cmdPostOverflowUpgrade)
+
+	var cmdPostOverflowInspect = &cobra.Command{
+		Use:     "inspect [config]",
+		Short:   "Inspect given postoverflow",
+		Long:    `Inspect given postoverflow`,
+		Example: `cscli postoverflow inspect crowdsec/xxx`,
+		Args:    cobra.MinimumNArgs(1),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cwhub.GetHubIdx(csConfig.Cscli); err != nil {
+				log.Fatalf("failed to get Hub index : %v", err)
+			}
+			InspectItem(args[0], cwhub.PARSERS_OVFLW)
+		},
+	}
+	cmdPostOverflow.AddCommand(cmdPostOverflowInspect)
+
+	return cmdPostOverflow
+}
--- cmd/crowdsec-cli/utils.go
+++ cmd/crowdsec-cli/utils.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/crowdsecurity/crowdsec/pkg/cwhub"
 	"github.com/crowdsecurity/crowdsec/pkg/cwversion"
+	"github.com/enescakir/emoji"
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/mod/semver"
+	"gopkg.in/yaml.v2"
 )
 
 func inSlice(s string, slice []string) bool {
@@ -50,4 +54,128 @@ func setHubBranch() error {
 		log.Debugf("Using branch '%s' for the hub", cwhub.HubBranch)
 	}
 	return nil
+}
+
+func InstallItem(name string, obtype string) {
+	it := cwhub.GetItem(obtype, name)
+	if it == nil {
+		log.Fatalf("unable to retrive item : %s", name)
+	}
+	item := *it
+	if downloadOnly && item.Downloaded && item.UpToDate {
+		log.Warningf("%s is already downloaded and up-to-date", item.Name)
+		return
+	}
+	item, err := cwhub.DownloadLatest(csConfig.Cscli, item, forceInstall)
+	if err != nil {
+		log.Fatalf("error while downloading %s : %v", item.Name, err)
+	}
+	cwhub.AddItemMap(obtype, item)
+	if downloadOnly {
+		log.Infof("Downloaded %s to %s", item.Name, csConfig.Cscli.HubDir+"/"+item.RemotePath)
+		return
+	}
+	item, err = cwhub.EnableItem(csConfig.Cscli, item)
+	if err != nil {
+		log.Fatalf("error while enabled %s : %v.", item.Name, err)
+	}
+	cwhub.AddItemMap(obtype, item)
+	log.Infof("Enabled %s", item.Name)
+	return
+	log.Warningf("%s not found in hub index", name)
+	/*iterate of pkg index data*/
+}
+
+func RemoveMany(ttype string, name string) {
+	var err error
+	var disabled int
+	if name != "" {
+		it := cwhub.GetItem(ttype, name)
+		if it == nil {
+			log.Fatalf("unable to retrieve: %s", name)
+		}
+		item := *it
+		item, err = cwhub.DisableItem(csConfig.Cscli, item, purgeRemove)
+		if err != nil {
+			log.Fatalf("unable to disable %s : %v", item.Name, err)
+		}
+		cwhub.AddItemMap(ttype, item)
+		return
+	} else if name == "" && removeAll {
+		for _, v := range cwhub.GetItemMap(ttype) {
+			v, err = cwhub.DisableItem(csConfig.Cscli, v, purgeRemove)
+			if err != nil {
+				log.Fatalf("unable to disable %s : %v", v.Name, err)
+			}
+			cwhub.AddItemMap(ttype, v)
+			disabled++
+		}
+	}
+	if name != "" && !removeAll {
+		log.Errorf("%s not found", name)
+		return
+	}
+	log.Infof("Disabled %d items", disabled)
+}
+
+func UpgradeConfig(ttype string, name string) {
+	var err error
+	var updated int
+	var found bool
+
+	for _, v := range cwhub.GetItemMap(ttype) {
+		if name != "" && name != v.Name {
+			continue
+		}
+		if !v.Installed {
+			log.Debugf("skip %s, not installed", v.Name)
+			continue
+		}
+		if !v.Downloaded {
+			log.Warningf("%s : not downloaded, please install.", v.Name)
+			continue
+		}
+		found = true
+		if v.UpToDate {
+			log.Infof("%s : up-to-date", v.Name)
+			continue
+		}
+		v, err = cwhub.DownloadLatest(csConfig.Cscli, v, forceUpgrade)
+		if err != nil {
+			log.Fatalf("%s : download failed : %v", v.Name, err)
+		}
+		if !v.UpToDate {
+			if v.Tainted {
+				log.Infof("%v %s is tainted, --force to overwrite", emoji.Warning, v.Name)
+			} else if v.Local {
+				log.Infof("%v %s is local", emoji.Prohibited, v.Name)
+			}
+		} else {
+			log.Infof("%v %s : updated", emoji.Package, v.Name)
+			updated++
+		}
+		cwhub.AddItemMap(ttype, v)
+	}
+	if !found {
+		log.Errorf("Didn't find %s", name)
+	} else if updated == 0 && found {
+		log.Errorf("Nothing to update")
+	} else if updated != 0 {
+		log.Infof("Upgraded %d items", updated)
+	}
+
+}
+
+func InspectItem(name string, objectType string) {
+
+	hubItem := cwhub.GetItem(objectType, name)
+	if hubItem == nil {
+		log.Fatalf("unable to retrieve item.")
+	}
+	buff, err := yaml.Marshal(*hubItem)
+	if err != nil {
+		log.Fatalf("unable to marshal item : %s", err)
+	}
+	fmt.Printf("%s", string(buff))
+
 }
