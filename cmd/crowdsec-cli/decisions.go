eb13eb8bb46a11a67d63975f3a14f9a484a50d18
erenJag <erenJag>
2020-09-24 18:29:03 +0200

    will see this after adding tests

--- cmd/crowdsec-cli/decisions.go
+++ cmd/crowdsec-cli/decisions.go
@@ -96,9 +96,6 @@ To list/add/delete decisions
 			if csConfig.LapiClient == nil {
 				log.Fatalln("There is no configuration on 'api_client:'")
 			}
-			if csConfig.LapiClient.Credentials.Url == "" {
-				log.Fatalln("There is no configuration on 'url:'")
-			}
 			apiclient.BaseURL, err = url.Parse(csConfig.LapiClient.Credentials.Url)
 			if err != nil {
 				log.Fatalf("failed to parse Local API URL %s : %v ", csConfig.LapiClient.Credentials.Url, err.Error())
