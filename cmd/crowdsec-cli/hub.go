dab46d37930a62834f1ec4aaff0f908b4e7e224f
erenJag <64777133+erenJag@users.noreply.github.com>
2020-09-25 11:08:51 +0200

    add hub command (#253)

---
+++ cmd/crowdsec-cli/hub.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/crowdsecurity/crowdsec/pkg/cwhub"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func NewHubCmd() *cobra.Command {
+	/* ---- HUB COMMAND */
+	var cmdHub = &cobra.Command{
+		Use:   "hub [action]",
+		Short: "Manage Hub",
+		Long: `
+Hub management
+
+List/update parsers/scenarios/postoverflows/collections from [Crowdsec Hub](https://hub.crowdsec.net).
+Hub is manage by cscli, to get latest hub files from [Crowdsec Hub](https://hub.crowdsec.net), you need to update.
+		`,
+		Example: `
+cscli hub list  # List all local configurations
+cscli hub update # update from [Crowdsec Hub](https://hub.crowdsec.net)
+		`,
+		Args: cobra.ExactArgs(0),
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if csConfig.Cscli == nil {
+				return fmt.Errorf("you must configure cli before interacting with hub")
+			}
+
+			return nil
+		},
+	}
+
+	var cmdHubList = &cobra.Command{
+		Use:   "list",
+		Short: "List available configs",
+		Long:  ``,
+		Args:  cobra.ExactArgs(0),
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cwhub.GetHubIdx(csConfig.Cscli); err != nil {
+				log.Fatalf("Failed to get Hub index : %v", err)
+			}
+
+			cwhub.DisplaySummary()
+			log.Printf("PARSERS:")
+			ListItem(cwhub.PARSERS, args)
+			log.Printf("SCENARIOS:")
+			ListItem(cwhub.SCENARIOS, args)
+			log.Printf("COLLECTIONS:")
+			ListItem(cwhub.COLLECTIONS, args)
+			log.Printf("POSTOVERFLOWS:")
+			ListItem(cwhub.PARSERS_OVFLW, args)
+		},
+	}
+	cmdHub.PersistentFlags().BoolVarP(&listAll, "all", "a", false, "List as well disabled items")
+	cmdHub.AddCommand(cmdHubList)
+
+	var cmdHubUpdate = &cobra.Command{
+		Use:   "update",
+		Short: "Fetch available configs from hub",
+		Long: `
+Fetches the [.index.json](https://github.com/crowdsecurity/hub/blob/master/.index.json) file from hub, containing the list of available configs.
+`,
+		Args: cobra.ExactArgs(0),
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			if csConfig.Cscli == nil {
+				return fmt.Errorf("you must configure cli before interacting with hub")
+			}
+
+			if err := setHubBranch(); err != nil {
+				return fmt.Errorf("error while setting hub branch: %s", err)
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cwhub.UpdateHubIdx(csConfig.Cscli); err != nil {
+				log.Fatalf("Failed to get Hub index : %v", err)
+			}
+		},
+	}
+	cmdHub.AddCommand(cmdHubUpdate)
+
+	return cmdHub
+}
