2cc40589dfad69757edbe4d443a6601b7b6b5eec
Thibault bui Koechlin <thibault@crowdsec.net>
2020-09-30 18:06:36 +0200

    if acquis dies first, wait for in-flight items

--- cmd/crowdsec/crowdsec.go
+++ cmd/crowdsec/crowdsec.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/crowdsecurity/crowdsec/pkg/acquisition"
 	"github.com/crowdsecurity/crowdsec/pkg/cwhub"
 	"github.com/crowdsecurity/crowdsec/pkg/exprhelpers"
+	leaky "github.com/crowdsecurity/crowdsec/pkg/leakybucket"
 	"github.com/crowdsecurity/crowdsec/pkg/types"
 	log "github.com/sirupsen/logrus"
 )
@@ -85,10 +87,69 @@ func serveCrowdsec(parsers *parsers) {
 			runCrowdsec(parsers)
 		}()
 
-		<-crowdsecTomb.Dying()
+		/*we should stop in two cases :
+		- crowdsecTomb has been Killed() : it might be shutdown or reload, so stop
+		- acquisTomb is dead, it means that we were in "cat" mode and files are done reading, quit
+		*/
+		waitOnTomb()
+		log.Warningf("Shutting down crowdsec routines")
 		if err := ShutdownCrowdsecRoutines(); err != nil {
 			log.Fatalf("unable to shutdown crowdsec routines: %s", err)
 		}
+		log.Errorf("everything is dead, return crowdsecTomb")
 		return nil
 	})
+}
+
+func waitOnTomb() {
+	for {
+		select {
+		case <-acquisTomb.Dead():
+			/*if it's acquisition dying it means that we were in "cat" mode.
+			while shutting down, we need to give time for all buckets to process in flight data*/
+			log.Warningf("Acquisition is finished, shutting down")
+			bucketCount := leaky.LeakyRoutineCount
+			rounds := 0
+			successiveStillRounds := 0
+			/*
+				While it might make sense to want to shut-down parser/buckets/etc. as soon as acquisition is finished,
+				we might have some pending buckets : buckets that overflowed, but which LeakRoutine are still alive because they
+				are waiting to be able to "commit" (push to api). This can happens specifically in a context where a lot of logs
+				are going to trigger overflow (ie. trigger buckets with ~100% of the logs triggering an overflow).
+
+				To avoid this (which would mean that we would "lose" some overflows), let's monitor the number of live buckets.
+				However, because of the blackhole mechanism, you can't really wait for the number of LeakRoutine to go to zero (we might have to wait $blackhole_duration).
+
+				So : we are waiting for the number of buckets to stop decreasing before returning. "how long" we should wait is a bit of the trick question,
+				as some operations (ie. reverse dns or such in post-overflow) can take some time :)
+			*/
+			for {
+				currBucketCount := leaky.LeakyRoutineCount
+
+				if currBucketCount == 0 {
+					/*no bucket to wait on*/
+					break
+				}
+				if currBucketCount != bucketCount {
+					if rounds == 0 || rounds%2 == 0 {
+						log.Printf("Still %d live LeakRoutines, waiting (was %d)", currBucketCount, bucketCount)
+					}
+					bucketCount = currBucketCount
+					successiveStillRounds = 0
+				} else {
+					if successiveStillRounds > 1 {
+						log.Printf("LeakRoutines commit over.")
+						break
+					}
+					successiveStillRounds++
+				}
+				rounds++
+				time.Sleep(5 * time.Second)
+			}
+			return
+		case <-crowdsecTomb.Dying():
+			log.Warningf("Crowdsec being killed, shutdown")
+			return
+		}
+	}
 }
