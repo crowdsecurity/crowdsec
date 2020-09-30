26a6320f1be2b69f883ca307202c9b883185c629
Thibault "bui" Koechlin <orixxx@gmail.com>
2020-09-30 18:44:00 +0200

    [lapi] fix oneshot shutdown (#274)

--- cmd/crowdsec/serve.go
+++ cmd/crowdsec/serve.go
@@ -139,6 +139,9 @@ func ShutdownCrowdsecRoutines() error {
 	}
 	log.Infof("outputs are done")
 
+	//everything is dead johny
+	crowdsecTomb.Kill(nil)
+
 	return reterr
 }
 
@@ -169,58 +172,6 @@ func termHandler(sig os.Signal) error {
 
 	log.Warningf("all routines are done, bye.")
 	return daemon.ErrStop
-}
-
-func serveOneTimeRun() error {
-	if err := acquisTomb.Wait(); err != nil {
-		log.Warningf("acquisition returned error : %s", err)
-	}
-	log.Infof("acquisition is finished, wait for parser/bucket/ouputs.")
-
-	/*
-		While it might make sense to want to shut-down parser/buckets/etc. as soon as acquisition is finished,
-		we might have some pending buckets : buckets that overflowed, but which LeakRoutine are still alive because they
-		are waiting to be able to "commit" (push to db). This can happens specifically in a context where a lot of logs
-		are going to trigger overflow (ie. trigger buckets with ~100% of the logs triggering an overflow).
-
-		To avoid this (which would mean that we would "lose" some overflows), let's monitor the number of live buckets.
-		However, because of the blackhole mechanism, you can't really wait for the number of LeakRoutine to go to zero (we might have to wait $blackhole_duration).
-
-		So : we are waiting for the number of buckets to stop decreasing before returning. "how long" we should wait is a bit of the trick question,
-		as some operations (ie. reverse dns or such in post-overflow) can take some time :)
-	*/
-
-	bucketCount := leaky.LeakyRoutineCount
-	rounds := 0
-	successiveStillRounds := 0
-	for {
-		rounds++
-		time.Sleep(5 * time.Second)
-		currBucketCount := leaky.LeakyRoutineCount
-		if currBucketCount != bucketCount {
-			if rounds == 0 || rounds%2 == 0 {
-				log.Printf("Still %d live LeakRoutines, waiting (was %d)", currBucketCount, bucketCount)
-			}
-			bucketCount = currBucketCount
-			successiveStillRounds = 0
-		} else {
-			if successiveStillRounds > 1 {
-				log.Printf("LeakRoutines commit over.")
-				break
-			}
-			successiveStillRounds++
-		}
-	}
-
-	time.Sleep(5 * time.Second)
-
-	// wait for the parser to parse all events
-	if err := ShutdownCrowdsecRoutines(); err != nil {
-		log.Errorf("failed shutting down routines : %s", err)
-	}
-	dumpMetrics()
-	log.Warningf("all routines are done, bye.")
-	return nil
 }
 
 func Serve(daemonCTX daemon.Context) error {
@@ -255,13 +206,7 @@ func Serve(daemonCTX daemon.Context) error {
 			log.Infof("lint done")
 			os.Exit(0)
 		}
-		if cConfig.Common != nil && !cConfig.Common.Daemonize {
-			if err := serveOneTimeRun(); err != nil {
-				log.Errorf(err.Error())
-			}
-		} else {
-			serveCrowdsec(csParsers)
-		}
+		serveCrowdsec(csParsers)
 	}
 
 	if cConfig.Common != nil && cConfig.Common.Daemonize {
@@ -270,23 +215,17 @@ func Serve(daemonCTX daemon.Context) error {
 		if err != nil {
 			return errors.Wrap(err, "serveDaemon returned")
 		}
-	}
-
-	/*
+	} else {
 		for {
 			select {
 			case <-apiTomb.Dead():
-				if err := shutdownCrowdsec(); err != nil {
-					log.Fatalf("unable to shutdown crowdsec: %s", err)
-				}
+				log.Errorf("api shutdown")
 				os.Exit(0)
 			case <-crowdsecTomb.Dead():
-				if err := shutdownAPI(); err != nil {
-					log.Fatalf("unable to shutdown api: %s", err)
-				}
+				log.Errorf("crowdsec shutdown")
 				os.Exit(0)
 			}
 		}
-	*/
+	}
 	return nil
 }
