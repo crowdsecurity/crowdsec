71e6b231eda23b75d331e28131c71ac8fba7b5f1
AlteredCoder <AlteredCoder>
2020-10-01 12:43:23 +0200

    fix wizard

--- wizard.sh
+++ wizard.sh
@@ -449,7 +449,7 @@ main() {
         log_info "installing crowdsec"
         install_crowdsec
          # api register
-        ${CSCLI_BIN_INSTALLED} watchers add --machine "$(cat /etc/machine-id)" --password "tmp^Crowdsec^password0.3.2"
+        ${CSCLI_BIN_INSTALLED} watchers add --machine "$(cat /etc/machine-id)" --password "$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-32};echo;)"
         log_info "Crowdsec api registered"
         return
     fi
