bcdb4e1026a077ce96882bc8fe972876a2ec6963
AlteredCoder <AlteredCoder>
2020-11-05 19:12:54 +0100

    fix signals

--- pkg/apiclient/signal.go
+++ pkg/apiclient/signal.go
@@ -9,21 +9,9 @@ import (
 	"github.com/pkg/errors"
 )
 
-type Signal struct {
-	Message         string         `json:"message"`
-	Scenario        string         `json:"scenario"`
-	ScenarioHash    string         `json:"scenario_hash"`
-	ScenarioVersion string         `json:"scenario_version"`
-	Source          *models.Source `json:"source"`
-	StartAt         string         `json:"start_at"`
-	StopAt          string         `json:"stop_at"`
-	MachineID       string         `json:"machine_id"`
-	CreatedAt       string         `json:"created_at"`
-}
-
 type SignalService service
 
-func (s *SignalService) Add(ctx context.Context, signals []*Signal) (interface{}, *Response, error) {
+func (s *SignalService) Add(ctx context.Context, signals *models.AddSignalsRequest) (interface{}, *Response, error) {
 	var response interface{}
 
 	u := fmt.Sprintf("%s/signals", s.client.URLPrefix)
