91b1c6da40db9bdad4176090ff4a40a4c4ed4f8a
AlteredCoder <AlteredCoder>
2020-11-07 03:00:34 +0100

    pimp cscli config show

--- pkg/csconfig/profiles.go
+++ pkg/csconfig/profiles.go
@@ -16,14 +16,14 @@ import (
 
 //Profile structure(s) are used by the local API to "decide" what kind of decision should be applied when a scenario with an active remediation has been triggered
 type ProfileCfg struct {
-	Name           string   `yaml:"name,omitempty"`
-	Debug          *bool    `yaml:"debug,omitempty"`
-	Filters        []string `yaml:"filters,omitempty"` //A list of OR'ed expressions. the models.Alert object
-	RuntimeFilters []*vm.Program
-	DebugFilters   []*exprhelpers.ExprDebugger
-	Decisions      []models.Decision `yaml:"decisions,omitempty"`
-	OnSuccess      string            `yaml:"on_success,omitempty"` //continue or break
-	OnFailure      string            `yaml:"on_failure,omitempty"` //continue or break
+	Name           string                      `yaml:"name,omitempty"`
+	Debug          *bool                       `yaml:"debug,omitempty"`
+	Filters        []string                    `yaml:"filters,omitempty"` //A list of OR'ed expressions. the models.Alert object
+	RuntimeFilters []*vm.Program               `json:"-"`
+	DebugFilters   []*exprhelpers.ExprDebugger `json:"-"`
+	Decisions      []models.Decision           `yaml:"decisions,omitempty"`
+	OnSuccess      string                      `yaml:"on_success,omitempty"` //continue or break
+	OnFailure      string                      `yaml:"on_failure,omitempty"` //continue or break
 }
 
 func (c *LocalApiServerCfg) LoadProfiles() error {
