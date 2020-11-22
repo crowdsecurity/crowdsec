3af90ea8c06ab804a93347f309110d8330afa8c7
Thibault bui Koechlin <thibault@crowdsec.net>
2020-11-22 11:05:19 +0100

    and cat mode as well for journalctl

--- pkg/acquisition/journalctl_reader.go
+++ pkg/acquisition/journalctl_reader.go
@@ -36,11 +36,18 @@ type JournaldSource struct {
 }
 
 func (j *JournaldSource) Configure(config DataSourceCfg) error {
+	var journalArgs []string
+
 	j.Config = config
 	if config.JournalctlFilters == nil {
 		return fmt.Errorf("journalctl_filter shouldn't be empty")
 	}
-	journalArgs := []string{"--follow"}
+
+	if j.Config.Mode == TAIL_MODE {
+		journalArgs = []string{"--follow"}
+	} else if j.Config.Mode == CAT_MODE {
+		journalArgs = []string{}
+	}
 	journalArgs = append(journalArgs, config.JournalctlFilters...)
 
 	j.Cmd = exec.Command("journalctl", journalArgs...)
@@ -51,8 +58,62 @@ func (j *JournaldSource) Configure(config DataSourceCfg) error {
 	return nil
 }
 
+func (j *JournaldSource) Mode() string {
+	return j.Config.Mode
+}
+
+func (j *JournaldSource) StartReading(out chan types.Event, t *tomb.Tomb) error {
+
+	if j.Config.Mode == CAT_MODE {
+		return j.StartCat(out, t)
+	} else if j.Config.Mode == TAIL_MODE {
+		return j.StartTail(out, t)
+	} else {
+		return fmt.Errorf("unknown mode '%s' for file acquisition", j.Config.Mode)
+	}
+}
+
 func (j *JournaldSource) StartCat(out chan types.Event, t *tomb.Tomb) error {
-	return fmt.Errorf("JournaldSource doesn't support cat")
+
+	/*
+		todo : handle the channel
+	*/
+	clog := log.WithFields(log.Fields{
+		"acquisition file": j.SrcName,
+	})
+	clog.Infof("starting (cat) journalctlxxxx")
+	if err := j.Cmd.Start(); err != nil {
+		clog.Errorf("failed to start journalctl: %s", err)
+		return errors.Wrapf(err, "starting journalctl (%s)", j.SrcName)
+	}
+	scanner := bufio.NewScanner(j.Stdout)
+	if scanner == nil {
+		clog.Errorf("failed to create scanner :<")
+		return fmt.Errorf("failed to create scanner")
+	}
+	for scanner.Scan() {
+		l := types.Line{}
+
+		ReaderHits.With(prometheus.Labels{"source": j.SrcName}).Inc()
+
+		l.Raw = scanner.Text()
+		l.Labels = j.Config.Labels
+		l.Time = time.Now()
+		l.Src = j.SrcName
+		l.Process = true
+		evt := types.Event{Line: l, Process: true, Type: types.LOG, ExpectMode: leaky.LIVE}
+		log.Printf("event -> %+v", evt)
+		out <- evt
+		if err := scanner.Err(); err != nil {
+			return errors.Wrapf(err, "reading %s", j.SrcName)
+		}
+	}
+	log.Errorf("Journald scanner %s returns", j.SrcName)
+	if err := scanner.Err(); err != nil {
+		return errors.Wrapf(err, "reading %s", j.SrcName)
+	}
+	t.Kill(nil)
+	return nil
 }
 
 func (j *JournaldSource) StartTail(out chan types.Event, t *tomb.Tomb) error {
@@ -73,28 +134,40 @@ func (j *JournaldSource) StartTail(out chan types.Event, t *tomb.Tomb) error {
 		clog.Errorf("failed to create scanner :<")
 		return fmt.Errorf("failed to create scanner")
 	}
-	for scanner.Scan() {
-		clog.Errorf("SCANNING LINNNNNE")
-		l := types.Line{}
 
-		ReaderHits.With(prometheus.Labels{"source": j.SrcName}).Inc()
-
-		l.Raw = scanner.Text()
-		l.Labels = j.Config.Labels
-		l.Time = time.Now()
-		l.Src = j.SrcName
-		l.Process = true
-		evt := types.Event{Line: l, Process: true, Type: types.LOG, ExpectMode: leaky.LIVE}
-		log.Printf("event -> %+v", evt)
-		out <- evt
+	readErr := make(chan error)
+	go func() {
+		for scanner.Scan() {
+			log.Debugf("journalctl : after scan")
+			l := types.Line{}
+			ReaderHits.With(prometheus.Labels{"source": j.SrcName}).Inc()
+			l.Raw = scanner.Text()
+			l.Labels = j.Config.Labels
+			l.Time = time.Now()
+			l.Src = j.SrcName
+			l.Process = true
+			evt := types.Event{Line: l, Process: true, Type: types.LOG, ExpectMode: leaky.LIVE}
+			log.Printf("event -> %+v", evt)
+			out <- evt
+			log.Debugf("journalctl : before scan")
+		}
 		if err := scanner.Err(); err != nil {
-			return errors.Wrapf(err, "reading %s", j.SrcName)
+			log.Warningf("reading %s : %s", j.SrcName, err)
+			readErr <- err
+		}
+	}()
+
+	for {
+		select {
+		case <-t.Dying():
+			log.Infof("journalctl datasource %s stopping", j.SrcName)
+			return nil
+		case err := <-readErr:
+			log.Infof("journalctl reader error : %s", err)
+			t.Kill(err)
+			return err
 		}
 	}
-	if err := scanner.Err(); err != nil {
-		return errors.Wrapf(err, "reading %s", j.SrcName)
-	}
-	return nil
 }
 
 func (j *JournaldSource) Exists() (bool, error) {
