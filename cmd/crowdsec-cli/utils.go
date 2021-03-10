6cdde4786113464675f7a5aa2ada7b713d1523c5
AlteredCoder <AlteredCoder>
2021-03-10 17:24:25 +0100

    fix

--- cmd/crowdsec-cli/utils.go
+++ cmd/crowdsec-cli/utils.go
@@ -71,23 +71,22 @@ func setHubBranch() error {
 	/*
 		if no branch has been specified in flags for the hub, then use the one corresponding to crowdsec version
 	*/
-
 	if cwhub.HubBranch == "" {
 		latest, err := cwversion.Latest()
 		if err != nil {
 			cwhub.HubBranch = "master"
 			return err
 		}
-
-		if cwversion.Version == latest {
+		csVersion := cwversion.VersionStrip()
+		if csVersion == latest {
 			cwhub.HubBranch = "master"
-		} else if semver.Compare(cwversion.Version, latest) == 1 { // if current version is greater than the latest we are in pre-release
-			log.Debugf("Your current crowdsec version seems to be a pre-release (%s)", cwversion.Version)
+		} else if semver.Compare(csVersion, latest) == 1 { // if current version is greater than the latest we are in pre-release
+			log.Debugf("Your current crowdsec version seems to be a pre-release (%s)", csVersion)
 			cwhub.HubBranch = "master"
 		} else {
-			log.Warnf("Crowdsec is not the latest version. Current version is '%s' and latest version is '%s'. Please update it!", cwversion.Version, latest)
+			log.Warnf("Crowdsec is not the latest version. Current version is '%s' and the latest stable version is '%s'. Please update it!", csVersion, latest)
 			log.Warnf("As a result, you will not be able to use parsers/scenarios/collections added to Crowdsec Hub after CrowdSec %s", latest)
-			cwhub.HubBranch = cwversion.Version
+			cwhub.HubBranch = csVersion
 		}
 		log.Debugf("Using branch '%s' for the hub", cwhub.HubBranch)
 	}
