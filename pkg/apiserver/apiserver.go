6d28599efa09606467a34fa337745f1aa7400433
Thibault "bui" Koechlin <thibault@crowdsec.net>
2021-03-22 17:46:55 +0100

    Ensure LAPI logs respect `log_media` (#707)
    * if log_media is set to file, don't try to log to stdout
    
    * use the log media no matter what

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -52,7 +52,7 @@ func NewServer(config *csconfig.LocalApiServerCfg) (*APIServer, error) {
 	}
 
 	logFile := ""
-	if config.LogDir != "" && config.LogMedia == "file" {
+	if config.LogMedia == "file" {
 		logFile = fmt.Sprintf("%s/crowdsec_api.log", config.LogDir)
 	}
 
@@ -86,7 +86,7 @@ func NewServer(config *csconfig.LocalApiServerCfg) (*APIServer, error) {
 		if err != nil {
 			return &APIServer{}, errors.Wrapf(err, "creating api access log file: %s", logFile)
 		}
-		gin.DefaultWriter = io.MultiWriter(file, os.Stdout)
+		gin.DefaultWriter = io.MultiWriter(file)
 	}
 
 	router.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
