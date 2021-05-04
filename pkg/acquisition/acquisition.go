34d0fea4048d7e271192c576fd95aebc255b43a9
Sebastien Blot <sebastien@crowdsec.net>
2021-05-04 16:36:24 +0200

    new acquisition interface

---
+++ pkg/acquisition/acquisition.go
@@ -0,0 +1,255 @@
+package acquisition
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"github.com/crowdsecurity/crowdsec/pkg/acquisition/configuration"
+	fileacquisition "github.com/crowdsecurity/crowdsec/pkg/acquisition/modules/file"
+	journalctlacquisition "github.com/crowdsecurity/crowdsec/pkg/acquisition/modules/journalctl"
+	"github.com/crowdsecurity/crowdsec/pkg/csconfig"
+	"github.com/crowdsecurity/crowdsec/pkg/types"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/yaml.v2"
+
+	tomb "gopkg.in/tomb.v2"
+)
+
+var ReaderHits = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "cs_reader_hits_total",
+		Help: "Total lines where read.",
+	},
+	[]string{"source"},
+)
+
+/*
+ current limits :
+ - The acquisition is not yet modular (cf. traefik/yaegi), but we start with an interface to pave the road for it.
+ - The configuration item unmarshaled (DataSourceCfg) isn't generic neither yet.
+ - This changes should be made when we're ready to have acquisition managed by the hub & cscli
+ once this change is done, we might go for the following configuration format instead :
+   ```yaml
+   ---
+   type: nginx
+   source: journald
+   filter: "PROG=nginx"
+   ---
+   type: nginx
+   source: files
+   filenames:
+	- "/var/log/nginx/*.log"
+    ---
+
+	type: nginx
+	source: file
+	file:
+		filenames:
+			- /var/log/xxx
+
+	```
+
+	!!! how to handle expect mode that is not directly linked to tail/cat mode
+*/
+
+/* Approach
+
+We support acquisition in two modes :
+ - tail mode : we're following a stream of info (tail -f $src). this is used when monitoring live logs
+ - cat mode : we're reading a file/source one-shot (cat $src), and scenarios will match the timestamp extracted from logs.
+
+One DataSourceCfg can lead to multiple goroutines, hence the Tombs passing around to allow proper tracking.
+tail mode shouldn't return except on errors or when externally killed via tombs.
+cat mode will return once source has been exhausted.
+*/
+
+// The interface each datasource must implement
+type DataSource interface {
+	GetMetrics() []prometheus.Collector              // Returns pointers to metrics that are managed by the module
+	Configure([]byte, *log.Entry) error              // Configure the datasource
+	ConfigureByDSN(string, string, *log.Entry) error // Configure the datasource
+	GetMode() string                                 // Get the mode (TAIL, CAT or SERVER)
+	GetName() string
+	OneShotAcquisition(chan types.Event, *tomb.Tomb) error   // Start one shot acquisition(eg, cat a file)
+	StreamingAcquisition(chan types.Event, *tomb.Tomb) error // Start live acquisition (eg, tail a file)
+	CanRun() error                                           // Whether the datasource can run or not (eg, journalctl on BSD is a non-sense)
+	Dump() interface{}
+}
+
+var AcquisitionSources = []struct {
+	name  string
+	iface func() DataSource
+}{
+	{
+		name:  "file",
+		iface: func() DataSource { return &fileacquisition.FileSource{} },
+	},
+	{
+		name:  "journalctl",
+		iface: func() DataSource { return &journalctlacquisition.JournalCtlSource{} },
+	},
+}
+
+func GetDataSourceIface(dataSourceType string) DataSource {
+	for _, source := range AcquisitionSources {
+		if source.name == dataSourceType {
+			return source.iface()
+		}
+	}
+	return nil
+}
+
+func DataSourceConfigure(commonConfig configuration.DataSourceCommonCfg) (*DataSource, error) {
+
+	//we dump it back to []byte, because we want to decode the yaml blob twice :
+	//once to DataSourceCommonCfg, and then later to the dedicated type of the datasource
+	yamlConfig, err := yaml.Marshal(commonConfig)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to marshal back interface")
+	}
+	if dataSrc := GetDataSourceIface(commonConfig.Source); dataSrc != nil {
+		/* this logger will then be used by the datasource at runtime */
+		clog := log.New()
+		if err := types.ConfigureLogger(clog); err != nil {
+			return nil, errors.Wrap(err, "while configuring datasource logger")
+		}
+		if commonConfig.LogLevel != nil {
+			clog.SetLevel(*commonConfig.LogLevel)
+		}
+		subLogger := clog.WithFields(log.Fields{
+			"type": commonConfig.Source,
+		})
+		/* check eventual dependencies are satisfied (ie. journald will check journalctl availability) */
+		if err := dataSrc.CanRun(); err != nil {
+			return nil, errors.Wrapf(err, "datasource %s cannot be run", commonConfig.Source)
+		}
+		/* configure the actual datasource */
+		if err := dataSrc.Configure(yamlConfig, subLogger); err != nil {
+			return nil, errors.Wrapf(err, "failed to configure datasource %s", commonConfig.Source)
+
+		}
+		return &dataSrc, nil
+	}
+	return nil, fmt.Errorf("cannot find source %s", commonConfig.Source)
+}
+
+//detectBackwardCompatAcquis : try to magically detect the type for backward compat (type was not mandatory then)
+func detectBackwardCompatAcquis(sub configuration.DataSourceCommonCfg) string {
+
+	if _, ok := sub.Config["filename"]; ok {
+		return "file"
+	}
+	if _, ok := sub.Config["filenames"]; ok {
+		return "file"
+	}
+	if _, ok := sub.Config["journalctl_filter"]; ok {
+		return "journalctl"
+	}
+	return ""
+}
+
+func LoadAcquisitionFromDSN(dsn string, label string) ([]DataSource, error) {
+	var sources []DataSource
+
+	frags := strings.Split(dsn, ":")
+	if len(frags) == 1 {
+		return nil, fmt.Errorf("%s isn't valid dsn (no protocol)", dsn)
+	}
+	dataSrc := GetDataSourceIface(frags[0])
+	if dataSrc == nil {
+		return nil, fmt.Errorf("no acquisition for protocol %s://", frags[0])
+	}
+	/* this logger will then be used by the datasource at runtime */
+	clog := log.New()
+	if err := types.ConfigureLogger(clog); err != nil {
+		return nil, errors.Wrap(err, "while configuring datasource logger")
+	}
+	subLogger := clog.WithFields(log.Fields{
+		"type": dsn,
+	})
+	err := dataSrc.ConfigureByDSN(dsn, label, subLogger)
+	if err != nil {
+		return nil, errors.Wrapf(err, "while configuration datasource for %s", dsn)
+	}
+	sources = append(sources, dataSrc)
+	return sources, nil
+}
+
+// LoadAcquisitionFromFile unmarshals the configuration item and checks its availability
+func LoadAcquisitionFromFile(config *csconfig.CrowdsecServiceCfg) ([]DataSource, error) {
+
+	var sources []DataSource
+
+	for _, acquisFile := range config.AcquisitionFiles {
+		log.Infof("loading acquisition file : %s", acquisFile)
+		yamlFile, err := os.Open(acquisFile)
+		if err != nil {
+			return nil, errors.Wrapf(err, "can't open %s", acquisFile)
+		}
+		dec := yaml.NewDecoder(yamlFile)
+		dec.SetStrict(true)
+		for {
+			var sub configuration.DataSourceCommonCfg
+			err = dec.Decode(&sub)
+			if err != nil {
+				if err == io.EOF {
+					log.Tracef("End of yaml file")
+					break
+				}
+				return nil, errors.Wrapf(err, "failed to yaml decode %s", acquisFile)
+			}
+
+			//for backward compat ('type' was not mandatory, detect it)
+			if guessType := detectBackwardCompatAcquis(sub); guessType != "" {
+				sub.Source = guessType
+			}
+			//it's an empty item, skip it
+			if len(sub.Labels) == 0 {
+				if sub.Source == "" {
+					log.Debugf("skipping empty item in %s", acquisFile)
+					continue
+				}
+				return nil, fmt.Errorf("missing labels in %s", acquisFile)
+			}
+
+			if GetDataSourceIface(sub.Source) == nil {
+				return nil, fmt.Errorf("unknown data source %s in %s", sub.Source, acquisFile)
+			}
+			src, err := DataSourceConfigure(sub)
+			if err != nil {
+				return nil, errors.Wrapf(err, "while configuring datasource of type %s from %s", sub.Source, acquisFile)
+			}
+			sources = append(sources, *src)
+		}
+	}
+	return sources, nil
+}
+
+func StartAcquisition(sources []DataSource, output chan types.Event, AcquisTomb *tomb.Tomb) error {
+	for i := 0; i < len(sources); i++ {
+		subsrc := sources[i] //ensure its a copy
+		log.Debugf("starting one source %d/%d ->> %T", i, len(sources), subsrc)
+		AcquisTomb.Go(func() error {
+			defer types.CatchPanic("crowdsec/acquis")
+			var err error
+			if subsrc.GetMode() == configuration.TAIL_MODE {
+				err = subsrc.StreamingAcquisition(output, AcquisTomb)
+			} else {
+				err = subsrc.OneShotAcquisition(output, AcquisTomb)
+			}
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+		//register acquisition specific metrics
+		prometheus.MustRegister(subsrc.GetMetrics()...)
+	}
+	/*return only when acquisition is over (cat) or never (tail)*/
+	err := AcquisTomb.Wait()
+	return err
+}
---
+++ pkg/acquisition/acquisition_test.go
@@ -0,0 +1,494 @@
+package acquisition
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/crowdsecurity/crowdsec/pkg/acquisition/configuration"
+	"github.com/crowdsecurity/crowdsec/pkg/csconfig"
+	"github.com/crowdsecurity/crowdsec/pkg/types"
+	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+	tomb "gopkg.in/tomb.v2"
+	"gopkg.in/yaml.v2"
+	"gotest.tools/v3/assert"
+)
+
+type MockSource struct {
+	configuration.DataSourceCommonCfg `yaml:",inline"`
+	Toto                              string `yaml:"toto"`
+	logger                            *log.Entry
+}
+
+func (f *MockSource) Configure(cfg []byte, logger *log.Entry) error {
+	f.logger = logger
+	if err := yaml.UnmarshalStrict(cfg, &f); err != nil {
+		return errors.Wrap(err, "while unmarshaling to reader specific config")
+	}
+	if f.Mode == "" {
+		f.Mode = configuration.CAT_MODE
+	}
+	if f.Mode != configuration.CAT_MODE && f.Mode != configuration.TAIL_MODE {
+		return fmt.Errorf("mode %s is not supported", f.Mode)
+	}
+	if f.Toto == "" {
+		return fmt.Errorf("expect non-empty toto")
+	}
+	return nil
+}
+func (f *MockSource) GetMode() string                                         { return f.Mode }
+func (f *MockSource) OneShotAcquisition(chan types.Event, *tomb.Tomb) error   { return nil }
+func (f *MockSource) StreamingAcquisition(chan types.Event, *tomb.Tomb) error { return nil }
+func (f *MockSource) CanRun() error                                           { return nil }
+func (f *MockSource) GetMetrics() []prometheus.Collector                      { return nil }
+func (f *MockSource) Dump() interface{}                                       { return f }
+func (f *MockSource) GetName() string                                         { return "mock" }
+func (f *MockSource) ConfigureByDSN(string, string, *log.Entry) error {
+	return fmt.Errorf("not supported")
+}
+
+//copy the mocksource, but this one can't run
+type MockSourceCantRun struct {
+	MockSource
+}
+
+func (f *MockSourceCantRun) CanRun() error   { return fmt.Errorf("can't run bro") }
+func (f *MockSourceCantRun) GetName() string { return "mock_cant_run" }
+
+//appendMockSource is only used to add mock source for tests
+func appendMockSource() {
+	if GetDataSourceIface("mock") == nil {
+		mock := struct {
+			name  string
+			iface func() DataSource
+		}{
+			name:  "mock",
+			iface: func() DataSource { return &MockSource{} },
+		}
+		AcquisitionSources = append(AcquisitionSources, mock)
+	}
+	if GetDataSourceIface("mock_cant_run") == nil {
+		mock := struct {
+			name  string
+			iface func() DataSource
+		}{
+			name:  "mock_cant_run",
+			iface: func() DataSource { return &MockSourceCantRun{} },
+		}
+		AcquisitionSources = append(AcquisitionSources, mock)
+	}
+}
+
+func TestDataSourceConfigure(t *testing.T) {
+	appendMockSource()
+	tests := []struct {
+		TestName      string
+		RawBytes      []byte
+		ExpectedError string
+	}{
+		{
+			TestName: "basic_valid_config",
+			RawBytes: []byte(`
+mode: cat
+labels:
+  test: foobar
+log_level: info
+source: mock
+toto: test_value1
+`),
+		},
+		{
+			TestName: "basic_debug_config",
+			RawBytes: []byte(`
+mode: cat
+labels:
+  test: foobar
+log_level: debug
+source: mock
+toto: test_value1
+`),
+		},
+		{
+			TestName: "basic_tailmode_config",
+			RawBytes: []byte(`
+mode: tail
+labels:
+  test: foobar
+log_level: debug
+source: mock
+toto: test_value1
+`),
+		},
+		{
+			TestName: "bad_mode_config",
+			RawBytes: []byte(`
+mode: ratata
+labels:
+  test: foobar
+log_level: debug
+source: mock
+toto: test_value1
+`),
+			ExpectedError: "failed to configure datasource mock: mode ratata is not supported",
+		},
+		{
+			TestName: "bad_type_config",
+			RawBytes: []byte(`
+mode: cat
+labels:
+  test: foobar
+log_level: debug
+source: tutu
+`),
+			ExpectedError: "cannot find source tutu",
+		},
+		{
+			TestName: "mismatch_config",
+			RawBytes: []byte(`
+mode: cat
+labels:
+  test: foobar
+log_level: debug
+source: mock
+wowo: ajsajasjas
+`),
+			ExpectedError: "field wowo not found in type acquisition.MockSource",
+		},
+		{
+			TestName: "cant_run_error",
+			RawBytes: []byte(`
+mode: cat
+labels:
+  test: foobar
+log_level: debug
+source: mock_cant_run
+wowo: ajsajasjas
+`),
+			ExpectedError: "datasource mock_cant_run cannot be run: can't run bro",
+		},
+	}
+
+	for _, test := range tests {
+		common := configuration.DataSourceCommonCfg{}
+		yaml.Unmarshal(test.RawBytes, &common)
+		ds, err := DataSourceConfigure(common)
+		if test.ExpectedError != "" {
+			if err == nil {
+				t.Fatalf("expected error %s, got none", test.ExpectedError)
+			}
+			if !strings.Contains(err.Error(), test.ExpectedError) {
+				t.Fatalf("%s : expected error '%s' in '%s'", test.TestName, test.ExpectedError, err.Error())
+			} else {
+				continue
+			}
+		} else {
+			if err != nil {
+				t.Fatalf("%s : unexpected error '%s'", test.TestName, err)
+			}
+		}
+
+		switch test.TestName {
+		case "basic_valid_config":
+			mock := (*ds).Dump().(*MockSource)
+			assert.Equal(t, mock.Toto, "test_value1")
+			assert.Equal(t, mock.Mode, "cat")
+			assert.Equal(t, mock.logger.Logger.Level, log.InfoLevel)
+			assert.DeepEqual(t, mock.Labels, map[string]string{"test": "foobar"})
+		case "basic_debug_config":
+			mock := (*ds).Dump().(*MockSource)
+			assert.Equal(t, mock.Toto, "test_value1")
+			assert.Equal(t, mock.Mode, "cat")
+			assert.Equal(t, mock.logger.Logger.Level, log.DebugLevel)
+			assert.DeepEqual(t, mock.Labels, map[string]string{"test": "foobar"})
+		case "basic_tailmode_config":
+			mock := (*ds).Dump().(*MockSource)
+			assert.Equal(t, mock.Toto, "test_value1")
+			assert.Equal(t, mock.Mode, "tail")
+			assert.Equal(t, mock.logger.Logger.Level, log.DebugLevel)
+			assert.DeepEqual(t, mock.Labels, map[string]string{"test": "foobar"})
+		}
+	}
+}
+
+func TestLoadAcquisitionFromFile(t *testing.T) {
+	appendMockSource()
+	tests := []struct {
+		TestName      string
+		Config        csconfig.CrowdsecServiceCfg
+		ExpectedError string
+		ExpectedLen   int
+	}{
+		{
+			TestName: "non_existent_file",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"does_not_exist"},
+			},
+			ExpectedError: "can't open does_not_exist",
+			ExpectedLen:   0,
+		},
+		{
+			TestName: "invalid_yaml_file",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/badyaml.yaml"},
+			},
+			ExpectedError: "failed to yaml decode test_files/badyaml.yaml: yaml: unmarshal errors",
+			ExpectedLen:   0,
+		},
+		{
+			TestName: "invalid_empty_yaml",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/emptyitem.yaml"},
+			},
+			ExpectedLen: 0,
+		},
+		{
+			TestName: "basic_valid",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/basic_filemode.yaml"},
+			},
+			ExpectedLen: 2,
+		},
+		{
+			TestName: "missing_labels",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/missing_labels.yaml"},
+			},
+			ExpectedError: "missing labels in test_files/missing_labels.yaml",
+		},
+		{
+			TestName: "backward_compat",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/backward_compat.yaml"},
+			},
+			ExpectedLen: 2,
+		},
+		{
+			TestName: "bad_type",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/bad_source.yaml"},
+			},
+			ExpectedError: "unknown data source does_not_exist in test_files/bad_source.yaml",
+		},
+		{
+			TestName: "invalid_filetype_config",
+			Config: csconfig.CrowdsecServiceCfg{
+				AcquisitionFiles: []string{"test_files/bad_filetype.yaml"},
+			},
+			ExpectedError: "while configuring datasource of type file from test_files/bad_filetype.yaml",
+		},
+	}
+	for _, test := range tests {
+		dss, err := LoadAcquisitionFromFile(&test.Config)
+		if test.ExpectedError != "" {
+			if err == nil {
+				t.Fatalf("expected error %s, got none", test.ExpectedError)
+			}
+			if !strings.Contains(err.Error(), test.ExpectedError) {
+				t.Fatalf("%s : expected error '%s' in '%s'", test.TestName, test.ExpectedError, err.Error())
+			} else {
+				continue
+			}
+		} else {
+			if err != nil {
+				t.Fatalf("%s : unexpected error '%s'", test.TestName, err)
+			}
+		}
+		if len(dss) != test.ExpectedLen {
+			t.Fatalf("%s : expected %d datasources got %d", test.TestName, test.ExpectedLen, len(dss))
+		}
+
+	}
+}
+
+/*
+ test start acquisition :
+  - create mock parser in cat mode : start acquisition, check it returns, count items in chan
+  - create mock parser in tail mode : start acquisition, sleep, check item count, tomb kill it, wait for it to return
+*/
+
+type MockCat struct {
+	configuration.DataSourceCommonCfg `yaml:",inline"`
+	logger                            *log.Entry
+}
+
+func (f *MockCat) Configure(cfg []byte, logger *log.Entry) error {
+	f.logger = logger
+	if f.Mode == "" {
+		f.Mode = configuration.CAT_MODE
+	}
+	if f.Mode != configuration.CAT_MODE {
+		return fmt.Errorf("mode %s is not supported", f.Mode)
+	}
+	return nil
+}
+func (f *MockCat) GetName() string { return "mock_cat" }
+func (f *MockCat) GetMode() string { return "cat" }
+func (f *MockCat) OneShotAcquisition(out chan types.Event, tomb *tomb.Tomb) error {
+	for i := 0; i < 10; i++ {
+		evt := types.Event{}
+		evt.Line.Src = "test"
+		out <- evt
+	}
+	return nil
+}
+func (f *MockCat) StreamingAcquisition(chan types.Event, *tomb.Tomb) error {
+	return fmt.Errorf("can't run in tail")
+}
+func (f *MockCat) CanRun() error                                   { return nil }
+func (f *MockCat) GetMetrics() []prometheus.Collector              { return nil }
+func (f *MockCat) Dump() interface{}                               { return f }
+func (f *MockCat) ConfigureByDSN(string, string, *log.Entry) error { return fmt.Errorf("not supported") }
+
+//----
+
+type MockTail struct {
+	configuration.DataSourceCommonCfg `yaml:",inline"`
+	logger                            *log.Entry
+}
+
+func (f *MockTail) Configure(cfg []byte, logger *log.Entry) error {
+	f.logger = logger
+	if f.Mode == "" {
+		f.Mode = configuration.TAIL_MODE
+	}
+	if f.Mode != configuration.TAIL_MODE {
+		return fmt.Errorf("mode %s is not supported", f.Mode)
+	}
+	return nil
+}
+func (f *MockTail) GetName() string { return "mock_tail" }
+func (f *MockTail) GetMode() string { return "tail" }
+func (f *MockTail) OneShotAcquisition(out chan types.Event, tomb *tomb.Tomb) error {
+	return fmt.Errorf("can't run in cat mode")
+}
+func (f *MockTail) StreamingAcquisition(out chan types.Event, t *tomb.Tomb) error {
+	for i := 0; i < 10; i++ {
+		evt := types.Event{}
+		evt.Line.Src = "test"
+		out <- evt
+	}
+	select {
+	case <-t.Dying():
+		return nil
+	}
+}
+func (f *MockTail) CanRun() error                      { return nil }
+func (f *MockTail) GetMetrics() []prometheus.Collector { return nil }
+func (f *MockTail) Dump() interface{}                  { return f }
+func (f *MockTail) ConfigureByDSN(string, string, *log.Entry) error {
+	return fmt.Errorf("not supported")
+}
+
+//func StartAcquisition(sources []DataSource, output chan types.Event, AcquisTomb *tomb.Tomb) error {
+
+func TestStartAcquisitionCat(t *testing.T) {
+	sources := []DataSource{
+		&MockCat{},
+	}
+	out := make(chan types.Event)
+	acquisTomb := tomb.Tomb{}
+
+	go func() {
+		if err := StartAcquisition(sources, out, &acquisTomb); err != nil {
+			t.Fatalf("unexpected error")
+		}
+	}()
+
+	count := 0
+READLOOP:
+	for {
+		select {
+		case <-out:
+			count++
+		case <-time.After(1 * time.Second):
+			break READLOOP
+		}
+	}
+	if count != 10 {
+		t.Fatalf("expected 10 results, got %d", count)
+	}
+}
+
+func TestStartAcquisitionTail(t *testing.T) {
+	sources := []DataSource{
+		&MockTail{},
+	}
+	out := make(chan types.Event)
+	acquisTomb := tomb.Tomb{}
+
+	go func() {
+		if err := StartAcquisition(sources, out, &acquisTomb); err != nil {
+			t.Fatalf("unexpected error")
+		}
+	}()
+
+	count := 0
+READLOOP:
+	for {
+		select {
+		case <-out:
+			count++
+		case <-time.After(1 * time.Second):
+			break READLOOP
+		}
+	}
+	if count != 10 {
+		t.Fatalf("expected 10 results, got %d", count)
+	}
+	acquisTomb.Kill(nil)
+	time.Sleep(1 * time.Second)
+	if acquisTomb.Err() != nil {
+		t.Fatalf("unexpected tomb error %s (should be dead)", acquisTomb.Err())
+	}
+}
+
+//
+type MockTailError struct {
+	MockTail
+}
+
+func (f *MockTailError) StreamingAcquisition(out chan types.Event, t *tomb.Tomb) error {
+	for i := 0; i < 10; i++ {
+		evt := types.Event{}
+		evt.Line.Src = "test"
+		out <- evt
+	}
+	t.Kill(fmt.Errorf("got error (tomb)"))
+	return fmt.Errorf("got error")
+}
+
+func TestStartAcquisitionTailError(t *testing.T) {
+	sources := []DataSource{
+		&MockTailError{},
+	}
+	out := make(chan types.Event)
+	acquisTomb := tomb.Tomb{}
+
+	go func() {
+		if err := StartAcquisition(sources, out, &acquisTomb); err != nil && err.Error() != "got error (tomb)" {
+			t.Fatalf("expected error, got '%s'", err.Error())
+		}
+	}()
+
+	count := 0
+READLOOP:
+	for {
+		select {
+		case <-out:
+			count++
+		case <-time.After(1 * time.Second):
+			break READLOOP
+		}
+	}
+	if count != 10 {
+		t.Fatalf("expected 10 results, got %d", count)
+	}
+	//acquisTomb.Kill(nil)
+	time.Sleep(1 * time.Second)
+	if acquisTomb.Err().Error() != "got error (tomb)" {
+		t.Fatalf("didn't got expected error, got '%s'", acquisTomb.Err().Error())
+	}
+}
