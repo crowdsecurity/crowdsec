edacc15099ea6e16c81309e563e46b102acd64b1
he2ss <he2ss>
2021-05-12 14:31:56 +0200

    don't send alerts in simulated mode to capi

--- pkg/apiserver/apic.go
+++ pkg/apiserver/apic.go
@@ -172,6 +172,11 @@ func (a *apic) Push() error {
 				if alert.ScenarioVersion == nil || *alert.ScenarioVersion == "" || *alert.ScenarioVersion == "?" {
 					continue
 				}
+				/*we also ignore alerts in simulated mode*/
+				if *alert.Simulated {
+					log.Debugf("ignoring alert (id:%d) in simulated mode", alert.ID)
+					continue
+				}
 				signals = append(signals, AlertToSignal(alert))
 			}
 			a.mu.Lock()
