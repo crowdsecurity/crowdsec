fc592426842a18d210effb6c5f40b90a3a4ff6db
Shivam Sandbhor <shivam@crowdsec.net>
2021-07-15 17:09:26 +0530

    Add barebone support for plugins
    Signed-off-by: Shivam Sandbhor <shivam@crowdsec.net>

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -186,9 +186,6 @@ func NewServer(config *csconfig.LocalApiServerCfg) (*APIServer, error) {
 		controller.CAPIChan = nil
 	}
 
-	if err := controller.Init(); err != nil {
-		return &APIServer{}, err
-	}
 	return &APIServer{
 		URL:            config.ListenURI,
 		TLS:            config.TLS,
@@ -283,7 +280,6 @@ func (s *APIServer) Shutdown() error {
 
 func (s *APIServer) AttachPluginBroker(broker *csplugin.PluginBroker) {
 	s.controller.PluginChannel = broker.PluginChannel
-	go func() { broker.Run() }()
 }
 
 func (s *APIServer) InitController() error {
