2ffbae22fd966cd814c952cb5f4ca126053139f9
Shivam Sandbhor <shivam@crowdsec.net>
2021-07-20 13:14:00 +0530

    Add grouping of alerts for notification plugins
    Signed-off-by: Shivam Sandbhor <shivam@crowdsec.net>

--- pkg/csplugin/broker.go
+++ pkg/csplugin/broker.go
@@ -10,8 +10,10 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 	"syscall"
 	"text/template"
+	"time"
 
 	"github.com/crowdsecurity/crowdsec/pkg/csconfig"
 	"github.com/crowdsecurity/crowdsec/pkg/models"
@@ -21,14 +23,34 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var pluginLock sync.Mutex
+
+const (
+	PluginProtocolVersion uint   = 1
+	CrowdsecPluginKey     string = "CROWDSEC_PLUGIN_KEY"
+)
+
 type PluginBroker struct {
-	ProfileConfigs              []*csconfig.ProfileCfg
-	PluginChannel               chan ProfileAlert
-	NotificationConfigsByPlugin map[string][][]byte // "slack" -> []{config1, config2}
-	PluginConfigByName          map[string]PluginConfig
-	PluginPaths                 []string
-	PluginMap                   map[string]plugin.Plugin
-	NotificationPluginByName    map[string]Notifier
+	AlertsByPluginName              map[string][]*models.Alert
+	ProfileConfigs                  []*csconfig.ProfileCfg
+	PluginChannel                   chan ProfileAlert
+	PluginConfigByName              map[string]PluginConfig
+	PluginPaths                     []string
+	PluginMap                       map[string]plugin.Plugin
+	NotificationConfigsByPluginType map[string][][]byte // "slack" -> []{config1, config2}
+	NotificationPluginByName        map[string]Notifier
+	Ticker                          PluginWatcher
+	PluginKillMethods               []func()
+}
+
+// holder to determine where to dispatch config and how to format messages
+type PluginConfig struct {
+	Type           string        `yaml:"type"`
+	Name           string        `yaml:"name"`
+	GroupWait      time.Duration `yaml:"group_wait"`
+	GroupThreshold int           `yaml:"group_threshold"`
+
+	Format string `yaml:"format"` // specific to notification plugins
 }
 
 type ProfileAlert struct {
@@ -36,27 +58,60 @@ type ProfileAlert struct {
 	Alert     *models.Alert
 }
 
-// temporary holder to determine where to dispatch config
-type PluginConfig struct {
-	Type   string `yaml:"type"`
-	Name   string `yaml:"name"`
-	Format string `yaml:"format"`
+type PluginWatcher struct {
+	PluginConfigByName map[string]PluginConfig
+	TickerByPluginName map[string]*time.Ticker
+	AlertsByPluginName map[string][]*models.Alert
+	C                  chan string
+}
+
+func (mpt *PluginWatcher) Start() {
+	mpt.TickerByPluginName = make(map[string]*time.Ticker)
+	mpt.C = make(chan string)
+	for name, cfg := range mpt.PluginConfigByName {
+		ticker := time.NewTicker(cfg.GroupWait)
+		mpt.TickerByPluginName[name] = ticker
+	}
+
+	for name, cfg := range mpt.PluginConfigByName {
+		go func(name string) {
+			for {
+				<-mpt.TickerByPluginName[name].C
+				mpt.C <- name
+			}
+		}(name)
+
+		if cfg.GroupThreshold == 0 {
+			continue
+		}
+
+		go func(name string, cfg PluginConfig) {
+			for {
+				time.Sleep(time.Second)
+				pluginLock.Lock()
+				if len(mpt.AlertsByPluginName[name]) > cfg.GroupThreshold {
+					mpt.C <- name
+				}
+				pluginLock.Unlock()
+			}
+		}(name, cfg)
+	}
+
 }
 
 func (pb *PluginBroker) Init(profileConfigs []*csconfig.ProfileCfg, configPaths *csconfig.ConfigurationPaths) error {
 	pb.PluginChannel = make(chan ProfileAlert)
-	pb.NotificationConfigsByPlugin = make(map[string][][]byte)
+	pb.NotificationConfigsByPluginType = make(map[string][][]byte)
 	pb.NotificationPluginByName = make(map[string]Notifier)
 	pb.PluginMap = make(map[string]plugin.Plugin)
 	pb.PluginConfigByName = make(map[string]PluginConfig)
-
+	pb.AlertsByPluginName = make(map[string][]*models.Alert)
 	pb.ProfileConfigs = profileConfigs
-
 	if err := pb.LoadConfig(configPaths.NotificationDir); err != nil {
 		return err
 	}
-
 	err := pb.LoadPlugins(configPaths.PluginDir)
+	pb.Ticker = PluginWatcher{PluginConfigByName: pb.PluginConfigByName, AlertsByPluginName: pb.AlertsByPluginName}
 	return err
 }
 
@@ -75,13 +130,12 @@ func (pb *PluginBroker) LoadConfig(path string) error {
 		if err != nil {
 			return err
 		}
-		pb.NotificationConfigsByPlugin[pc.Type] = append(pb.NotificationConfigsByPlugin[pc.Type], data)
+		pb.NotificationConfigsByPluginType[pc.Type] = append(pb.NotificationConfigsByPluginType[pc.Type], data)
 	}
 	return nil
 }
 
 func (pb *PluginBroker) LoadPlugins(path string) error {
-	// TODO: break this into smaller methods
 	binaryPaths, err := listFilesAtPath(path)
 	if err != nil {
 		return err
@@ -96,87 +150,111 @@ func (pb *PluginBroker) LoadPlugins(path string) error {
 			log.Error(err)
 			continue
 		}
-		// TODO: assign this by using some sort of map.
 		if Type == "notification" {
-			log.Info("found notification plugin")
-			pb.PluginMap[name] = &NotifierPlugin{}
-			cmd := exec.Command("sh", "-c", binaryPath)
-			cmd.SysProcAttr = getProccessAtr()
-			uuid, err := getUUID()
+			pluginClient, err := pb.LoadNotificationPlugin(name, binaryPath)
 			if err != nil {
-				return err
-			}
-			handshake := plugin.HandshakeConfig{
-				ProtocolVersion:  1,
-				MagicCookieKey:   "CROWDSEC_PLUGIN_KEY",
-				MagicCookieValue: uuid,
+				log.Error(err)
+				continue
 			}
 
-			c := plugin.NewClient(&plugin.ClientConfig{
-				HandshakeConfig:  handshake,
-				Plugins:          pb.PluginMap,
-				Cmd:              cmd,
-				AllowedProtocols: []plugin.Protocol{plugin.ProtocolGRPC},
-			})
-
-			client, err := c.Client()
-			if err != nil {
-				return err
-			}
-			raw, err := client.Dispense(name)
-			if err != nil {
-				return err
-			}
-
-			pluginClient := raw.(Notifier)
-			for _, config := range pb.NotificationConfigsByPlugin[name] {
+			for _, config := range pb.NotificationConfigsByPluginType[name] {
 				pc := PluginConfig{}
-				// TODO: Refactor to avoid double marshalling.
 				err = yaml.Unmarshal(config, &pc)
 				if err != nil {
-					return err
+					log.Error(err)
+					continue
 				}
+
 				pb.PluginConfigByName[pc.Name] = pc
 				pb.NotificationPluginByName[pc.Name] = pluginClient
 				pluginClient.Configure(context.Background(), &protobufs.Config{Config: config})
 			}
 		}
 	}
-	return nil
+	return err
 }
 
+func (pb *PluginBroker) LoadNotificationPlugin(name string, binaryPath string) (Notifier, error) {
+	handshake, err := getHandshake()
+	if err != nil {
+		return nil, err
+	}
+	cmd := exec.Command(binaryPath)
+	cmd.SysProcAttr = getProccessAtr()
+	pb.PluginMap[name] = &NotifierPlugin{}
+	c := plugin.NewClient(&plugin.ClientConfig{
+		HandshakeConfig:  handshake,
+		Plugins:          pb.PluginMap,
+		Cmd:              cmd,
+		AllowedProtocols: []plugin.Protocol{plugin.ProtocolGRPC},
+	})
+	client, err := c.Client()
+	if err != nil {
+		return nil, err
+	}
+	raw, err := client.Dispense(name)
+	if err != nil {
+		return nil, err
+	}
+	pb.PluginKillMethods = append(pb.PluginKillMethods, c.Kill)
+	return raw.(Notifier), nil
+}
+
+func (pb *PluginBroker) KillPlugins() {
+	for _, kill := range pb.PluginKillMethods {
+		kill()
+	}
+}
+
+func (pb *PluginBroker) PushNotificationsToPlugin(pluginName string) error {
+	pluginLock.Lock()
+	defer pluginLock.Unlock()
+	message, err := formatAlerts(pb.PluginConfigByName[pluginName].Format, pb.AlertsByPluginName[pluginName])
+	if err != nil {
+		return err
+	}
+	log.Infof("%d total alerts", len(pb.AlertsByPluginName[pluginName]))
+	plugin := pb.NotificationPluginByName[pluginName]
+	_, err = plugin.Notify(
+		context.Background(),
+		&protobufs.Notification{
+			Text: message,
+			Name: pluginName,
+		},
+	)
+	if err != nil {
+		return err
+	}
+	pb.AlertsByPluginName[pluginName] = make([]*models.Alert, 0)
+	return nil
+}
 func (pb *PluginBroker) Run() {
+	go pb.Ticker.Start()
 	for {
-		profileAlert := <-pb.PluginChannel
-		for _, pluginName := range pb.ProfileConfigs[profileAlert.ProfileID].Notifications {
-			log.Infof("%s", pluginName)
-			template, err := template.New("").Parse(
-				pb.PluginConfigByName[pluginName].Format,
-			)
-			if err != nil {
-				log.Error(err)
-			}
-			b := new(strings.Builder)
-			err = template.Execute(b, profileAlert.Alert)
-			if err != nil {
-				log.WithField("plugin:", pluginName).Error(err)
-			}
-			log.Info(b.String())
-			log.WithField("plugin:", pluginName).Infof("receiving alert")
-
-			go func(plugin Notifier, name string) {
-				_, err = plugin.Notify(
-					context.TODO(), &protobufs.Notification{
-						Text: b.String(),
-						Name: name,
-					},
-				)
-				if err != nil {
-					log.WithField("plugin:", name).Error(err)
+		select {
+		case profileAlert := <-pb.PluginChannel:
+			go func() {
+				pluginLock.Lock()
+				defer pluginLock.Unlock()
+				for _, pluginName := range pb.ProfileConfigs[profileAlert.ProfileID].Notifications {
+					if _, ok := pb.PluginConfigByName[pluginName]; !ok {
+						log.Errorf("binary for notification plugin %s  not found in", pluginName)
+						continue
+					}
+					pb.AlertsByPluginName[pluginName] = append(pb.AlertsByPluginName[pluginName], profileAlert.Alert)
 				}
-			}(pb.NotificationPluginByName[pluginName], pluginName)
-		}
+			}()
 
+		case pluginName := <-pb.Ticker.C:
+			go func() {
+				if len(pb.AlertsByPluginName[pluginName]) == 0 {
+					return
+				}
+				if err := pb.PushNotificationsToPlugin(pluginName); err != nil {
+					log.Error(err)
+				}
+			}()
+		}
 	}
 }
 
@@ -246,4 +324,30 @@ func getUUID() (string, error) {
 	} else {
 		return string(d), nil
 	}
+}
+
+func getHandshake() (plugin.HandshakeConfig, error) {
+	uuid, err := getUUID()
+	if err != nil {
+		return plugin.HandshakeConfig{}, err
+	}
+	handshake := plugin.HandshakeConfig{
+		ProtocolVersion:  PluginProtocolVersion,
+		MagicCookieKey:   CrowdsecPluginKey,
+		MagicCookieValue: uuid,
+	}
+	return handshake, nil
+}
+
+func formatAlerts(format string, alerts []*models.Alert) (string, error) {
+	template, err := template.New("").Parse(format)
+	if err != nil {
+		return "", err
+	}
+	b := new(strings.Builder)
+	err = template.Execute(b, alerts)
+	if err != nil {
+		return "", err
+	}
+	return b.String(), nil
 }
