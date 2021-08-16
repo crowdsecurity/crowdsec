f53657fd971033fe42dad8b17de69660eef3301a
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2021-08-16 17:56:23 +0200

    Update test_build.ps1

--- test_build.ps1
+++ test_build.ps1
@@ -4,6 +4,7 @@ choco install -y golang
 choco install -y jq 
 choco install -y git
 choco install -y mingw
+refreshenv
 git clone https://github.com/zecloud/crowdsec
 cd .\crowdsec\cmd\crowdsec\
 go build 
@@ -11,8 +12,8 @@ cd ..\crowdsec-cli\
 go build 
 mkdir  $HOME\crowdsec\config\hub\
 mkdir  $HOME\crowdsec\data\
-crowdsec-cli -c ..\..\config\config_win.yaml hub update -b master
-crowdsec-cli -c ..\..\config\config_win.yaml machines add -a
-crowdsec-cli -c ..\..\config\config_win.yaml capi register
+.\crowdsec-cli.exe -c ..\..\config\config_win.yaml hub update -b master
+.\crowdsec-cli.exe -c ..\..\config\config_win.yaml machines add -a
+.\crowdsec-cli.exe -c ..\..\config\config_win.yaml capi register
 cd ..\crowdsec\
-crowdsec -c ..\..\config\config_win.yaml
+.\crowdsec.exe -c ..\..\config\config_win.yaml
