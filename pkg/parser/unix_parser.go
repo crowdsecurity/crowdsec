c82185d044e134be1e688b6d4b561fd25420b4b8
ThinkChaos <ThinkChaos@users.noreply.github.com>
2021-08-20 13:40:17 -0400

    Fix manual path joining

--- pkg/parser/unix_parser.go
+++ pkg/parser/unix_parser.go
@@ -37,7 +37,7 @@ func Init(c map[string]interface{}) (*UnixParserCtx, error) {
 	}
 	r.DataFolder = c["data"].(string)
 	for _, f := range files {
-		if err := r.Grok.AddFromFile(c["patterns"].(string) + f.Name()); err != nil {
+		if err := r.Grok.AddFromFile(path.Join(c["patterns"].(string), f.Name())); err != nil {
 			log.Errorf("failed to load pattern %s : %v", f.Name(), err)
 			return nil, err
 		}
