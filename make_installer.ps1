9a59a8a5dba31c92e35e877a132a2bcdec8e30b2
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2021-08-24 13:46:23 +0000

    template for generating choco  package

--- make_installer.ps1
+++ make_installer.ps1
@@ -1,3 +1,4 @@
 choco install -y wixtoolset
 $env:Path += ";C:\Program Files (x86)\WiX Toolset v3.11\bin"
-go-msi make --msi crowdsec.msi --version 0.0.1 -s .\windows\installer\
+go-msi make --msi crowdsec.msi --version 0.0.1 -s .\windows\installer\ --arch amd64
+go-msi choco --out choco --version 0.0.1 -s .\windows\choco\ -i crowdsec.msi 
