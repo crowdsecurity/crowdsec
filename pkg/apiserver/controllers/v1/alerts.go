59e3663433bfd1288769333067824437029d02c9
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2021-08-25 16:05:47 +0200

    Fix indentation in alerts.go

--- pkg/apiserver/controllers/v1/alerts.go
+++ pkg/apiserver/controllers/v1/alerts.go
@@ -167,14 +167,14 @@ func (c *Controller) CreateAlert(gctx *gin.Context) {
 		alert.MachineID = machineID
 	}
 
-  if c.CAPIChan != nil {
-      select {
-      case c.CAPIChan <- input:
-              log.Debug("alert sent to CAPI channel")
-      default:
-              log.Warning("Cannot send alert to Central API channel")
-      }
-  }
+  	if c.CAPIChan != nil {
+      		select {
+      		case c.CAPIChan <- input:
+	      		log.Debug("alert sent to CAPI channel")
+      		default:
+	      		log.Warning("Cannot send alert to Central API channel")
+      		}
+  	}
 
 	gctx.JSON(http.StatusCreated, alerts)
 	return
