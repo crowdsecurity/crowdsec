f908c47b698ff6473cb855d68a8b99d687dc2bd8
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2021-10-05 12:40:08 +0200

    Update hubtest.go

--- cmd/crowdsec-cli/hubtest.go
+++ cmd/crowdsec-cli/hubtest.go
@@ -30,9 +30,9 @@ func NewHubTestCmd() *cobra.Command {
 
 	var cmdHubTest = &cobra.Command{
 		Use:   "hubtest",
-		Short: "Run functionals tests on hub configurations",
+		Short: "Run functional tests on hub configurations",
 		Long: `
-		Run functionals tests on hub configurations (parsers, scenarios, collections...)
+		Run functional tests on hub configurations (parsers, scenarios, collections...)
 		`,
 		Args:              cobra.ExactArgs(0),
 		DisableAutoGenTag: true,
