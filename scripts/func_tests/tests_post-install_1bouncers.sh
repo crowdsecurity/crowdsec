08ce35376645fd6b59a8390c85b1ad0433274642
Marco Mariani <marco@crowdsec.net>
2021-12-01 11:08:32 +0100

    fatal if cannot add bouncer: fix for #1066

--- scripts/func_tests/tests_post-install_1bouncers.sh
+++ scripts/func_tests/tests_post-install_1bouncers.sh
@@ -12,8 +12,8 @@ ${CSCLI} bouncers list -ojson  | ${JQ} '. | length == 0' || fail "expected 0 bou
 # we can add one bouncer - should we save token for later ?
 ${CSCLI} bouncers add ciTestBouncer || fail "failed to add bouncer"
 
-# but we can't add it twice - we would get an error
-${CSCLI} bouncers add ciTestBouncer -ojson  2>&1 | ${JQ} '.level == "error"' || fail "didn't receive the expected error"
+# but we can't add it twice - we would get a fatal error
+${CSCLI} bouncers add ciTestBouncer -ojson  2>&1 | ${JQ} '.level == "fatal"' || fail "didn't receive the expected error"
 
 # we should have 1 bouncer
 ${CSCLI} bouncers list -ojson  | ${JQ} '. | length == 1' || fail "expected 1 bouncers"
