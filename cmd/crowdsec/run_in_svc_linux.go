009705dd0958ab632c910bd0b5208cc66e9ecb6d
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2021-12-30 16:02:03 +0000

    avoiding duplicate loadconfig

--- cmd/crowdsec/run_in_svc_linux.go
+++ cmd/crowdsec/run_in_svc_linux.go
@@ -1,6 +1,50 @@
 // +build linux
 package main
 
+import (
+	"os"
+
+	"github.com/crowdsecurity/crowdsec/pkg/csconfig"
+	"github.com/crowdsecurity/crowdsec/pkg/cwversion"
+	"github.com/crowdsecurity/crowdsec/pkg/types"
+	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus/hooks/writer"
+)
+
 func StartRunSvc() {
 
+	var (
+		cConfig *csconfig.Config
+		err     error
+	)
+
+	log.AddHook(&writer.Hook{ // Send logs with level higher than warning to stderr
+		Writer: os.Stderr,
+		LogLevels: []log.Level{
+			log.PanicLevel,
+			log.FatalLevel,
+		},
+	})
+	cConfig, err = csconfig.NewConfig(flags.ConfigFile, flags.DisableAgent, flags.DisableAPI)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if err := LoadConfig(cConfig); err != nil {
+		log.Fatalf(err.Error())
+	}
+	// Configure logging
+	if err = types.SetDefaultLoggerConfig(cConfig.Common.LogMedia, cConfig.Common.LogDir, *cConfig.Common.LogLevel); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	log.Infof("Crowdsec %s", cwversion.VersionStr())
+
+	// Enable profiling early
+	if cConfig.Prometheus != nil {
+		go registerPrometheus(cConfig.Prometheus)
+	}
+
+	if err := Serve(cConfig); err != nil {
+		log.Fatalf(err.Error())
+	}
 }
