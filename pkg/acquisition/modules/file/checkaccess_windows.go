e7d09406cbb4b96a6970e22d8843e1a176e29560
Sebastien Blot <sebastien@crowdsec.net>
2022-01-18 02:27:32 -0800

    Update file acquis tests for windows

--- pkg/acquisition/modules/file/checkaccess_windows.go
+++ pkg/acquisition/modules/file/checkaccess_windows.go
@@ -1,7 +1,17 @@
-//+build windows
+//go:build windows
+// +build windows
 
 package fileacquisition
 
+import (
+	"os"
+)
+
 func checkAccess(file string) error {
+	f, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+	_ = f.Close()
 	return nil
 }
