122f479e68dc317f6c81f441e44ba592d91179b1
alteredCoder <kevin@crowdsec.net>
2022-02-10 16:57:38 +0100

    update downloaddata function

--- cmd/crowdsec-cli/utils.go
+++ cmd/crowdsec-cli/utils.go
@@ -259,7 +259,7 @@ func UpgradeConfig(itemType string, name string, force bool) {
 		if v.UpToDate {
 			log.Infof("%s : up-to-date", v.Name)
 
-			if err = cwhub.DownloadDataIfNeeded(csConfig.Hub, v, false); err != nil {
+			if err = cwhub.DownloadDataIfNeeded(csConfig.Hub, v, force); err != nil {
 				log.Fatalf("%s : download failed : %v", v.Name, err)
 			}
 			continue
