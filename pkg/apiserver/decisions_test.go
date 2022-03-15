a350329a59fbf83b4a6766ea54040c974c536323
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2022-03-15 19:19:45 +0530

    Fix tests
    Signed-off-by: Shivam Sandbhor <shivam.sandbhor@gmail.com>

--- pkg/apiserver/decisions_test.go
+++ pkg/apiserver/decisions_test.go
@@ -257,7 +257,7 @@ func TestGetDecision(t *testing.T) {
 	router.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.Contains(t, w.Body.String(), "\"id\":3,\"origin\":\"test\",\"scenario\":\"crowdsecurity/test\",\"scope\":\"Ip\",\"type\":\"ban\",\"value\":\"127.0.0.1\"}]")
+	assert.Contains(t, w.Body.String(), "\"id\":1,\"origin\":\"test\",\"scenario\":\"crowdsecurity/test\",\"scope\":\"Ip\",\"type\":\"ban\",\"value\":\"127.0.0.1\"}]")
 
 	// Get Decision with invalid filter. It should ignore this filter
 	w = httptest.NewRecorder()
