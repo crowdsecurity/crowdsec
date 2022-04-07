5956ec172a872c2533ed8acdbdc8f035267a00fd
Thibault bui Koechlin <thibault@crowdsec.net>
2022-04-07 12:33:03 +0200

    cleanup config

--- pkg/apiserver/controllers/v1/controller.go
+++ pkg/apiserver/controllers/v1/controller.go
@@ -29,34 +29,28 @@ type Controller struct {
 }
 
 type ControllerV1Config struct {
-	DbClient          *database.Client
-	Ctx               context.Context
-	Profiles          []*csconfig.ProfileCfg
-	CapiChan          chan []*models.Alert
-	PluginChannel     chan csplugin.ProfileAlert
-	ConsoleConfig     csconfig.ConsoleConfig
-	TrustedIPs        []net.IPNet
-	AllowedAgentsOU   []string
-	AllowedBouncersOU []string
-	CRLPath           string
+	DbClient      *database.Client
+	Ctx           context.Context
+	Profiles      []*csconfig.ProfileCfg
+	CapiChan      chan []*models.Alert
+	PluginChannel chan csplugin.ProfileAlert
+	ConsoleConfig csconfig.ConsoleConfig
+	TrustedIPs    []net.IPNet
 }
 
 func New(cfg *ControllerV1Config) (*Controller, error) {
 	var err error
 	v1 := &Controller{
-		Ectx:              cfg.Ctx,
-		DBClient:          cfg.DbClient,
-		APIKeyHeader:      middlewares.APIKeyHeader,
-		Profiles:          cfg.Profiles,
-		CAPIChan:          cfg.CapiChan,
-		PluginChannel:     cfg.PluginChannel,
-		ConsoleConfig:     cfg.ConsoleConfig,
-		TrustedIPs:        cfg.TrustedIPs,
-		AllowedAgentsOU:   cfg.AllowedAgentsOU,
-		AllowedBouncersOU: cfg.AllowedBouncersOU,
-		CRLPath:           cfg.CRLPath,
+		Ectx:          cfg.Ctx,
+		DBClient:      cfg.DbClient,
+		APIKeyHeader:  middlewares.APIKeyHeader,
+		Profiles:      cfg.Profiles,
+		CAPIChan:      cfg.CapiChan,
+		PluginChannel: cfg.PluginChannel,
+		ConsoleConfig: cfg.ConsoleConfig,
+		TrustedIPs:    cfg.TrustedIPs,
 	}
-	v1.Middlewares, err = middlewares.NewMiddlewares(cfg.DbClient, cfg.AllowedAgentsOU, cfg.AllowedBouncersOU, cfg.CRLPath)
+	v1.Middlewares, err = middlewares.NewMiddlewares(cfg.DbClient)
 	if err != nil {
 		return v1, err
 	}
