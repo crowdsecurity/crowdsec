1f7a9297ebc0d80ca29fe5672b0ff55616d575a2
Sebastien Blot <sebastien@crowdsec.net>
2022-04-11 18:31:23 +0200

    up

--- pkg/cwhub/path_separator_windows.go
+++ pkg/cwhub/path_separator_windows.go
@@ -1,11 +1,17 @@
 package cwhub
 
+import (
+	"path/filepath"
+	"strings"
+)
+
 func CheckSuffix(hubpath string, remotePath string) bool {
-	//if !strings.HasSuffix(hubpath, remotePath) {
-	//	return true
-	//} else {
-	return false
-	//}
+	newPath := filepath.ToSlash(hubpath)
+	if !strings.HasSuffix(newPath, remotePath) {
+		return true
+	} else {
+		return false
+	}
 }
 
 func CheckName(vname string, fauthor string, fname string) bool {
