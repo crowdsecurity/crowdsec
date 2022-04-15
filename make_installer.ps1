3a448dfc47c0587085c887e40006e8bf3f12cffe
Sebastien Blot <sebastien@crowdsec.net>
2022-04-15 12:52:05 +0200

    up

--- make_installer.ps1
+++ make_installer.ps1
@@ -6,7 +6,7 @@ if ($version.StartsWith("v"))
 {
     $version = $version.Substring(1)
 }
-Remove-Item -Force -Recurse -Path .\msi
+Remove-Item -Force -Recurse -Path .\msi -ErrorAction SilentlyContinue
 #we only harvest the patterns dir, as we want to handle differently some yaml files in the config directory, and I really don't want to write xlst filters to exclude the files :(
 heat.exe dir config\patterns -nologo -cg CrowdsecPatterns -dr PatternsDir -g1 -gg -sf -srd -scom -sreg -out "msi\fragment.wxs"
 candle.exe -arch x64 -dSourceDir=config\patterns -dVersion="$version" -out msi\ msi\fragment.wxs windows\installer\WixUI_HK.wxs windows\installer\product.wxs
