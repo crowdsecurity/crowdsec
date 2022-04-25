66ee9411f329574846cb3ae5deb8badf534aed90
Sebastien Blot <sebastien@crowdsec.net>
2022-04-25 21:51:14 +0200

    Add AGENT_ONLY property to disable LAPI at install time

--- windows/Chocolatey/crowdsec/tools/chocolateyinstall.ps1
+++ windows/Chocolatey/crowdsec/tools/chocolateyinstall.ps1
@@ -2,16 +2,28 @@
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $fileLocation = Join-Path $toolsDir 'crowdsec.msi'
 
+$silentArgs = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+
+
+$pp = Get-PackageParameters
+
+if ($pp['AgentOnly']) {
+  $silentArgs += " AGENT_ONLY=1"
+}
+
+
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
   file64         = $fileLocation
   softwareName  = 'Crowdsec'
-  checksum64    = ''
-  checksumType64= 'sha256'
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  silentArgs    = $silentArgs
   validExitCodes= @(0, 3010, 1641)
 }
 
 Install-ChocolateyInstallPackage @packageArgs
+
+if ($pp['AgentOnly']) {
+  Write-Host "/AgentOnly was specified. LAPI is disabled, please register your agent manually and configure the service to start on boot."
+}
