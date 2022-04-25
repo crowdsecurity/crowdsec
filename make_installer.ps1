04b5478f6cba4ca506b478ddadd9f5ed8865f5d7
Sebastien Blot <sebastien@crowdsec.net>
2022-04-25 21:01:16 +0200

    Handle pre-release in windows build scripts

--- make_installer.ps1
+++ make_installer.ps1
@@ -6,6 +6,13 @@ if ($version.StartsWith("v"))
 {
     $version = $version.Substring(1)
 }
+
+#Pre-releases will be like 1.4.0-rc1, remove everything after the dash as it does not conform to the MSI versioning scheme
+if ($version.Contains("-")) 
+{
+    $version = $version.Substring(0, $version.IndexOf("-"))
+}
+
 Remove-Item -Force -Recurse -Path .\msi -ErrorAction SilentlyContinue
 #we only harvest the patterns dir, as we want to handle differently some yaml files in the config directory, and I really don't want to write xlst filters to exclude the files :(
 heat.exe dir config\patterns -nologo -cg CrowdsecPatterns -dr PatternsDir -g1 -gg -sf -srd -scom -sreg -out "msi\fragment.wxs"
