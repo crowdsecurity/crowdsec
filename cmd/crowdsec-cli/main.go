d63ff44ec622fed1bc12d44c2cb4c59b5f3b5809
sabban <15465465+sabban@users.noreply.github.com>
2022-05-18 13:38:08 +0200

    add notifications command

--- cmd/crowdsec-cli/main.go
+++ cmd/crowdsec-cli/main.go
@@ -86,7 +86,7 @@ func initConfig() {
 var validArgs = []string{
 	"scenarios", "parsers", "collections", "capi", "lapi", "postoverflows", "machines",
 	"metrics", "bouncers", "alerts", "decisions", "simulation", "hub", "dashboard",
-	"config", "completion", "version", "console",
+	"config", "completion", "version", "console", "notifications",
 }
 
 func prepender(filename string) string {
@@ -181,6 +181,7 @@ It is meant to allow you to manage bans, parsers/scenarios/etc, api and generall
 	rootCmd.AddCommand(NewConsoleCmd())
 	rootCmd.AddCommand(NewExplainCmd())
 	rootCmd.AddCommand(NewHubTestCmd())
+	rootCmd.AddCommand(NewNotificationsCmd())
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("While executing root command : %s", err)
 	}
