46731ca677df373c913041219f5ff19057647cf6
Mathieu Lecarme <mathieu@garambrogne.net>
2022-06-07 15:54:01 +0200

    Fix: specific entry unmarshal

--- pkg/acquisition/modules/loki/entry.go
+++ pkg/acquisition/modules/loki/entry.go
@@ -1,10 +1,29 @@
 package loki
 
-import "time"
+import (
+	"encoding/json"
+	"strconv"
+	"time"
+)
 
 type Entry struct {
 	Timestamp time.Time
 	Line      string
+}
+
+func (e *Entry) UnmarshalJSON(b []byte) error {
+	var values []string
+	err := json.Unmarshal(b, &values)
+	if err != nil {
+		return err
+	}
+	t, err := strconv.Atoi(values[0])
+	if err != nil {
+		return err
+	}
+	e.Timestamp = time.Unix(int64(t), 0)
+	e.Line = values[1]
+	return nil
 }
 
 type Stream struct {
