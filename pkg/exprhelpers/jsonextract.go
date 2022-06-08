1857f6b4894895cda37fbc86ac1a483bd75c202e
Sebastien Blot <sebastien@crowdsec.net>
2022-06-08 09:53:09 +0200

    wip

--- pkg/exprhelpers/jsonextract.go
+++ pkg/exprhelpers/jsonextract.go
@@ -1,6 +1,8 @@
 package exprhelpers
 
 import (
+	"encoding/json"
+	"fmt"
 	"strings"
 
 	"github.com/buger/jsonparser"
@@ -57,4 +59,81 @@ func JsonExtract(jsblob string, target string) string {
 
 	log.Tracef("extract path %+v", fullpath)
 	return JsonExtractLib(jsblob, fullpath...)
+}
+
+func jsonExtractType(jsblob string, target string, t jsonparser.ValueType) ([]byte, error) {
+	if !strings.HasPrefix(target, "[") {
+		target = strings.Replace(target, "[", ".[", -1)
+	}
+	fullpath := strings.Split(target, ".")
+
+	log.Tracef("extract path %+v", fullpath)
+
+	value, dataType, _, err := jsonparser.Get(
+		jsonparser.StringToBytes(jsblob),
+		fullpath...,
+	)
+
+	if err != nil {
+		if err == jsonparser.KeyPathNotFoundError {
+			log.Debugf("Key %+v doesn't exist", target)
+			return nil, fmt.Errorf("key %s does not exist", target)
+		}
+		log.Errorf("jsonExtractType : %s : %s", target, err)
+		return nil, fmt.Errorf("jsonExtractType: %s : %w", target, err)
+	}
+
+	if dataType != t {
+		log.Errorf("jsonExtractType : expected type %s for target %s but found %s", t, target, dataType.String())
+		return nil, fmt.Errorf("jsonExtractType: expected type %s for target %s but found %s", t, target, dataType.String())
+	}
+
+	return value, nil
+}
+
+func JsonExtractSlice(jsblob string, target string) []interface{} {
+
+	value, err := jsonExtractType(jsblob, target, jsonparser.Array)
+
+	if err != nil {
+		log.Errorf("JsonExtractSlice : %s", err)
+		return nil
+	}
+
+	s := make([]interface{}, 0)
+
+	err = json.Unmarshal(value, &s)
+	if err != nil {
+		log.Errorf("JsonExtractSlice : %s : %s", target, err)
+		return nil
+	}
+	return s
+}
+
+func JsonExtractObject(jsblob string, target string) map[string]interface{} {
+
+	value, err := jsonExtractType(jsblob, target, jsonparser.Object)
+
+	if err != nil {
+		log.Errorf("JsonExtractObject: %s", err)
+		return nil
+	}
+
+	s := make(map[string]interface{})
+
+	err = json.Unmarshal(value, &s)
+	if err != nil {
+		log.Errorf("JsonExtractObject: %s : %s", target, err)
+		return nil
+	}
+	return s
+}
+
+func ToJson(obj interface{}) string {
+	b, err := json.Marshal(obj)
+	if err != nil {
+		log.Errorf("ToJson : %s", err)
+		return ""
+	}
+	return string(b)
 }
--- pkg/exprhelpers/jsonextract_test.go
+++ pkg/exprhelpers/jsonextract_test.go
@@ -85,5 +85,80 @@ func TestJsonExtractUnescape(t *testing.T) {
 		}
 		log.Printf("test '%s' : OK", test.name)
 	}
+}
 
+func TestJsonExtractSlice(t *testing.T) {
+	if err := Init(); err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err := FileInit(TestFolder, "test_data_re.txt", "regex")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	tests := []struct {
+		name         string
+		jsonBlob     string
+		targetField  string
+		expectResult []interface{}
+	}{
+		{
+			name:         "try to extract a string as a slice",
+			jsonBlob:     `{"test" : "1234"}`,
+			targetField:  "test",
+			expectResult: nil,
+		},
+		{
+			name:         "basic json slice extract",
+			jsonBlob:     `{"test" : ["1234"]}`,
+			targetField:  "test",
+			expectResult: []interface{}{"1234"},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := JsonExtractSlice(test.jsonBlob, test.targetField)
+			assert.Equal(t, test.expectResult, result)
+		})
+	}
+}
+
+func TestJsonExtractObject(t *testing.T) {
+	if err := Init(); err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	err := FileInit(TestFolder, "test_data_re.txt", "regex")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	tests := []struct {
+		name         string
+		jsonBlob     string
+		targetField  string
+		expectResult map[string]interface{}
+	}{
+		{
+			name:         "try to extract a string as an object",
+			jsonBlob:     `{"test" : "1234"}`,
+			targetField:  "test",
+			expectResult: nil,
+		},
+		{
+			name:         "basic json object extract",
+			jsonBlob:     `{"test" : {"1234": {"foo": "bar"}}}`,
+			targetField:  "test",
+			expectResult: map[string]interface{}{"1234": map[string]interface{}{"foo": "bar"}},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			result := JsonExtractObject(test.jsonBlob, test.targetField)
+			assert.Equal(t, test.expectResult, result)
+		})
+	}
 }
