003306697dea4b98d53db340d2d5e3de20baebe2
Shivam Sandbhor <shivam.sandbhor@gmail.com>
2022-08-04 12:27:24 +0530

    Suggest bouncers to delete

--- cmd/crowdsec-cli/bouncers.go
+++ cmd/crowdsec-cli/bouncers.go
@@ -5,6 +5,8 @@ import (
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
+	"os"
+	"strings"
 	"time"
 
 	middlewares "github.com/crowdsecurity/crowdsec/pkg/apiserver/middlewares/v1"
@@ -111,7 +113,52 @@ Note: This command requires database direct access, so is intended to be run on 
 		Run: func(cmd *cobra.Command, arg []string) {
 			bouncers, err := getBouncers(dbClient)
 			if err != nil {
-				log.Fatalf("unable to list bouncers: %s", err)
+				log.Errorf("unable to list bouncers: %s", err)
+			}
+			if csConfig.Cscli.Output == "human" {
+
+				table := tablewriter.NewWriter(os.Stdout)
+				table.SetCenterSeparator("")
+				table.SetColumnSeparator("")
+
+				table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+				table.SetAlignment(tablewriter.ALIGN_LEFT)
+				table.SetHeader([]string{"Name", "IP Address", "Valid", "Last API pull", "Type", "Version", "Auth Type"})
+				for _, b := range blockers {
+					var revoked string
+					if !b.Revoked {
+						revoked = emoji.CheckMark.String()
+					} else {
+						revoked = emoji.Prohibited.String()
+					}
+					table.Append([]string{b.Name, b.IPAddress, revoked, b.LastPull.Format(time.RFC3339), b.Type, b.Version, b.AuthType})
+				}
+				table.Render()
+			} else if csConfig.Cscli.Output == "json" {
+				x, err := json.MarshalIndent(blockers, "", " ")
+				if err != nil {
+					log.Fatalf("failed to unmarshal")
+				}
+				fmt.Printf("%s", string(x))
+			} else if csConfig.Cscli.Output == "raw" {
+				csvwriter := csv.NewWriter(os.Stdout)
+				err := csvwriter.Write([]string{"name", "ip", "revoked", "last_pull", "type", "version", "auth_type"})
+				if err != nil {
+					log.Fatalf("failed to write raw header: %s", err)
+				}
+				for _, b := range blockers {
+					var revoked string
+					if !b.Revoked {
+						revoked = "validated"
+					} else {
+						revoked = "pending"
+					}
+					err := csvwriter.Write([]string{b.Name, b.IPAddress, revoked, b.LastPull.Format(time.RFC3339), b.Type, b.Version, b.AuthType})
+					if err != nil {
+						log.Fatalf("failed to write raw: %s", err)
+					}
+				}
+				csvwriter.Flush()
 			}
 			fmt.Printf("%s", bouncers)
 		},
@@ -171,6 +218,33 @@ cscli bouncers add MyBouncerName -k %s`, generatePassword(32)),
 		Args:              cobra.MinimumNArgs(1),
 		Aliases:           []string{"remove"},
 		DisableAutoGenTag: true,
+		ValidArgsFunction: func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+			var err error
+			if err := csConfig.LoadAPIServer(); err != nil || csConfig.DisableAPI {
+				cobra.CompError("Local API is disabled, please run this command on the local API machine" + err.Error())
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
+			if err := csConfig.LoadDBConfig(); err != nil {
+				cobra.CompError(err.Error())
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
+			dbClient, err = database.NewClient(csConfig.DbConfig)
+			if err != nil {
+				cobra.CompError("unable to create new database client: " + err.Error())
+				return nil, cobra.ShellCompDirectiveNoFileComp
+			}
+			bouncers, err := dbClient.ListBouncers()
+			if err != nil {
+				cobra.CompError("unable to list bouncers " + err.Error())
+			}
+			ret := make([]string, 0)
+			for _, bouncer := range bouncers {
+				if strings.Contains(bouncer.Name, toComplete) && !inSlice(bouncer.Name, args) {
+					ret = append(ret, bouncer.Name)
+				}
+			}
+			return ret, cobra.ShellCompDirectiveNoFileComp
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			for _, bouncerID := range args {
 				err := dbClient.DeleteBouncer(bouncerID)
