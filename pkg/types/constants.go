b47c6c5bb682eee933282449a483c5cc46294588
bui <thibault@crowdsec.net>
2022-08-23 17:01:51 +0200

    move urls to types

--- pkg/types/constants.go
+++ pkg/types/constants.go
@@ -3,3 +3,6 @@ package types
 const ApiKeyAuthType = "api-key"
 const TlsAuthType = "tls"
 const PasswordAuthType = "password"
+
+const PAPIBaseURL = "http://127.0.0.1:8082/v1/decisions/stream/poll"
+const CAPIBaseURL = "https://api.dev.crowdsec.net/"
