d52e3b221d27aa6dd83c4257d2c8cc28670e554a
bui <thibault@crowdsec.net>
2022-08-23 17:02:48 +0200

    pull PAPI, probably submitted to config or opt-in ?

--- pkg/apiserver/apiserver.go
+++ pkg/apiserver/apiserver.go
@@ -323,6 +323,15 @@ func (s *APIServer) Run(apiReady chan bool) error {
 			}
 			return nil
 		})
+
+		s.apic.pullTomb.Go(func() error {
+			if err := s.apic.PullPAPI(); err != nil {
+				log.Errorf("papi pull: %s", err)
+				return err
+			}
+			return nil
+		})
+
 		s.apic.metricsTomb.Go(func() error {
 			if err := s.apic.SendMetrics(); err != nil {
 				log.Errorf("capi metrics: %s", err)
