bc196565cb6edcb5e831fee1a9159f9ecf8a3f70
Marco Mariani <marco@crowdsec.net>
2022-09-22 10:46:32 +0200

    console_table.go

---
+++ cmd/crowdsec-cli/console_table.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"os"
+	"github.com/olekukonko/tablewriter"
+	"github.com/crowdsecurity/crowdsec/pkg/csconfig"
+	"github.com/enescakir/emoji"
+)
+
+func consoleStatusTable(csConfig csconfig.Config) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeaderAlignment(tablewriter.ALIGN_LEFT)
+	table.SetAlignment(tablewriter.ALIGN_LEFT)
+	table.SetHeader([]string{"Option Name", "Activated", "Description"})
+
+	for _, option := range csconfig.CONSOLE_CONFIGS {
+		switch option {
+		case csconfig.SEND_CUSTOM_SCENARIOS:
+			activated := string(emoji.CrossMark)
+			if *csConfig.API.Server.ConsoleConfig.ShareCustomScenarios {
+				activated = string(emoji.CheckMarkButton)
+			}
+
+			table.Append([]string{option, activated, "Send alerts from custom scenarios to the console"})
+
+		case csconfig.SEND_MANUAL_SCENARIOS:
+			activated := string(emoji.CrossMark)
+			if *csConfig.API.Server.ConsoleConfig.ShareManualDecisions {
+				activated = string(emoji.CheckMarkButton)
+			}
+
+			table.Append([]string{option, activated, "Send manual decisions to the console"})
+
+		case csconfig.SEND_TAINTED_SCENARIOS:
+			activated := string(emoji.CrossMark)
+			if *csConfig.API.Server.ConsoleConfig.ShareTaintedScenarios {
+				activated = string(emoji.CheckMarkButton)
+			}
+
+			table.Append([]string{option, activated, "Send alerts from tainted scenarios to the console"})
+		}
+	}
+
+	table.Render()
+}
