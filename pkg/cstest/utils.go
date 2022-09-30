edced6818a1bdd9fba75b1c3a91cbeb9be782280
mmetc <92726601+mmetc@users.noreply.github.com>
2022-09-30 16:01:42 +0200

    cleanup + fix flaky tests in file_test.go, apic_test.go (#1773)

--- pkg/cstest/utils.go
+++ pkg/cstest/utils.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Copy(sourceFile string, destinationFile string) error {
@@ -110,10 +111,23 @@ func CopyDir(src string, dest string) error {
 }
 
 func AssertErrorContains(t *testing.T, err error, expectedErr string) {
+	t.Helper()
+
 	if expectedErr != "" {
 		assert.ErrorContains(t, err, expectedErr)
 		return
 	}
 
 	assert.NoError(t, err)
+}
+
+func RequireErrorContains(t *testing.T, err error, expectedErr string) {
+	t.Helper()
+
+	if expectedErr != "" {
+		require.ErrorContains(t, err, expectedErr)
+		return
+	}
+
+	require.NoError(t, err)
 }
