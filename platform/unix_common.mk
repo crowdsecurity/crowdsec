crowdsecurity/crowdsec

commit 0449ec1868d934095d2bc39aa65ad007be4f603c

Windows Support (#1159)

platform/unix_common.mk
@@ -1,0 +1,18 @@
+
+RM=rm -rf
+CP=cp
+CPR=cp -r
+MKDIR=mkdir -p
+
+GO_MAJOR_VERSION = $(shell go version | cut -c 14- | cut -d' ' -f1 | cut -d'.' -f1)
+GO_MINOR_VERSION = $(shell go version | cut -c 14- | cut -d' ' -f1 | cut -d'.' -f2)
+
+BUILD_GOVERSION="$(shell go version | cut -d " " -f3 | sed -E 's/[go]+//g')"
+
+#Current versioning information from env
+BUILD_VERSION?="$(shell git describe --tags $$(git rev-list --tags --max-count=1))"
+BUILD_CODENAME="alphaga"
+BUILD_TIMESTAMP=$(shell date +%F"_"%T)
+BUILD_TAG?="$(shell git rev-parse HEAD)"
+DEFAULT_CONFIGDIR?=/etc/crowdsec
+DEFAULT_DATADIR?=/var/lib/crowdsec/data

commit 9c6eb0e8815eae52f6c10c0e83058c5ae1380085

set BUILD_VERSION to the correct tag
This fixes a regression introduced in 0449ec18 (Windows Support #1159)
where the build system uses the last local tag that has been created
(chonologically) instead of the most recent tag in the current branch.
This problem is not detected while working with stable versions because
they are built on a clean checkout.

platform/unix_common.mk
@@ -14,7 +14,7 @@ GO_MINOR_VERSION = $(shell command -v go >/dev/null && go version | cut -c 14- |
 BUILD_GOVERSION="$(shell command -v go >/dev/null && go version | cut -d " " -f3 | sed -E 's/[go]+//g')"
 
 #Current versioning information from env
-BUILD_VERSION?="$(shell git describe --tags $$(git rev-list --tags --max-count=1))"
+BUILD_VERSION?="$(shell git describe --tags)"
 BUILD_CODENAME="alphaga"
 BUILD_TIMESTAMP=$(shell date +%F"_"%T)
 BUILD_TAG?="$(shell git rev-parse HEAD)"