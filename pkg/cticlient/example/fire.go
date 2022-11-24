468abffcb7279b01d5c76e782a69552207566eb6
Sebastien Blot <sebastien@crowdsec.net>
2022-11-24 14:04:02 +0100

    update test script to be compatible with cscli import

--- pkg/cticlient/example/fire.go
+++ pkg/cticlient/example/fire.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/crowdsecurity/crowdsec/pkg/cticlient"
 )
@@ -14,8 +16,23 @@ func intPtr(i int) *int {
 func main() {
 	client := cticlient.NewCrowdsecCTIClient(cticlient.WithAPIKey(os.Getenv("CTI_API_KEY")))
 	paginator := cticlient.NewFirePaginator(client, cticlient.FireParams{
-		Limit: intPtr(1),
+		Limit: intPtr(1000),
 	})
+
+	csvHeader := []string{
+		"value",
+		"reason",
+		"type",
+		"scope",
+		"duration",
+	}
+	csvFile, err := os.Create("fire.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer csvFile.Close()
+	csvWriter := csv.NewWriter(csvFile)
+	allItems := make([][]string, 0)
 
 	for {
 		items, err := paginator.Next()
@@ -26,6 +43,17 @@ func main() {
 			break
 		}
 
-		fmt.Printf("Got %d items\n", len(items))
+		for _, item := range items {
+			banDuration := time.Until(item.Expiration.Time)
+			allItems = append(allItems, []string{
+				item.Ip,
+				"fire-import",
+				"ban",
+				"ip",
+				fmt.Sprintf("%ds", int(banDuration.Seconds())),
+			})
+		}
 	}
+	csvWriter.Write(csvHeader)
+	csvWriter.WriteAll(allItems)
 }
