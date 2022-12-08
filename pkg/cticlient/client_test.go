3d5a48637c1f79780d64976542deb0eaa75e2df9
bui <thibault@crowdsec.net>
2022-12-08 15:41:21 +0100

    moar tests

--- pkg/cticlient/client_test.go
+++ pkg/cticlient/client_test.go
@@ -3,11 +3,14 @@ package cticlient
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strconv"
 	"strings"
 	"testing"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/crowdsecurity/crowdsec/pkg/types"
 	"github.com/stretchr/testify/assert"
@@ -16,14 +19,11 @@ import (
 const validApiKey = "my-api-key"
 
 // Copy pasted from actual API response
-var responses = map[string]string{
+var smokeResponses = map[string]string{
 	"1.1.1.1": `{"ip_range_score": 0, "ip": "1.1.1.1", "ip_range": "1.1.1.0/24", "as_name": "CLOUDFLARENET", "as_num": 13335, "location": {"country": null, "city": null, "latitude": null, "longitude": null}, "reverse_dns": "one.one.one.one", "behaviors": [{"name": "ssh:bruteforce", "label": "SSH Bruteforce", "description": "IP has been reported for performing brute force on ssh services."}, {"name": "tcp:scan", "label": "TCP Scan", "description": "IP has been reported for performing TCP port scanning."}, {"name": "http:scan", "label": "HTTP Scan", "description": "IP has been reported for performing actions related to HTTP vulnerability scanning and discovery."}], "history": {"first_seen": "2021-04-18T18:00:00+00:00", "last_seen": "2022-11-23T13:00:00+00:00", "full_age": 583, "days_age": 583}, "classifications": {"false_positives": [], "classifications": [{"name": "profile:insecure_services", "label": "Dangerous Services Exposed", "description": "IP exposes dangerous services (vnc, telnet, rdp), possibly due to a misconfiguration or because it's a honeypot."}, {"name": "profile:many_services", "label": "Many Services Exposed", "description": "IP exposes many open port, possibly due to a misconfiguration or because it's a honeypot."}]}, "attack_details": [{"name": "crowdsecurity/ssh-bf", "label": "SSH Bruteforce", "description": "Detect ssh brute force", "references": []}, {"name": "crowdsecurity/iptables-scan-multi_ports", "label": "Port Scanner", "description": "Detect tcp port scan", "references": []}, {"name": "crowdsecurity/ssh-slow-bf", "label": "Slow SSH Bruteforce", "description": "Detect slow ssh brute force", "references": []}, {"name": "crowdsecurity/http-probing", "label": "HTTP Scanner", "description": "Detect site scanning/probing from a single ip", "references": []}, {"name": "crowdsecurity/http-path-traversal-probing", "label": "Path Traversal Scanner", "description": "Detect path traversal attempt", "references": []}, {"name": "crowdsecurity/http-bad-user-agent", "label": "Known Bad User-Agent", "description": "Detect bad user-agents", "references": []}], "target_countries": {"DE": 33, "FR": 25, "US": 12, "CA": 8, "JP": 8, "AT": 4, "GB": 4, "AE": 4}, "background_noise_score": 4, "scores": {"overall": {"aggressiveness": 2, "threat": 2, "trust": 1, "anomaly": 2, "total": 2}, "last_day": {"aggressiveness": 0, "threat": 0, "trust": 0, "anomaly": 2, "total": 0}, "last_week": {"aggressiveness": 1, "threat": 2, "trust": 0, "anomaly": 2, "total": 1}, "last_month": {"aggressiveness": 3, "threat": 2, "trust": 0, "anomaly": 2, "total": 2}}, "references": []}`,
 }
 
-var fireResponse = []string{
-	"foobar",
-	"foobar2",
-}
+var fireResponses []string
 
 // RoundTripFunc .
 type RoundTripFunc func(req *http.Request) *http.Response
@@ -35,25 +35,56 @@ func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
 
 // wip
 func fireHandler(req *http.Request) *http.Response {
+	var err error
 	apiKey := req.Header.Get("x-api-key")
 	if apiKey != validApiKey {
+		log.Warningf("invalid api key: %s", apiKey)
 		return &http.Response{
 			StatusCode: 403,
 			Body:       nil,
 			Header:     make(http.Header),
 		}
 	}
-
+	//unmarshal data
+	if fireResponses == nil {
+		page1, err := ioutil.ReadFile("tests/fire-page1.json")
+		if err != nil {
+			panic("can't read file")
+		}
+		page2, err := ioutil.ReadFile("tests/fire-page2.json")
+		if err != nil {
+			panic("can't read file")
+		}
+		fireResponses = []string{string(page1), string(page2)}
+	}
 	//let's assume we have two valid pages.
-	page, err := strconv.Atoi(req.URL.Query().Get("page"))
-	if err != nil {
-		return &http.Response{StatusCode: 500}
+	page := 1
+	if req.URL.Query().Get("page") != "" {
+		page, err = strconv.Atoi(req.URL.Query().Get("page"))
+		if err != nil {
+			log.Warningf("no page ?!")
+			return &http.Response{StatusCode: 500}
+		}
 	}
+
 	//how to react if you give a page number that is too big ?
-	if page > len(fireResponse) {
-		return &http.Response{StatusCode: 404}
+	if page > len(fireResponses) {
+		log.Warningf(" page too big %d vs %d", page, len(fireResponses))
+		emptyResponse := `{
+			"_links": {
+			  "first": {
+				"href": "https://cti.api.crowdsec.net/v1/fire/"
+			  },
+			  "self": {
+				"href": "https://cti.api.crowdsec.net/v1/fire/?page=3&limit=3"
+			  }
+			},
+			"items": []
+		  }
+		  `
+		return &http.Response{StatusCode: 200, Body: io.NopCloser(strings.NewReader(emptyResponse))}
 	}
-	reader := io.NopCloser(strings.NewReader(fireResponse[page-1]))
+	reader := io.NopCloser(strings.NewReader(fireResponses[page-1]))
 	//we should care about limit too
 	return &http.Response{
 		StatusCode: 200,
@@ -75,7 +106,7 @@ func smokeHandler(req *http.Request) *http.Response {
 	}
 
 	requestedIP := strings.Split(req.URL.Path, "/")[3]
-	response, ok := responses[requestedIP]
+	response, ok := smokeResponses[requestedIP]
 	if !ok {
 		return &http.Response{
 			StatusCode: 404,
@@ -134,7 +165,7 @@ func searchHandler(req *http.Request) *http.Response {
 	notFound := 0
 	ips := strings.Split(ipsParam, ",")
 	for _, ip := range ips {
-		_, ok := responses[ip]
+		_, ok := smokeResponses[ip]
 		if ok {
 			totalIps++
 		} else {
@@ -143,7 +174,7 @@ func searchHandler(req *http.Request) *http.Response {
 	}
 	response := fmt.Sprintf(`{"total": %d, "not_found": %d, "items": [`, totalIps, notFound)
 	for _, ip := range ips {
-		response += responses[ip]
+		response += smokeResponses[ip]
 	}
 	response += "]}"
 	reader := io.NopCloser(strings.NewReader(response))
@@ -160,6 +191,45 @@ func TestBadFireAuth(t *testing.T) {
 	}))
 	_, err := ctiClient.Fire(FireParams{})
 	assert.EqualError(t, err, ErrUnauthorized.Error())
+}
+
+func TestFireOk(t *testing.T) {
+	cticlient := NewCrowdsecCTIClient(WithAPIKey(validApiKey), WithHTTPClient(&http.Client{
+		Transport: RoundTripFunc(fireHandler),
+	}))
+	data, err := cticlient.Fire(FireParams{})
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(data.Items), 3)
+	assert.Equal(t, data.Items[0].Ip, "1.2.3.4")
+	//page 1 is the default
+	data, err = cticlient.Fire(FireParams{Page: types.IntPtr(1)})
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(data.Items), 3)
+	assert.Equal(t, data.Items[0].Ip, "1.2.3.4")
+	//page 2
+	data, err = cticlient.Fire(FireParams{Page: types.IntPtr(2)})
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(data.Items), 3)
+	assert.Equal(t, data.Items[0].Ip, "4.2.3.4")
+}
+
+func TestFirePaginator(t *testing.T) {
+	cticlient := NewCrowdsecCTIClient(WithAPIKey(validApiKey), WithHTTPClient(&http.Client{
+		Transport: RoundTripFunc(fireHandler),
+	}))
+	paginator := NewFirePaginator(cticlient, FireParams{})
+	items, err := paginator.Next()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(items), 3)
+	assert.Equal(t, items[0].Ip, "1.2.3.4")
+	items, err = paginator.Next()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(items), 3)
+	assert.Equal(t, items[0].Ip, "4.2.3.4")
+	items, err = paginator.Next()
+	assert.Equal(t, err, nil)
+	assert.Equal(t, len(items), 0)
+
 }
 
 func TestBadSmokeAuth(t *testing.T) {
