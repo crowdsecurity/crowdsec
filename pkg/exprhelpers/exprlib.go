e361c97e6bd5f4640cf16c1fc9fa9331cd8190ea
laurence <laurence.jones@live.co.uk>
2022-12-28 16:23:25 +0000

    Add original value not parsed error

--- pkg/exprhelpers/exprlib.go
+++ pkg/exprhelpers/exprlib.go
@@ -296,7 +296,7 @@ func UnixDate(value string) string {
 	//Splitting string here as some unix timestamp may have milliseconds and break ParseInt
 	i, err := strconv.ParseInt(strings.Split(value, ".")[0], 10, 64)
 	if err != nil {
-		log.Errorf("Unable to parse %s as unix timestamp. Error message: %s", i, err.Error())
+		log.Errorf("Unable to parse %s as unix timestamp. Error message: %s", value, err.Error())
 	}
 	return time.Unix(i, 0).Format(time.RFC3339)
 }
