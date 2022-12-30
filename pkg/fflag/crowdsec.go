7247f9ddc9b3621ac4d5a8052b38902df33ef677
Cristian Nitescu <cristian@crowdsec.net>
2022-12-30 12:18:15 +0100

    use feature toggling to improve testability with http retry backoff

--- pkg/fflag/crowdsec.go
+++ pkg/fflag/crowdsec.go
@@ -3,9 +3,14 @@ package fflag
 var Crowdsec = FeatureRegister{EnvPrefix: "CROWDSEC_FEATURE_"}
 
 var CscliSetup = &Feature{Name: "cscli_setup"}
+var DisableHttpRetryBackoff = &Feature{Name: "disable_http_retry_backoff", Description: "Disable http retry backoff"}
 
 func RegisterAllFeatures() error {
 	err := Crowdsec.RegisterFeature(CscliSetup)
+	if err != nil {
+		return err
+	}
+	err = Crowdsec.RegisterFeature(DisableHttpRetryBackoff)
 	if err != nil {
 		return err
 	}
