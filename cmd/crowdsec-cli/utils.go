185f9ad54192b9ac376642476d4acdf7179232fa
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2023-01-04 16:50:02 +0100

    Alert context (#1895)
    Co-authored-by: bui <thibault@crowdsec.net>

--- cmd/crowdsec-cli/utils.go
+++ cmd/crowdsec-cli/utils.go
@@ -748,3 +748,28 @@ func getDBClient() (*database.Client, error) {
 	}
 	return ret, nil
 }
+
+
+func removeFromSlice(val string, slice []string) []string {
+	var i int
+	var value string
+
+	valueFound := false
+
+	// get the index
+	for i, value = range slice {
+		if value == val {
+			valueFound = true
+			break
+		}
+	}
+
+	if valueFound {
+		slice[i] = slice[len(slice)-1]
+		slice[len(slice)-1] = ""
+		slice = slice[:len(slice)-1]
+	}
+
+	return slice
+
+}
