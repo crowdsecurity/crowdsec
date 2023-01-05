7c3d618416582e5d7e492245f9a14fae109aee32
bui <thibault@crowdsec.net>
2023-01-05 12:14:44 +0100

    linter

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -8,14 +8,18 @@ import (
 )
 
 func TestCreateSetGet(t *testing.T) {
-	CacheInit(CacheCfg{Name: "test", Size: 100, TTL: 1 * time.Second})
+	err := CacheInit(CacheCfg{Name: "test", Size: 100, TTL: 1 * time.Second})
+	assert.Empty(t, err)
 	//set & get
-	SetKey("test", "testkey0", "testvalue1", nil)
+	err = SetKey("test", "testkey0", "testvalue1", nil)
+	assert.Empty(t, err)
+
 	ret, err := GetKey("test", "testkey0")
 	assert.Equal(t, "testvalue1", ret)
 	assert.Empty(t, err)
 	//re-set
-	SetKey("test", "testkey0", "testvalue2", nil)
+	err = SetKey("test", "testkey0", "testvalue2", nil)
+	assert.Empty(t, err)
 	assert.Equal(t, "testvalue1", ret)
 	assert.Empty(t, err)
 	//expire
