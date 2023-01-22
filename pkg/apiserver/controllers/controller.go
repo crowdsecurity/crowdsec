a8ff9c1efae119094bf7910d15e4ab2242ff148e
laurence <laurence.jones@live.co.uk>
2023-01-22 19:38:43 +0000

    Extract method and make it extendable as a generic middleware

--- pkg/apiserver/controllers/controller.go
+++ pkg/apiserver/controllers/controller.go
@@ -84,13 +84,7 @@ func (c *Controller) NewV1() error {
 	})
 
 	groupV1 := c.Router.Group("/v1")
-	groupV1.POST("/watchers", func(gctx *gin.Context) {
-		incomingIP := gctx.ClientIP()
-		if c.DisableRemoteLapiRegistration && incomingIP != "127.0.0.1" && incomingIP != "::1" {
-			gctx.JSON(http.StatusForbidden, gin.H{"message": "access forbidden"})
-			gctx.Abort()
-		}
-	}, c.HandlerV1.CreateMachine)
+	groupV1.POST("/watchers", c.HandlerV1.AbortIfRemote(c.DisableRemoteLapiRegistration), c.HandlerV1.CreateMachine)
 	groupV1.POST("/watchers/login", c.HandlerV1.Middlewares.JWT.Middleware.LoginHandler)
 
 	jwtAuth := groupV1.Group("")
