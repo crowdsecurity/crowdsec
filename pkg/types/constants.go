7e871d2278895497c827d13607c05616f5ac5c45
AlteredCoder <64792091+AlteredCoder@users.noreply.github.com>
2023-02-03 12:10:02 +0100

    rename PAPI base URL (#2033)

--- pkg/types/constants.go
+++ pkg/types/constants.go
@@ -4,7 +4,7 @@ const ApiKeyAuthType = "api-key"
 const TlsAuthType = "tls"
 const PasswordAuthType = "password"
 
-const PAPIBaseURL = "https://papi.crowdsec.net/v1/decisions/stream/poll"
+const PAPIBaseURL = "https://papi.api.crowdsec.net/v1/decisions/stream/poll"
 const CAPIBaseURL = "https://api.crowdsec.net/"
 
 const CscliOrigin = "cscli"
