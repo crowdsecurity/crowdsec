5c9a4e8d2e003e0777dad5c2d6429fc7ade21183
Sebastien Blot <sebastien@crowdsec.net>
2023-02-10 12:01:37 +0100

    update tests

--- pkg/exprhelpers/exprlib_test.go
+++ pkg/exprhelpers/exprlib_test.go
@@ -1066,6 +1066,18 @@ func TestIsIp(t *testing.T) {
 			value:    "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
 			expected: true,
 		},
+		{
+			name:     "IsIPV4() test: invalid IPv4",
+			method:   IsIPV4,
+			value:    "foo.bar",
+			expected: false,
+		},
+		{
+			name:     "IsIPV6() test: invalid IPv6",
+			method:   IsIPV6,
+			value:    "foo.bar",
+			expected: false,
+		},
 	}
 	for _, tc := range tests {
 		tc := tc
