bc20b115aa5e0e81d5ca37ea9546324956bc980b
Marco Mariani <marco@crowdsec.net>
2023-02-13 22:11:27 +0100

    docker tests: relax timeout

--- docker/test/tests/test_simple.py
+++ docker/test/tests/test_simple.py
@@ -10,7 +10,7 @@ pytestmark = pytest.mark.docker
 # XXX this is redundant, already tested in pytest_cs
 def test_crowdsec(crowdsec):
     with crowdsec() as cont:
-        for waiter in log_waiters(cont, timeout=5):
+        for waiter in log_waiters(cont):
             with waiter as matcher:
                 matcher.fnmatch_lines(["*Starting processing data*"])
         res = cont.exec_run('sh -c "echo $CI_TESTING"')
