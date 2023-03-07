c4040ce892484703e228b5d40f72f5f87380c6e5
Marco Mariani <marco@crowdsec.net>
2023-03-07 11:19:44 +0100

    better variable names

--- mk/goversion.mk
+++ mk/goversion.mk
@@ -1,6 +1,6 @@
 
-MINIMUM_MAJOR ?= 1
-MINIMUM_MINOR ?= 20
+BUILD_REQUIRE_GO_MAJOR ?= 1
+BUILD_REQUIRE_GO_MINOR ?= 20
 
 BUILD_GOVERSION = $(subst go,,$(shell go env GOVERSION))
 
@@ -8,21 +8,21 @@ go_major_minor = $(subst ., ,$(BUILD_GOVERSION))
 GO_MAJOR_VERSION = $(word 1, $(go_major_minor))
 GO_MINOR_VERSION = $(word 2, $(go_major_minor))
 
-GO_VERSION_VALIDATION_ERR_MSG = Golang version ($(BUILD_GOVERSION)) is not supported, please use at least $(MINIMUM_MAJOR).$(MINIMUM_MINOR)
+GO_VERSION_VALIDATION_ERR_MSG = Golang version ($(BUILD_GOVERSION)) is not supported, please use at least $(BUILD_REQUIRE_GO_MAJOR).$(BUILD_REQUIRE_GO_MINOR)
 
 .PHONY: goversion
 goversion:
 ifneq ($(OS), Windows_NT)
-	@if [ $(GO_MAJOR_VERSION) -gt $(MINIMUM_MAJOR) ]; then \
+	@if [ $(GO_MAJOR_VERSION) -gt $(BUILD_REQUIRE_GO_MAJOR) ]; then \
 		exit 0; \
-	elif [ $(GO_MAJOR_VERSION) -lt $(MINIMUM_MAJOR) ]; then \
+	elif [ $(GO_MAJOR_VERSION) -lt $(BUILD_REQUIRE_GO_MAJOR) ]; then \
 		echo '$(GO_VERSION_VALIDATION_ERR_MSG)';\
 		exit 1; \
-	elif [ $(GO_MINOR_VERSION) -lt $(MINIMUM_MINOR) ] ; then \
+	elif [ $(GO_MINOR_VERSION) -lt $(BUILD_REQUIRE_GO_MINOR) ] ; then \
 		echo '$(GO_VERSION_VALIDATION_ERR_MSG)';\
 		exit 1; \
 	fi
 else
 	# This needs Set-ExecutionPolicy -Scope CurrentUser Unrestricted
-	@$(CURDIR)/mk/check_go_version.ps1 $(MINIMUM_MAJOR) $(MINIMUM_MINOR)
+	@$(CURDIR)/mk/check_go_version.ps1 $(BUILD_REQUIRE_GO_MAJOR) $(BUILD_REQUIRE_GO_MINOR)
 endif
