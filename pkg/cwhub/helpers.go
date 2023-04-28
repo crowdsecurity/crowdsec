96419b83843d259479d9459c62ee9dcf703adfab
alteredCoder <kevin@crowdsec.net>
2023-04-28 10:02:43 +0200

    Fix chooseHubBranch when latest() doesn't work

--- pkg/cwhub/helpers.go
+++ pkg/cwhub/helpers.go
@@ -16,7 +16,7 @@ import (
 func chooseHubBranch() (string, error) {
 	latest, err := cwversion.Latest()
 	if err != nil {
-		return "master", err
+		return "master", nil
 	}
 
 	csVersion := cwversion.VersionStrip()
