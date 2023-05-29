1c2b34566ef3650dd4f0962d6a36c9340789764e
Sebastien Blot <sebastien@crowdsec.net>
2023-05-29 10:10:22 +0200

    defaut to file polling if reading a file on a network FS

--- pkg/acquisition/modules/file/file.go
+++ pkg/acquisition/modules/file/file.go
@@ -39,9 +39,9 @@ type FileConfiguration struct {
 	Filenames                         []string
 	ExcludeRegexps                    []string `yaml:"exclude_regexps"`
 	Filename                          string
-	ForceInotify                      bool `yaml:"force_inotify"`
-	MaxBufferSize                     int  `yaml:"max_buffer_size"`
-	PollWithoutInotify                bool `yaml:"poll_without_inotify"`
+	ForceInotify                      bool  `yaml:"force_inotify"`
+	MaxBufferSize                     int   `yaml:"max_buffer_size"`
+	PollWithoutInotify                *bool `yaml:"poll_without_inotify"`
 	configuration.DataSourceCommonCfg `yaml:",inline"`
 }
 
@@ -331,7 +331,20 @@ func (f *FileSource) StreamingAcquisition(out chan types.Event, t *tomb.Tomb) er
 			continue
 		}
 
-		tail, err := tail.TailFile(file, tail.Config{ReOpen: true, Follow: true, Poll: f.config.PollWithoutInotify, Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekEnd}, Logger: log.NewEntry(log.StandardLogger())})
+		inotifyPoll := true
+		if f.config.PollWithoutInotify != nil {
+			inotifyPoll = *f.config.PollWithoutInotify
+		} else {
+			fsType, err := types.GetFSType(file)
+			if err != nil {
+				f.logger.Warningf("Could not get fs type for %s : %s", file, err)
+			}
+			if fsType == "nfs" || fsType == "cifs" || fsType == "smb" {
+				inotifyPoll = false
+			}
+		}
+
+		tail, err := tail.TailFile(file, tail.Config{ReOpen: true, Follow: true, Poll: inotifyPoll, Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekEnd}, Logger: log.NewEntry(log.StandardLogger())})
 		if err != nil {
 			f.logger.Errorf("Could not start tailing file %s : %s", file, err)
 			continue
@@ -413,8 +426,22 @@ func (f *FileSource) monitorNewFiles(out chan types.Event, t *tomb.Tomb) error {
 					f.logger.Errorf("unable to close %s : %s", event.Name, err)
 					continue
 				}
+
+				inotifyPoll := true
+				if f.config.PollWithoutInotify != nil {
+					inotifyPoll = *f.config.PollWithoutInotify
+				} else {
+					fsType, err := types.GetFSType(event.Name)
+					if err != nil {
+						f.logger.Warningf("Could not get fs type for %s : %s", event.Name, err)
+					}
+					if fsType == "nfs" || fsType == "cifs" || fsType == "smb" {
+						inotifyPoll = false
+					}
+				}
+
 				//Slightly different parameters for Location, as we want to read the first lines of the newly created file
-				tail, err := tail.TailFile(event.Name, tail.Config{ReOpen: true, Follow: true, Poll: f.config.PollWithoutInotify, Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekStart}})
+				tail, err := tail.TailFile(event.Name, tail.Config{ReOpen: true, Follow: true, Poll: inotifyPoll, Location: &tail.SeekInfo{Offset: 0, Whence: io.SeekStart}})
 				if err != nil {
 					logger.Errorf("Could not start tailing file %s : %s", event.Name, err)
 					break
