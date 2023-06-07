d55812be15c22bbd515f5c2f9d2aee9606d1f696
sabban <github@sabban.eu>
2023-06-07 12:08:27 +0200

    hop

--- pkg/time/rate/rate.go
+++ pkg/time/rate/rate.go
@@ -440,7 +440,7 @@ func (lim *Limiter) advance(t time.Time) (newT time.Time, newTokens float64) {
 	if burst := float64(lim.burst); tokens > burst {
 		tokens = burst
 	}
-	fmt.Printf("time: %s, tokens: %f\n", t.String(), tokens)
+	fmt.Printf("time: %s, tokens: %f, elapsed: %f, delta: %f\n", t.String(), tokens, elapsed.Seconds(), delta)
 	return t, tokens
 }
 
