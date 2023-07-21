3a089d61782a671f60cf20ffc62732e932484c2a
Laurence <laurence.jones@live.co.uk>
2023-07-21 17:01:09 +0100

    Quick fix

--- cmd/crowdsec-cli/dashboard.go
+++ cmd/crowdsec-cli/dashboard.go
@@ -224,7 +224,7 @@ func NewDashboardStartCmd() *cobra.Command {
 				log.Fatalf("Failed to start metabase container : %s", err)
 			}
 			log.Infof("Started metabase")
-			log.Infof("url : http://%s:%s", metabaseListenAddress, metabaseListenPort)
+			log.Infof("url : http://%s:%s", mb.Config.ListenAddr, mb.config.ListenPort)
 		},
 	}
 	return cmdDashStart
