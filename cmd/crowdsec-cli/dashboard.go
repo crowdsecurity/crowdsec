1c55cfe86212cc51769f8a43386ac63037ce30bb
Laurence <laurence.jones@live.co.uk>
2023-07-26 10:48:30 +0100

    Revert "Check if GID is already the group if so no need to chown"
    This reverts commit c7cef1773e38a7eef784da1bce8d35092da7c7c3.

--- cmd/crowdsec-cli/dashboard.go
+++ cmd/crowdsec-cli/dashboard.go
@@ -9,7 +9,6 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"syscall"
 	"unicode"
 
 	"github.com/AlecAivazis/survey/v2"
@@ -401,12 +400,8 @@ func checkGroups(forceYes *bool) (*user.Group, error) {
 	if err != nil {
 		return dockerGroup, fmt.Errorf("unable to convert group ID to int: %s", err)
 	}
-	if stat, err := os.Stat(csConfig.DbConfig.DbPath); err == nil {
-		if uint32(intID) != stat.Sys().(*syscall.Stat_t).Gid {
-			if err := os.Chown(csConfig.DbConfig.DbPath, 0, intID); err != nil {
-				return dockerGroup, fmt.Errorf("unable to chown sqlite db file '%s': %s", csConfig.DbConfig.DbPath, err)
-			}
-		}
+	if err := os.Chown(csConfig.DbConfig.DbPath, 0, intID); err != nil {
+		return dockerGroup, fmt.Errorf("unable to chown sqlite db file '%s': %s", csConfig.DbConfig.DbPath, err)
 	}
 	return dockerGroup, nil
 }
