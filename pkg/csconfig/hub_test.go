9ae8bd79c5dea83b8a54f9310c8ce1286d0e0402
mmetc <92726601+mmetc@users.noreply.github.com>
2023-10-09 11:10:51 +0200

    Refact pkg/csconfig tests (#2526)
    * remove unused method
    * whitespace, redundant comments
    * use test helpers
    * move DumpConsoleConfig() from pkg/csconfig to cscli
    * package doc header
    * var -> const
    * rename ./tests -> ./testdata
    * shorter tests with more error checks
    * lint/formatting
    * use helpers; fix tests that didn't actually test
    * lint; rename expectedResult -> expected

--- pkg/csconfig/hub_test.go
+++ pkg/csconfig/hub_test.go
@@ -1,52 +1,45 @@
 package csconfig
 
 import (
-	"fmt"
 	"path/filepath"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/crowdsecurity/go-cs-lib/cstest"
 )
 
 func TestLoadHub(t *testing.T) {
 	hubFullPath, err := filepath.Abs("./hub")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	dataFullPath, err := filepath.Abs("./data")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
-	configDirFullPath, err := filepath.Abs("./tests")
-	if err != nil {
-		t.Fatal(err)
-	}
+	configDirFullPath, err := filepath.Abs("./testdata")
+	require.NoError(t, err)
 
 	hubIndexFileFullPath, err := filepath.Abs("./hub/.index.json")
-	if err != nil {
-		t.Fatal(err)
-	}
+	require.NoError(t, err)
 
 	tests := []struct {
-		name           string
-		Input          *Config
-		expectedResult *Hub
-		err            string
+		name        string
+		input       *Config
+		expected    *Hub
+		expectedErr string
 	}{
 		{
 			name: "basic valid configuration",
-			Input: &Config{
+			input: &Config{
 				ConfigPaths: &ConfigurationPaths{
-					ConfigDir:    "./tests",
+					ConfigDir:    "./testdata",
 					DataDir:      "./data",
 					HubDir:       "./hub",
 					HubIndexFile: "./hub/.index.json",
 				},
 			},
-			expectedResult: &Hub{
+			expected: &Hub{
 				ConfigDir:    configDirFullPath,
 				DataDir:      dataFullPath,
 				HubDir:       hubFullPath,
@@ -55,40 +48,32 @@ func TestLoadHub(t *testing.T) {
 		},
 		{
 			name: "no data dir",
-			Input: &Config{
+			input: &Config{
 				ConfigPaths: &ConfigurationPaths{
-					ConfigDir:    "./tests",
+					ConfigDir:    "./testdata",
 					HubDir:       "./hub",
 					HubIndexFile: "./hub/.index.json",
 				},
 			},
-			expectedResult: nil,
+			expectedErr: "please provide a data directory with the 'data_dir' directive in the 'config_paths' section",
 		},
 		{
-			name:           "no configuration path",
-			Input:          &Config{},
-			expectedResult: nil,
+			name:        "no configuration path",
+			input:       &Config{},
+			expectedErr: "no configuration paths provided",
 		},
 	}
 
-	for idx, test := range tests {
-		err := test.Input.LoadHub()
-		if err == nil && test.err != "" {
-			fmt.Printf("TEST '%s': NOK\n", test.name)
-			t.Fatalf("%d/%d expected error, didn't get it", idx, len(tests))
-		} else if test.err != "" {
-			if !strings.HasPrefix(fmt.Sprintf("%s", err), test.err) {
-				fmt.Printf("TEST '%s': NOK\n", test.name)
-				t.Fatalf("%d/%d expected '%s' got '%s'", idx, len(tests),
-					test.err,
-					fmt.Sprintf("%s", err))
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.input.LoadHub()
+			cstest.RequireErrorContains(t, err, tc.expectedErr)
+			if tc.expectedErr != "" {
+				return
 			}
-		}
-		isOk := assert.Equal(t, test.expectedResult, test.Input.Hub)
-		if !isOk {
-			t.Fatalf("TEST '%s': NOK", test.name)
-		} else {
-			fmt.Printf("TEST '%s': OK\n", test.name)
-		}
+
+			assert.Equal(t, tc.expected, tc.input.Hub)
+		})
 	}
 }
