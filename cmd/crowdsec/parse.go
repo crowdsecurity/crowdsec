dd4962092293147a7701532cb9883b59c84533bd
bui <thibault@crowdsec.net>
2023-10-24 17:23:29 +0200

    our shortcut for waap events

--- cmd/crowdsec/parse.go
+++ cmd/crowdsec/parse.go
@@ -22,6 +22,13 @@ LOOP:
 			if !event.Process {
 				continue
 			}
+			/*Waap is going to generate 2 events:
+			- one that is treated as a log and can go to scenarios
+			- another one that will go directly to LAPI*/
+			if event.Type == types.WAAP {
+				outputEventChan <- event
+				continue
+			}
 			if event.Line.Module == "" {
 				log.Errorf("empty event.Line.Module field, the acquisition module must set it ! : %+v", event.Line)
 				continue
