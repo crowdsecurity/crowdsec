0c030a3bb559518be75e842447c4f7b962e01dc5
Sebastien Blot <sebastien@crowdsec.net>
2023-12-05 16:49:34 +0100

    use fmt.Printf to make it more readable

--- pkg/hubtest/hubtest_item.go
+++ pkg/hubtest/hubtest_item.go
@@ -617,7 +617,7 @@ func (t *HubTestItem) RunWithNucleiTemplate() error {
 				log.Errorf("unable to read crowdsec log file '%s': %s", crowdsecLogFile, err)
 			} else {
 				log.Errorf("crowdsec log file '%s'", crowdsecLogFile)
-				log.Errorf("%s", string(crowdsecLog))
+				fmt.Printf("%s\n", string(crowdsecLog))
 			}
 		}
 	} else {
@@ -631,7 +631,7 @@ func (t *HubTestItem) RunWithNucleiTemplate() error {
 				log.Errorf("unable to read crowdsec log file '%s': %s", crowdsecLogFile, err)
 			} else {
 				log.Errorf("crowdsec log file '%s'", crowdsecLogFile)
-				log.Errorf("%s", string(crowdsecLog))
+				fmt.Printf("%s\n", string(crowdsecLog))
 			}
 		}
 	}
