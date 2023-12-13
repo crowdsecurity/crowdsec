5117247766b10aae69a7366cdda32babf7af5ccc
Marco Mariani <marco@crowdsec.net>
2023-12-13 13:53:25 +0100

    bump yaml.v3

--- cmd/crowdsec-cli/machines.go
+++ cmd/crowdsec-cli/machines.go
@@ -18,7 +18,7 @@ import (
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
-	"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v3"
 
 	"github.com/crowdsecurity/machineid"
 
