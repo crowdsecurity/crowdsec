ee6f9d70fc917737010f3d5763cb9d7ab660a9cb
marco <marco@crowdsec.net>
2024-01-16 10:27:39 +0100

    match -> Match

--- pkg/appsec/appsec_rule/modsec_rule_test.go
+++ pkg/appsec/appsec_rule/modsec_rule_test.go
@@ -13,7 +13,7 @@ func TestVPatchRuleString(t *testing.T) {
 			rule: CustomRule{
 				Zones:     []string{"ARGS"},
 				Variables: []string{"foo"},
-				Match:     match{Type: "eq", Value: "1"},
+				Match:     Match{Type: "eq", Value: "1"},
 				Transform: []string{"count"},
 			},
 			expected: `SecRule &ARGS_GET:foo "@eq 1" "id:853070236,phase:2,deny,log,msg:'Collection count',tag:'crowdsec-Collection count'"`,
