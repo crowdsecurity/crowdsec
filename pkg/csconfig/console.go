19d36c0fb286af30bf4aa7310677ff8d0aec0369
Thibault "bui" Koechlin <thibault@crowdsec.net>
2024-01-19 15:49:00 +0100

    Support `console options` in `console enroll` (#2760)
    * make dev.yaml has a valid/default console path
    
    * simplify and make more consistent help message about console opts
    
    * allow enroll to specify options to enable
    
    * allow 'all' shortcut for --enable

--- pkg/csconfig/console.go
+++ pkg/csconfig/console.go
@@ -19,6 +19,13 @@ const (
 )
 
 var CONSOLE_CONFIGS = []string{SEND_CUSTOM_SCENARIOS, SEND_MANUAL_SCENARIOS, SEND_TAINTED_SCENARIOS, SEND_CONTEXT, CONSOLE_MANAGEMENT}
+var CONSOLE_CONFIGS_HELP = map[string]string{
+	SEND_CUSTOM_SCENARIOS:  "Forward alerts from custom scenarios to the console",
+	SEND_MANUAL_SCENARIOS:  "Forward manual decisions to the console",
+	SEND_TAINTED_SCENARIOS: "Forward alerts from tainted scenarios to the console",
+	SEND_CONTEXT:           "Forward context with alerts to the console",
+	CONSOLE_MANAGEMENT:     "Receive decisions from console",
+}
 
 var DefaultConsoleConfigFilePath = DefaultConfigPath("console.yaml")
 
