955a2a86553c28fcbf82ab425ccf12a4314876df
marco <marco@crowdsec.net>
2024-03-08 09:37:34 +0100

    pkg/cwhub: download data assets to a temporary file to avoid partial fetch

--- pkg/cwhub/dataset.go
+++ pkg/cwhub/dataset.go
@@ -1,6 +1,7 @@
 package cwhub
 
 import (
+	"path/filepath"
 	"errors"
 	"fmt"
 	"io"
@@ -31,19 +32,32 @@ func downloadFile(url string, destPath string) error {
 		return fmt.Errorf("bad http code %d for %s", resp.StatusCode, url)
 	}
 
-	file, err := os.Create(destPath)
+	tmpFile, err := os.CreateTemp(filepath.Dir(destPath), filepath.Base(destPath) + ".*.tmp")
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+
+	tmpFileName := tmpFile.Name()
+	defer func() {
+		tmpFile.Close()
+		os.Remove(tmpFileName)
+	}()
 
 	// avoid reading the whole file in memory
-	_, err = io.Copy(file, resp.Body)
+	_, err = io.Copy(tmpFile, resp.Body)
 	if err != nil {
 		return err
 	}
 
-	if err = file.Sync(); err != nil {
+	if err = tmpFile.Sync(); err != nil {
+		return err
+	}
+
+	if err = tmpFile.Close(); err != nil {
+		return err
+	}
+
+	if err = os.Rename(tmpFileName, destPath); err != nil {
 		return err
 	}
 
