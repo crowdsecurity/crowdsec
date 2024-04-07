434e18c42c2055fe71f11bc8bd3d99c24ace8db7
Laurence <laurence.jones@live.co.uk>
2024-04-07 14:30:49 +0100

    wip: the plugin should not handle it own data queue since the plugin process may timeout, so instead have a function that uses said context and loop whilst locking the filewriter this may not be the best way :shrug:, however, I dont want multiple notifications to attempt to reopen the file if it has been rotated outside of the plugin context

--- cmd/notification-file/main.go
+++ cmd/notification-file/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 	"sync"
-	"time"
 
 	"github.com/crowdsecurity/crowdsec/pkg/protobufs"
 	"github.com/hashicorp/go-hclog"
@@ -13,13 +12,16 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+var (
+	FileWriter     *os.File
+	FileWriteMutex *sync.Mutex
+)
+
 type PluginConfig struct {
-	Name       string      `yaml:"name"`
-	LogLevel   string      `yaml:"log_level"`
-	LogPath    string      `yaml:"log_path"`
-	WriteChan  chan string `yaml:"-"`
-	FileWriter *os.File    `yaml:"-"`
-	LogRotate  LogRotate   `yaml:"rotate"`
+	Name      string    `yaml:"name"`
+	LogLevel  string    `yaml:"log_level"`
+	LogPath   string    `yaml:"log_path"`
+	LogRotate LogRotate `yaml:"rotate"`
 }
 
 type LogRotate struct {
@@ -41,81 +43,34 @@ var logger hclog.Logger = hclog.New(&hclog.LoggerOptions{
 	JSONFormat: true,
 })
 
-func Monit(cfg PluginConfig) {
-	logger.Trace("Starting monit process")
-	queue := make([]string, 0)
-	queueMutex := &sync.Mutex{}
-	ticker := time.NewTicker(2 * time.Second)
-	if cfg.FileWriter == nil {
-		cfg.FileWriter, _ = os.OpenFile(cfg.LogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	}
+func WriteToFileWithCtx(ctx context.Context, cfg PluginConfig, log string) error {
 	for {
 		select {
-		case <-ticker.C:
-			logger.Debug("Checking queue")
-			if len(queue) == 0 {
-				logger.Debug("Queue is empty")
+		case <-ctx.Done():
+			logger.Error("Context is cancelled")
+			return nil
+		default:
+			if !FileWriteMutex.TryLock() {
 				continue
 			}
-			logger.Debug(fmt.Sprintf("Writing %d logs to file", len(queue)))
-			newQueue := make([]string, 0, len(queue))
-			originalFileInfo, err := cfg.FileWriter.Stat()
+			defer FileWriteMutex.Unlock()
+			originalFileInfo, err := FileWriter.Stat()
 			if err != nil {
 				logger.Error("Failed to get file info", "error", err)
 			}
-			for _, log := range queue {
-				var err error
-				currentFileInfo, _ := os.Stat(cfg.LogPath)
-				// Check if the file writer is still pointing to the same file
-				if !os.SameFile(originalFileInfo, currentFileInfo) {
-					// The file has been rotated
-					logger.Info("Log file has been rotated or missing attempting to reopen it")
-					cfg.FileWriter.Close()
-					cfg.FileWriter, err = os.OpenFile(cfg.LogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-					if err != nil {
-						logger.Error("Failed to reopen log file", "error", err)
-						newQueue = append(newQueue, log)
-						continue
-					}
-					logger.Info("Log file has been reopened successfully")
-					originalFileInfo, _ = cfg.FileWriter.Stat()
-				}
-				_, err = cfg.FileWriter.WriteString(log)
+			currentFileInfo, _ := os.Stat(cfg.LogPath)
+			if !os.SameFile(originalFileInfo, currentFileInfo) {
+				// The file has been rotated
+				logger.Info("Log file has been rotated or missing attempting to reopen it")
+				FileWriter.Close()
+				FileWriter, err = os.OpenFile(cfg.LogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 				if err != nil {
-					logger.Error("Failed to write log", "error", err)
-					newQueue = append(newQueue, log)
+					return err
 				}
+				logger.Info("Log file has been reopened successfully")
 			}
-			cfg.FileWriter.Sync()
-			queueMutex.Lock()
-			if len(newQueue) > 0 {
-				queue = newQueue
-			} else {
-				queue = make([]string, 0)
-			}
-			queueMutex.Unlock()
-			// TODO! Implement log rotation
-			// if cfg.LogRotate.Enabled {
-			// 	// check if file size is greater than max size
-			// 	fileInfo, _ := cfg.FileWriter.Stat()
-			// 	if fileInfo.Size() > int64(cfg.LogRotate.MaxSize) {
-			// 		// close file
-			// 		cfg.FileWriter.Close()
-			// 		// rename file
-			// 		os.Rename(cfg.LogPath, cfg.LogPath+".1")
-			// 		// open file
-			// 		cfg.FileWriter, _ = os.OpenFile(cfg.LogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			// 		// compress file
-			// 		if cfg.LogRotate.Compress {
-			// 			// compress file
-			// 		}
-			// 	}
-			// }
-		case log := <-cfg.WriteChan:
-			logger.Trace("Received log", log)
-			queueMutex.Lock()
-			queue = append(queue, log)
-			queueMutex.Unlock()
+			_, err = FileWriter.WriteString(log)
+			return err
 		}
 	}
 }
@@ -126,18 +81,18 @@ func (s *FilePlugin) Notify(ctx context.Context, notification *protobufs.Notific
 	}
 	cfg := s.PluginConfigByName[notification.Name]
 
-	cfg.WriteChan <- notification.Text
-
-	return &protobufs.Empty{}, nil
+	return &protobufs.Empty{}, WriteToFileWithCtx(ctx, cfg, notification.Text)
 }
 
 func (s *FilePlugin) Configure(ctx context.Context, config *protobufs.Config) (*protobufs.Empty, error) {
 	d := PluginConfig{}
 	err := yaml.Unmarshal(config.Config, &d)
-	d.WriteChan = make(chan string)
-	s.PluginConfigByName[d.Name] = d
-	logger.SetLevel(hclog.LevelFromString(d.LogLevel))
-	go Monit(d)
+	if err == nil {
+		FileWriteMutex = &sync.Mutex{}
+		FileWriter, err = os.OpenFile(d.LogPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		s.PluginConfigByName[d.Name] = d
+		logger.SetLevel(hclog.LevelFromString(d.LogLevel))
+	}
 	return &protobufs.Empty{}, err
 }
 
