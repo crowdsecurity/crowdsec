6e93d41055bbb14cdd50a980ffeb872cf97b534b
marco <marco@crowdsec.net>
2024-06-13 11:39:30 +0200

    ling: gocritic (badRegexp, regexpSimplify)

--- pkg/hubtest/regexp.go
+++ pkg/hubtest/regexp.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	variableRE = regexp.MustCompile(`(?P<variable>[^  =]+) == .*`)
+	variableRE = regexp.MustCompile(`(?P<variable>[^ =]+) == .*`)
 	parserResultRE = regexp.MustCompile(`^results\["[^"]+"\]\["(?P<parser>[^"]+)"\]\[[0-9]+\]\.Evt\..*`)
 	scenarioResultRE = regexp.MustCompile(`^results\[[0-9]+\].Overflow.Alert.GetScenario\(\) == "(?P<scenario>[^"]+)"`)
 )
