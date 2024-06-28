2ecce7c65bde7194b407f4451a773ffa86514e3e
marco <marco@crowdsec.net>
2024-06-28 14:59:07 +0200

    lint: replace "github.com/pkg/errors" in apiserver

--- pkg/apiserver/controllers/v1/errors.go
+++ pkg/apiserver/controllers/v1/errors.go
@@ -1,35 +1,35 @@
 package v1
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 
 	"github.com/crowdsecurity/crowdsec/pkg/database"
 )
 
 func (c *Controller) HandleDBErrors(gctx *gin.Context, err error) {
-	switch errors.Cause(err) {
-	case database.ItemNotFound:
+	switch {
+	case errors.Is(err, database.ItemNotFound):
 		gctx.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
 		return
-	case database.UserExists:
+	case errors.Is(err, database.UserExists):
 		gctx.JSON(http.StatusForbidden, gin.H{"message": err.Error()})
 		return
-	case database.HashError:
+	case errors.Is(err, database.HashError):
 		gctx.JSON(http.StatusBadRequest, gin.H{"message": err.Error()})
 		return
-	case database.InsertFail:
+	case errors.Is(err, database.InsertFail):
 		gctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
-	case database.QueryFail:
+	case errors.Is(err, database.QueryFail):
 		gctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
-	case database.ParseTimeFail:
+	case errors.Is(err, database.ParseTimeFail):
 		gctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
-	case database.ParseDurationFail:
+	case errors.Is(err, database.ParseDurationFail):
 		gctx.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 		return
 	default:
