5357466f5e303b00c3f9834cfef0423e2c5f4102
marco <marco@crowdsec.net>
2024-07-04 15:22:36 +0200

    extract the remaining types

---
+++ cmd/crowdsec-cli/metrics/statstash.go
@@ -0,0 +1,56 @@
+package metrics
+
+import (
+	"io"
+	"strconv"
+
+	"github.com/jedib0t/go-pretty/v6/text"
+
+	"github.com/crowdsecurity/go-cs-lib/maptools"
+
+	"github.com/crowdsecurity/crowdsec/cmd/crowdsec-cli/cstable"
+)
+
+func (s statStash) Description() (string, string) {
+	return "Parser Stash Metrics",
+		`Tracks the status of stashes that might be created by various parsers and scenarios.`
+}
+
+func (s statStash) Process(name, mtype string, val int) {
+	s[name] = struct {
+		Type  string
+		Count int
+	}{
+		Type:  mtype,
+		Count: val,
+	}
+}
+
+func (s statStash) Table(out io.Writer, wantColor string, noUnit bool, showEmpty bool) {
+	t := cstable.New(out, wantColor)
+	t.SetRowLines(false)
+	t.SetHeaders("Name", "Type", "Items")
+	t.SetAlignment(text.AlignLeft, text.AlignLeft, text.AlignLeft)
+
+	// unfortunately, we can't reuse metricsToTable as the structure is too different :/
+	numRows := 0
+
+	for _, alabel := range maptools.SortedKeys(s) {
+		astats := s[alabel]
+
+		row := []string{
+			alabel,
+			astats.Type,
+			strconv.Itoa(astats.Count),
+		}
+		t.AddRow(row...)
+
+		numRows++
+	}
+
+	if numRows > 0 || showEmpty {
+		title, _ := s.Description()
+		cstable.RenderTitle(out, "\n"+title+":")
+		t.Render()
+	}
+}
