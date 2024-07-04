2d76528db52a130fdeb154f3b7c211c46f584a82
marco <marco@crowdsec.net>
2024-07-04 15:14:19 +0200

    extract statappsecrule.go

--- cmd/crowdsec-cli/metrics/metrics_table.go
+++ cmd/crowdsec-cli/metrics/metrics_table.go
@@ -2,7 +2,6 @@ package metrics
 
 import (
 	"errors"
-	"fmt"
 	"io"
 	"sort"
 	"strconv"
@@ -130,41 +129,6 @@ func metricsToTable(t *cstable.Table, stats map[string]map[string]int, keys []st
 	}
 
 	return numRows, nil
-}
-
-func (s statAppsecRule) Description() (string, string) {
-	return "Appsec Rule Metrics",
-		`Provides “per AppSec Component” information about the number of matches for loaded AppSec Rules.`
-}
-
-func (s statAppsecRule) Process(appsecEngine, appsecRule string, metric string, val int) {
-	if _, ok := s[appsecEngine]; !ok {
-		s[appsecEngine] = make(map[string]map[string]int)
-	}
-
-	if _, ok := s[appsecEngine][appsecRule]; !ok {
-		s[appsecEngine][appsecRule] = make(map[string]int)
-	}
-
-	s[appsecEngine][appsecRule][metric] += val
-}
-
-func (s statAppsecRule) Table(out io.Writer, wantColor string, noUnit bool, showEmpty bool) {
-	for appsecEngine, appsecEngineRulesStats := range s {
-		t := cstable.New(out, wantColor)
-		t.SetRowLines(false)
-		t.SetHeaders("Rule ID", "Triggered")
-		t.SetAlignment(text.AlignLeft, text.AlignLeft)
-
-		keys := []string{"triggered"}
-
-		if numRows, err := metricsToTable(t, appsecEngineRulesStats, keys, noUnit); err != nil {
-			log.Warningf("while collecting appsec rules stats: %s", err)
-		} else if numRows > 0 || showEmpty {
-			cstable.RenderTitle(out, fmt.Sprintf("\nAppsec '%s' Rules Metrics:", appsecEngine))
-			t.Render()
-		}
-	}
 }
 
 func (s statWhitelist) Description() (string, string) {
