4cd0c84406dd99019f5bd93e12fee5e8ea00926a
marco <marco@crowdsec.net>
2024-07-04 15:08:08 +0200

    extract statbucket.go

--- cmd/crowdsec-cli/metrics/metrics_table.go
+++ cmd/crowdsec-cli/metrics/metrics_table.go
@@ -132,37 +132,6 @@ func metricsToTable(t *cstable.Table, stats map[string]map[string]int, keys []st
 	return numRows, nil
 }
 
-func (s statBucket) Description() (string, string) {
-	return "Scenario Metrics",
-		`Measure events in different scenarios. Current count is the number of buckets during metrics collection. ` +
-			`Overflows are past event-producing buckets, while Expired are the ones that didn’t receive enough events to Overflow.`
-}
-
-func (s statBucket) Process(bucket, metric string, val int) {
-	if _, ok := s[bucket]; !ok {
-		s[bucket] = make(map[string]int)
-	}
-
-	s[bucket][metric] += val
-}
-
-func (s statBucket) Table(out io.Writer, wantColor string, noUnit bool, showEmpty bool) {
-	t := cstable.New(out, wantColor)
-	t.SetRowLines(false)
-	t.SetHeaders("Scenario", "Current Count", "Overflows", "Instantiated", "Poured", "Expired")
-	t.SetAlignment(text.AlignLeft, text.AlignLeft, text.AlignLeft, text.AlignLeft, text.AlignLeft, text.AlignLeft)
-
-	keys := []string{"curr_count", "overflow", "instantiation", "pour", "underflow"}
-
-	if numRows, err := metricsToTable(t, s, keys, noUnit); err != nil {
-		log.Warningf("while collecting scenario stats: %s", err)
-	} else if numRows > 0 || showEmpty {
-		title, _ := s.Description()
-		cstable.RenderTitle(out, "\n"+title+":")
-		t.Render()
-	}
-}
-
 func (s statAcquis) Description() (string, string) {
 	return "Acquisition Metrics",
 		`Measures the lines read, parsed, and unparsed per datasource. ` +
