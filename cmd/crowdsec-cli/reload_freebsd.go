83cc0cfae1af76a2b3f07cb20f2163b47b497d69
marco <marco@crowdsec.net>
2024-07-24 14:47:23 +0200

    cscli refact: package 'cliconsole'

---
+++ cmd/crowdsec-cli/reload_freebsd.go
@@ -0,0 +1,4 @@
+package main
+
+// actually sudo is not that popular on freebsd, but this will do
+const reloadMessage = "Run 'sudo service crowdsec reload' for the new configuration to be effective."
