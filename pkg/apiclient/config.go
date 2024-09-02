d2616766deba0cf9c4a67c7e634b1226db86f2ed
blotus <sebastien@crowdsec.net>
2024-09-02 13:13:40 +0200

    Allow auto registration of machines in LAPI (#3202)
    Co-authored-by: marco <marco@crowdsec.net>

--- pkg/apiclient/config.go
+++ pkg/apiclient/config.go
@@ -7,12 +7,13 @@ import (
 )
 
 type Config struct {
-	MachineID      string
-	Password       strfmt.Password
-	Scenarios      []string
-	URL            *url.URL
-	PapiURL        *url.URL
-	VersionPrefix  string
-	UserAgent      string
-	UpdateScenario func() ([]string, error)
+	MachineID         string
+	Password          strfmt.Password
+	Scenarios         []string
+	URL               *url.URL
+	PapiURL           *url.URL
+	VersionPrefix     string
+	UserAgent         string
+	RegistrationToken string
+	UpdateScenario    func() ([]string, error)
 }
