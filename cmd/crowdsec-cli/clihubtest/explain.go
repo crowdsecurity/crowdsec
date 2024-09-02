5fe2f10deaa3d655686aa3caf8644e845001621d
marco <marco@crowdsec.net>
2024-09-02 14:46:24 +0200

    enhance: add additional explain options to hubtest

--- cmd/crowdsec-cli/clihubtest/explain.go
+++ cmd/crowdsec-cli/clihubtest/explain.go
@@ -8,7 +8,52 @@ import (
 	"github.com/crowdsecurity/crowdsec/pkg/dumps"
 )
 
+
+func (cli *cliHubTest) explain(testName string, details bool, skipOk bool) error {
+	test, err := HubTest.LoadTestItem(testName)
+	if err != nil {
+		return fmt.Errorf("can't load test: %+v", err)
+	}
+
+	err = test.ParserAssert.LoadTest(test.ParserResultFile)
+	if err != nil {
+		if err = test.Run(); err != nil {
+			return fmt.Errorf("running test '%s' failed: %+v", test.Name, err)
+		}
+
+		if err = test.ParserAssert.LoadTest(test.ParserResultFile); err != nil {
+			return fmt.Errorf("unable to load parser result after run: %w", err)
+		}
+	}
+
+	err = test.ScenarioAssert.LoadTest(test.ScenarioResultFile, test.BucketPourResultFile)
+	if err != nil {
+		if err = test.Run(); err != nil {
+			return fmt.Errorf("running test '%s' failed: %+v", test.Name, err)
+		}
+
+		if err = test.ScenarioAssert.LoadTest(test.ScenarioResultFile, test.BucketPourResultFile); err != nil {
+			return fmt.Errorf("unable to load scenario result after run: %w", err)
+		}
+	}
+
+	opts := dumps.DumpOpts{
+		Details: details,
+		SkipOk: skipOk,
+	}
+
+	dumps.DumpTree(*test.ParserAssert.TestData, *test.ScenarioAssert.PourData, opts)
+
+	return nil
+}
+
+
 func (cli *cliHubTest) NewExplainCmd() *cobra.Command {
+	var (
+		details bool
+		skipOk  bool
+	)
+
 	cmd := &cobra.Command{
 		Use:               "explain",
 		Short:             "explain [test_name]",
@@ -16,38 +61,18 @@ func (cli *cliHubTest) NewExplainCmd() *cobra.Command {
 		DisableAutoGenTag: true,
 		RunE: func(_ *cobra.Command, args []string) error {
 			for _, testName := range args {
-				test, err := HubTest.LoadTestItem(testName)
-				if err != nil {
-					return fmt.Errorf("can't load test: %+v", err)
+				if err := cli.explain(testName, details, skipOk); err != nil {
+					return err
 				}
-				err = test.ParserAssert.LoadTest(test.ParserResultFile)
-				if err != nil {
-					if err = test.Run(); err != nil {
-						return fmt.Errorf("running test '%s' failed: %+v", test.Name, err)
-					}
-
-					if err = test.ParserAssert.LoadTest(test.ParserResultFile); err != nil {
-						return fmt.Errorf("unable to load parser result after run: %w", err)
-					}
-				}
-
-				err = test.ScenarioAssert.LoadTest(test.ScenarioResultFile, test.BucketPourResultFile)
-				if err != nil {
-					if err = test.Run(); err != nil {
-						return fmt.Errorf("running test '%s' failed: %+v", test.Name, err)
-					}
-
-					if err = test.ScenarioAssert.LoadTest(test.ScenarioResultFile, test.BucketPourResultFile); err != nil {
-						return fmt.Errorf("unable to load scenario result after run: %w", err)
-					}
-				}
-				opts := dumps.DumpOpts{}
-				dumps.DumpTree(*test.ParserAssert.TestData, *test.ScenarioAssert.PourData, opts)
 			}
 
 			return nil
 		},
 	}
+
+	flags := cmd.Flags()
+	flags.BoolVarP(&details, "verbose", "v", false, "Display individual changes")
+	flags.BoolVar(&skipOk, "failures", false, "Only show failed lines")
 
 	return cmd
 }
