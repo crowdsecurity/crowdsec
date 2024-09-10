1bee691c7ebfbd2aef6959c8a52843709bc3f228
marco <marco@crowdsec.net>
2024-09-10 15:49:59 +0200

    cscli papi: extract methods status(), sync()

--- cmd/crowdsec-cli/clipapi/papi.go
+++ cmd/crowdsec-cli/clipapi/papi.go
@@ -1,10 +1,13 @@
 package clipapi
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"time"
 
 	log "github.com/sirupsen/logrus"
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"gopkg.in/tomb.v2"
 
@@ -46,104 +49,110 @@ func (cli *cliPapi) NewCommand() *cobra.Command {
 		},
 	}
 
-	cmd.AddCommand(cli.NewStatusCmd())
-	cmd.AddCommand(cli.NewSyncCmd())
+	cmd.AddCommand(cli.newStatusCmd())
+	cmd.AddCommand(cli.newSyncCmd())
 
 	return cmd
 }
 
-func (cli *cliPapi) NewStatusCmd() *cobra.Command {
+func (cli *cliPapi) status(ctx context.Context, out io.Writer) error {
+	cfg := cli.cfg()
+	db, err := require.DBClient(ctx, cfg.DbConfig)
+	if err != nil {
+		return err
+	}
+
+	apic, err := apiserver.NewAPIC(cfg.API.Server.OnlineClient, db, cfg.API.Server.ConsoleConfig, cfg.API.Server.CapiWhitelists)
+	if err != nil {
+		return fmt.Errorf("unable to initialize API client: %w", err)
+	}
+
+	papi, err := apiserver.NewPAPI(apic, db, cfg.API.Server.ConsoleConfig, log.GetLevel())
+	if err != nil {
+		return fmt.Errorf("unable to initialize PAPI client: %w", err)
+	}
+
+	perms, err := papi.GetPermissions()
+	if err != nil {
+		return fmt.Errorf("unable to get PAPI permissions: %w", err)
+	}
+	var lastTimestampStr *string
+	lastTimestampStr, err = db.GetConfigItem(apiserver.PapiPullKey)
+	if err != nil {
+		lastTimestampStr = ptr.Of("never")
+	}
+	log.Infof("You can successfully interact with Polling API (PAPI)")
+	log.Infof("Console plan: %s", perms.Plan)
+	log.Infof("Last order received: %s", *lastTimestampStr)
+
+	log.Infof("PAPI subscriptions:")
+	for _, sub := range perms.Categories {
+		log.Infof(" - %s", sub)
+	}
+
+	return nil
+}
+
+func (cli *cliPapi) newStatusCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "status",
 		Short:             "Get status of the Polling API",
 		Args:              cobra.MinimumNArgs(0),
 		DisableAutoGenTag: true,
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			var err error
-			cfg := cli.cfg()
-			db, err := require.DBClient(cmd.Context(), cfg.DbConfig)
-			if err != nil {
-				return err
-			}
-
-			apic, err := apiserver.NewAPIC(cfg.API.Server.OnlineClient, db, cfg.API.Server.ConsoleConfig, cfg.API.Server.CapiWhitelists)
-			if err != nil {
-				return fmt.Errorf("unable to initialize API client: %w", err)
-			}
-
-			papi, err := apiserver.NewPAPI(apic, db, cfg.API.Server.ConsoleConfig, log.GetLevel())
-			if err != nil {
-				return fmt.Errorf("unable to initialize PAPI client: %w", err)
-			}
-
-			perms, err := papi.GetPermissions()
-			if err != nil {
-				return fmt.Errorf("unable to get PAPI permissions: %w", err)
-			}
-			var lastTimestampStr *string
-			lastTimestampStr, err = db.GetConfigItem(apiserver.PapiPullKey)
-			if err != nil {
-				lastTimestampStr = ptr.Of("never")
-			}
-			log.Infof("You can successfully interact with Polling API (PAPI)")
-			log.Infof("Console plan: %s", perms.Plan)
-			log.Infof("Last order received: %s", *lastTimestampStr)
-
-			log.Infof("PAPI subscriptions:")
-			for _, sub := range perms.Categories {
-				log.Infof(" - %s", sub)
-			}
-
-			return nil
+			return cli.status(cmd.Context(), color.Output)
 		},
 	}
 
 	return cmd
 }
 
-func (cli *cliPapi) NewSyncCmd() *cobra.Command {
+func (cli *cliPapi) sync(ctx context.Context, out io.Writer) error {
+	cfg := cli.cfg()
+	t := tomb.Tomb{}
+
+	db, err := require.DBClient(ctx, cfg.DbConfig)
+	if err != nil {
+		return err
+	}
+
+	apic, err := apiserver.NewAPIC(cfg.API.Server.OnlineClient, db, cfg.API.Server.ConsoleConfig, cfg.API.Server.CapiWhitelists)
+	if err != nil {
+		return fmt.Errorf("unable to initialize API client: %w", err)
+	}
+
+	t.Go(apic.Push)
+
+	papi, err := apiserver.NewPAPI(apic, db, cfg.API.Server.ConsoleConfig, log.GetLevel())
+	if err != nil {
+		return fmt.Errorf("unable to initialize PAPI client: %w", err)
+	}
+
+	t.Go(papi.SyncDecisions)
+
+	err = papi.PullOnce(time.Time{}, true)
+	if err != nil {
+		return fmt.Errorf("unable to sync decisions: %w", err)
+	}
+
+	log.Infof("Sending acknowledgements to CAPI")
+
+	apic.Shutdown()
+	papi.Shutdown()
+	t.Wait()
+	time.Sleep(5 * time.Second) // FIXME: the push done by apic.Push is run inside a sub goroutine, sleep to make sure it's done
+
+	return nil
+}
+
+func (cli *cliPapi) newSyncCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "sync",
 		Short:             "Sync with the Polling API, pulling all non-expired orders for the instance",
 		Args:              cobra.MinimumNArgs(0),
 		DisableAutoGenTag: true,
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			var err error
-			cfg := cli.cfg()
-			t := tomb.Tomb{}
-
-			db, err := require.DBClient(cmd.Context(), cfg.DbConfig)
-			if err != nil {
-				return err
-			}
-
-			apic, err := apiserver.NewAPIC(cfg.API.Server.OnlineClient, db, cfg.API.Server.ConsoleConfig, cfg.API.Server.CapiWhitelists)
-			if err != nil {
-				return fmt.Errorf("unable to initialize API client: %w", err)
-			}
-
-			t.Go(apic.Push)
-
-			papi, err := apiserver.NewPAPI(apic, db, cfg.API.Server.ConsoleConfig, log.GetLevel())
-			if err != nil {
-				return fmt.Errorf("unable to initialize PAPI client: %w", err)
-			}
-
-			t.Go(papi.SyncDecisions)
-
-			err = papi.PullOnce(time.Time{}, true)
-			if err != nil {
-				return fmt.Errorf("unable to sync decisions: %w", err)
-			}
-
-			log.Infof("Sending acknowledgements to CAPI")
-
-			apic.Shutdown()
-			papi.Shutdown()
-			t.Wait()
-			time.Sleep(5 * time.Second) // FIXME: the push done by apic.Push is run inside a sub goroutine, sleep to make sure it's done
-
-			return nil
+			return cli.sync(cmd.Context(), color.Output)
 		},
 	}
 
