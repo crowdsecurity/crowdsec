3a002f25ba74ebfdb1d60b413082ec81d2959d0f
marco <marco@crowdsec.net>
2024-09-12 16:37:00 +0200

    lint

--- pkg/leakybucket/trigger.go
+++ pkg/leakybucket/trigger.go
@@ -16,22 +16,28 @@ func (t *Trigger) OnBucketPour(b *BucketFactory) func(types.Event, *Leaky) *type
 	// Pour makes the bucket overflow all the time
 	// TriggerPour unconditionally overflows
 	return func(msg types.Event, l *Leaky) *types.Event {
+		now := time.Now().UTC()
+
 		if l.Mode == types.TIMEMACHINE {
 			var d time.Time
+
 			err := d.UnmarshalText([]byte(msg.MarshaledTime))
 			if err != nil {
 				log.Warningf("Failed unmarshaling event time (%s) : %v", msg.MarshaledTime, err)
-				d = time.Now().UTC()
+
+				d = now
 			}
+
 			l.logger.Debugf("yay timemachine overflow time : %s --> %s", d, msg.MarshaledTime)
 			l.Last_ts = d
 			l.First_ts = d
 			l.Ovflw_ts = d
 		} else {
-			l.Last_ts = time.Now().UTC()
-			l.First_ts = time.Now().UTC()
-			l.Ovflw_ts = time.Now().UTC()
+			l.Last_ts = now
+			l.First_ts = now
+			l.Ovflw_ts = now
 		}
+
 		l.Total_count = 1
 
 		l.logger.Debug("Bucket overflow")
