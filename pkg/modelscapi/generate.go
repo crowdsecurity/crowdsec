70881df3f41f4da0a90d97b3dee566bcc73b3724
marco <marco@crowdsec.net>
2024-09-24 16:16:13 +0200

    re-generate pkg/modelscapi

--- pkg/modelscapi/generate.go
+++ pkg/modelscapi/generate.go
@@ -1,4 +1,4 @@
 package modelscapi
 
-//go:generate go run -mod=mod github.com/go-swagger/go-swagger/cmd/swagger@v0.30.5 generate model --spec=./centralapi_swagger.yaml --target=../
+//go:generate go run -mod=mod github.com/go-swagger/go-swagger/cmd/swagger@v0.30.5 generate model --spec=./centralapi_swagger.yaml --target=../ --model-package=modelscapi
 
