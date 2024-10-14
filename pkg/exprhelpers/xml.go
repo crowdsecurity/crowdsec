2c272c3c6101d6e210d643cc7ddd96ac78fcd93a
blotus <sebastien@crowdsec.net>
2024-10-14 17:41:09 +0200

    refactor redundant code

--- pkg/exprhelpers/xml.go
+++ pkg/exprhelpers/xml.go
@@ -11,9 +11,53 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-var pathCache = make(map[string]etree.Path)
-var rwMutex = sync.RWMutex{}
-var xmlDocumentCache gcache.Cache
+var (
+	pathCache        = make(map[string]etree.Path)
+	rwMutex          = sync.RWMutex{}
+	xmlDocumentCache gcache.Cache
+)
+
+func compileOrGetPath(path string) (etree.Path, error) {
+	rwMutex.RLock()
+	compiledPath, ok := pathCache[path]
+	rwMutex.RUnlock()
+
+	if !ok {
+		var err error
+		compiledPath, err = etree.CompilePath(path)
+		if err != nil {
+			return etree.Path{}, err
+		}
+
+		rwMutex.Lock()
+		pathCache[path] = compiledPath
+		rwMutex.Unlock()
+	}
+
+	return compiledPath, nil
+}
+
+func getXMLDocumentFromCache(xmlString string) (*etree.Document, error) {
+	cacheKey := xxhash.Sum64String(xmlString)
+	cacheObj, err := xmlDocumentCache.Get(cacheKey)
+
+	if err != nil && !errors.Is(err, gcache.KeyNotFoundError) {
+		return nil, err
+	}
+
+	doc, ok := cacheObj.(*etree.Document)
+	if !ok || cacheObj == nil {
+		doc = etree.NewDocument()
+		if err := doc.ReadFromString(xmlString); err != nil {
+			return nil, err
+		}
+		if err := xmlDocumentCache.Set(cacheKey, doc); err != nil {
+			log.Warnf("Could not set XML document in cache: %s", err)
+		}
+	}
+
+	return doc, nil
+}
 
 func XMLCacheInit() {
 	gc := gcache.New(50)
@@ -26,54 +70,20 @@ func XMLCacheInit() {
 
 // func XMLGetAttributeValue(xmlString string, path string, attributeName string) string {
 func XMLGetAttributeValue(params ...any) (any, error) {
-	var compiledPath etree.Path
-	var err error
-	var ok bool
-
 	xmlString := params[0].(string)
 	path := params[1].(string)
 	attributeName := params[2].(string)
-	rwMutex.RLock()
-	if compiledPath, ok = pathCache[path]; !ok {
-		compiledPath, err = etree.CompilePath(path)
-		if err != nil {
-			log.Errorf("Could not compile path %s: %s", path, err)
-			rwMutex.RUnlock()
-			return "", nil
-		}
-		rwMutex.RUnlock()
-		rwMutex.Lock()
-		pathCache[path] = compiledPath
-		rwMutex.Unlock()
-	} else {
-		rwMutex.RUnlock()
-	}
 
-	cacheKey := xxhash.Sum64String(xmlString)
-
-	cacheObj, err := xmlDocumentCache.Get(cacheKey)
-
-	if err != nil && !errors.Is(err, gcache.KeyNotFoundError) {
-		log.Errorf("Could not get XML document from cache: %s", err)
+	compiledPath, err := compileOrGetPath(path)
+	if err != nil {
+		log.Errorf("Could not compile path %s: %s", path, err)
 		return "", nil
 	}
 
-	var doc *etree.Document
-
-	doc, ok = cacheObj.(*etree.Document)
-
-	if cacheObj == nil || !ok {
-		doc = etree.NewDocument()
-		err = doc.ReadFromString(xmlString)
-		if err != nil {
-			log.Tracef("Could not parse XML: %s", err)
-			return "", nil
-		}
-		err = xmlDocumentCache.Set(cacheKey, doc)
-
-		if err != nil {
-			log.Warnf("Could not set XML document in cache: %s", err)
-		}
+	doc, err := getXMLDocumentFromCache(xmlString)
+	if err != nil {
+		log.Tracef("Could not parse XML: %s", err)
+		return "", nil
 	}
 
 	elem := doc.FindElementPath(compiledPath)
@@ -81,64 +91,31 @@ func XMLGetAttributeValue(params ...any) (any, error) {
 		log.Debugf("Could not find element %s", path)
 		return "", nil
 	}
+
 	attr := elem.SelectAttr(attributeName)
 	if attr == nil {
 		log.Debugf("Could not find attribute %s", attributeName)
 		return "", nil
 	}
+
 	return attr.Value, nil
 }
 
 // func XMLGetNodeValue(xmlString string, path string) string {
 func XMLGetNodeValue(params ...any) (any, error) {
-	var compiledPath etree.Path
-	var err error
-	var ok bool
-
 	xmlString := params[0].(string)
 	path := params[1].(string)
 
-	rwMutex.RLock()
-	if compiledPath, ok = pathCache[path]; !ok {
-		compiledPath, err = etree.CompilePath(path)
-		if err != nil {
-			log.Errorf("Could not compile path %s: %s", path, err)
-			rwMutex.RUnlock()
-			return "", nil
-		}
-		rwMutex.RUnlock()
-		rwMutex.Lock()
-		pathCache[path] = compiledPath
-		rwMutex.Unlock()
-	} else {
-		rwMutex.RUnlock()
-	}
-
-	cacheKey := xxhash.Sum64String(xmlString)
-
-	cacheObj, err := xmlDocumentCache.Get(cacheKey)
-
-	if err != nil && !errors.Is(err, gcache.KeyNotFoundError) {
-		log.Errorf("Could not get XML document from cache: %s", err)
+	compiledPath, err := compileOrGetPath(path)
+	if err != nil {
+		log.Errorf("Could not compile path %s: %s", path, err)
 		return "", nil
 	}
 
-	var doc *etree.Document
-
-	doc, ok = cacheObj.(*etree.Document)
-
-	if cacheObj == nil || !ok {
-		doc = etree.NewDocument()
-		err = doc.ReadFromString(xmlString)
-		if err != nil {
-			log.Tracef("Could not parse XML: %s", err)
-			return "", nil
-		}
-		err = xmlDocumentCache.Set(cacheKey, doc)
-
-		if err != nil {
-			log.Warnf("Could not set XML document in cache: %s", err)
-		}
+	doc, err := getXMLDocumentFromCache(xmlString)
+	if err != nil {
+		log.Tracef("Could not parse XML: %s", err)
+		return "", nil
 	}
 
 	elem := doc.FindElementPath(compiledPath)
@@ -146,5 +123,6 @@ func XMLGetNodeValue(params ...any) (any, error) {
 		log.Debugf("Could not find element %s", path)
 		return "", nil
 	}
+
 	return elem.Text(), nil
 }
