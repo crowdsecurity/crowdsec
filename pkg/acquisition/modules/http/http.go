791901fb83d2b9202bf343c95a54b56f44afb733
he2ss <hamza.essahely@gmail.com>
2024-10-23 15:14:58 +0200

    add HTTP datasource

---
+++ pkg/acquisition/modules/http/http.go
@@ -0,0 +1,353 @@
+package httpacquisition
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+
+	// import gin for http server
+
+	"github.com/prometheus/client_golang/prometheus"
+	log "github.com/sirupsen/logrus"
+
+	"gopkg.in/tomb.v2"
+	"gopkg.in/yaml.v2"
+
+	"github.com/crowdsecurity/go-cs-lib/pkg/trace"
+
+	"github.com/crowdsecurity/crowdsec/pkg/acquisition/configuration"
+	"github.com/crowdsecurity/crowdsec/pkg/types"
+)
+
+var (
+	dataSourceName = "http"
+)
+
+var linesRead = prometheus.NewCounterVec(
+	prometheus.CounterOpts{
+		Name: "cs_httpsource_hits_total",
+		Help: "Total lines that were read from http source",
+	},
+	[]string{"path"})
+
+const ()
+
+type HttpConfiguration struct {
+	//IPFilter                        []string          `yaml:"ip_filter"`
+	Port                              int                `yaml:"port"`
+	Path                              string             `yaml:"path"`
+	AuthType                          string             `yaml:"auth_type"`
+	BasicAuth                         *BasicAuthConfig   `yaml:"basic_auth"`
+	Headers                           *map[string]string `yaml:"headers"`
+	TLS                               *TLSConfig         `yaml:"tls"`
+	CustomStatusCode                  *int               `yaml:"custom_status_code"`
+	CustomHeaders                     *map[string]string `yaml:"custom_headers"`
+	MaxBodySize                       *int64             `yaml:"max_body_size"`
+	ChunkSize                         *int64             `yaml:"chunk_size"`
+	Timeout                           *time.Duration     `yaml:"timeout"`
+	configuration.DataSourceCommonCfg `yaml:",inline"`
+}
+
+type BasicAuthConfig struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+}
+
+type TLSConfig struct {
+	InsecureSkipVerify bool   `yaml:"insecure_skip_verify"`
+	ServerCert         string `yaml:"server_cert"`
+	ServerKey          string `yaml:"server_key"`
+	CaCert             string `yaml:"ca_cert"`
+}
+
+type HTTPSource struct {
+	Config HttpConfiguration
+	logger *log.Entry
+	Server *http.Server
+}
+
+func (h *HTTPSource) GetUuid() string {
+	return h.Config.UniqueId
+}
+
+func (h *HTTPSource) UnmarshalConfig(yamlConfig []byte) error {
+	h.Config = HttpConfiguration{}
+	err := yaml.Unmarshal(yamlConfig, &h.Config)
+	if err != nil {
+		return fmt.Errorf("cannot parse %s datasource configuration: %w", dataSourceName, err)
+	}
+	return nil
+}
+
+func (hc *HttpConfiguration) Validate() error {
+	if hc.Port == 0 {
+		return fmt.Errorf("port is required")
+	}
+	if hc.Path == "" {
+		return fmt.Errorf("path is required")
+	}
+
+	switch hc.AuthType {
+	case "basic_auth":
+		if hc.BasicAuth == nil {
+			return fmt.Errorf("basic_auth is required")
+		}
+		if hc.BasicAuth.Username == "" {
+			return fmt.Errorf("username is required")
+		}
+		if hc.BasicAuth.Password == "" {
+			return fmt.Errorf("password is required")
+		}
+	case "headers":
+		if hc.Headers == nil {
+			return fmt.Errorf("headers is required")
+		}
+	case "mtls":
+		if hc.TLS == nil || hc.TLS != nil && hc.TLS.CaCert == "" {
+			return fmt.Errorf("ca_cert is required")
+		}
+	default:
+		if hc.TLS == nil {
+			return fmt.Errorf("at least one of tls or auth_type is required")
+		}
+	}
+
+	if hc.TLS != nil {
+		if hc.TLS.ServerCert == "" {
+			return fmt.Errorf("server_cert is required")
+		}
+		if hc.TLS.ServerKey == "" {
+			return fmt.Errorf("server_key is required")
+		}
+	}
+
+	if hc.MaxBodySize != nil && *hc.MaxBodySize <= 0 {
+		return fmt.Errorf("max_body_size must be positive")
+	}
+
+	if hc.ChunkSize != nil && *hc.ChunkSize <= 0 {
+		return fmt.Errorf("chunk_size must be positive")
+	}
+
+	// validate custom status code
+	if hc.CustomStatusCode != nil {
+		statusText := http.StatusText(*hc.CustomStatusCode)
+		if statusText == "" {
+			return fmt.Errorf("invalid HTTP status code")
+		}
+	}
+
+	return nil
+}
+
+func (h *HTTPSource) Configure(yamlConfig []byte, logger *log.Entry) error {
+	h.logger = logger
+	err := h.UnmarshalConfig(yamlConfig)
+	if err != nil {
+		return err
+	}
+
+	if err := h.Config.Validate(); err != nil {
+		return fmt.Errorf("invalid configuration: %w", err)
+	}
+
+	return nil
+}
+
+func (h *HTTPSource) ConfigureByDSN(string, map[string]string, *log.Entry, string) error {
+	return fmt.Errorf("%s datasource does not support command-line acquisition", dataSourceName)
+}
+
+func (h *HTTPSource) GetMode() string {
+	return h.Config.Mode
+}
+
+func (h *HTTPSource) GetName() string {
+	return dataSourceName
+}
+
+func (k *HTTPSource) OneShotAcquisition(out chan types.Event, t *tomb.Tomb) error {
+	return fmt.Errorf("%s datasource does not support one-shot acquisition", dataSourceName)
+}
+
+func (h *HTTPSource) CanRun() error {
+	return nil
+}
+
+func (h *HTTPSource) GetMetrics() []prometheus.Collector {
+	return []prometheus.Collector{linesRead}
+}
+
+func (k *HTTPSource) GetAggregMetrics() []prometheus.Collector {
+	return []prometheus.Collector{linesRead}
+}
+
+func (h *HTTPSource) Dump() interface{} {
+	return h
+}
+
+func (hc *HttpConfiguration) NewTLSConfig() (*tls.Config, error) {
+	tlsConfig := tls.Config{
+		InsecureSkipVerify: hc.TLS.InsecureSkipVerify,
+	}
+
+	if hc.TLS.ServerCert != "" && hc.TLS.ServerKey != "" {
+		cert, err := tls.LoadX509KeyPair(hc.TLS.ServerCert, hc.TLS.ServerKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load server cert/key: %w", err)
+		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	}
+
+	if hc.AuthType == "mtls" && hc.TLS.CaCert != "" {
+		caCert, err := os.ReadFile(hc.TLS.CaCert)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read ca cert: %w", err)
+		}
+		caCertPool := x509.NewCertPool()
+		caCertPool.AppendCertsFromPEM(caCert)
+		tlsConfig.ClientCAs = caCertPool
+		tlsConfig.ClientAuth = tls.RequireAndVerifyClientCert
+	}
+
+	return &tlsConfig, nil
+}
+
+func authorizeRequest(r *http.Request, hc *HttpConfiguration) error {
+	if hc.AuthType == "basic_auth" {
+		username, password, ok := r.BasicAuth()
+		if !ok {
+			return fmt.Errorf("missing basic auth")
+		}
+		if username != hc.BasicAuth.Username || password != hc.BasicAuth.Password {
+			return fmt.Errorf("invalid basic auth")
+		}
+	}
+	if hc.AuthType == "headers" {
+		for key, value := range *hc.Headers {
+			if r.Header.Get(key) != value {
+				return fmt.Errorf("invalid headers")
+			}
+		}
+	}
+	return nil
+}
+
+func (h *HTTPSource) processRequest(w http.ResponseWriter, r *http.Request, hc *HttpConfiguration, out chan types.Event, t *tomb.Tomb) error {
+	if hc.MaxBodySize != nil && r.ContentLength > *hc.MaxBodySize {
+		w.WriteHeader(http.StatusRequestEntityTooLarge)
+		return fmt.Errorf("body size exceeds max body size: %d > %d", r.ContentLength, *hc.MaxBodySize)
+	}
+	body, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return fmt.Errorf("failed to read body: %w", err)
+	}
+	h.logger.Tracef("body received: %+v", string(body))
+
+	t.Go(func() error {
+		line := types.Line{
+			Raw:     string(body),
+			Src:     r.RemoteAddr,
+			Time:    time.Now().UTC(),
+			Labels:  hc.Labels,
+			Process: true,
+			Module:  h.GetName(),
+		}
+
+		evt := types.Event{
+			Line:       line,
+			Process:    true,
+			Type:       types.LOG,
+			ExpectMode: types.LIVE,
+		}
+		out <- evt
+		return nil
+	})
+
+	return nil
+}
+
+func (h *HTTPSource) RunServer(out chan types.Event, t *tomb.Tomb) error {
+	mux := http.NewServeMux()
+	mux.HandleFunc(h.Config.Path, func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			h.logger.Errorf("method not allowed: %s", r.Method)
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		if err := authorizeRequest(r, &h.Config); err != nil {
+			h.logger.Errorf("failed to authorize request: %s", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+		err := h.processRequest(w, r, &h.Config, out, t)
+		if err != nil {
+			h.logger.Errorf("failed to process request: %s", err)
+			return
+		}
+
+		if h.Config.CustomHeaders != nil {
+			for key, value := range *h.Config.CustomHeaders {
+				w.Header().Set(key, value)
+			}
+		}
+		if h.Config.CustomStatusCode != nil {
+			w.WriteHeader(*h.Config.CustomStatusCode)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+
+		w.Write([]byte("OK"))
+	})
+
+	h.Server = &http.Server{
+		Addr:    fmt.Sprintf(":%d", h.Config.Port),
+		Handler: mux,
+	}
+
+	if h.Config.Timeout != nil {
+		h.Server.ReadTimeout = *h.Config.Timeout
+	}
+
+	if h.Config.TLS != nil {
+		tlsConfig, err := h.Config.NewTLSConfig()
+		if err != nil {
+			return fmt.Errorf("failed to create tls config: %w", err)
+		}
+		h.logger.Tracef("tls config: %+v", tlsConfig)
+		h.Server.TLSConfig = tlsConfig
+	}
+
+	if h.Config.TLS != nil {
+		h.logger.Infof("start https server on port %d", h.Config.Port)
+		err := h.Server.ListenAndServeTLS(h.Config.TLS.ServerCert, h.Config.TLS.ServerKey)
+		if err != nil && err != http.ErrServerClosed {
+			return fmt.Errorf("https server failed: %w", err)
+		}
+	} else {
+		h.logger.Infof("start http server on port %d", h.Config.Port)
+		err := h.Server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			return fmt.Errorf("http server failed: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func (h *HTTPSource) StreamingAcquisition(out chan types.Event, t *tomb.Tomb) error {
+	h.logger.Debugf("start http server on port %d", h.Config.Port)
+
+	t.Go(func() error {
+		defer trace.CatchPanic("crowdsec/acquis/http/live")
+		return h.RunServer(out, t)
+	})
+
+	return nil
+}
---
+++ pkg/acquisition/modules/http/http_test.go
@@ -0,0 +1,702 @@
+package httpacquisition
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/crowdsecurity/crowdsec/pkg/types"
+	"github.com/crowdsecurity/go-cs-lib/pkg/cstest"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/tomb.v2"
+)
+
+const (
+	testHTTPServerAddr    = "http://127.0.0.1:8080"
+	testHTTPServerAddrTLS = "https://127.0.0.1:8080"
+)
+
+func TestConfigure(t *testing.T) {
+	tests := []struct {
+		config      string
+		expectedErr string
+	}{
+		{
+			config: `
+foobar: bla`,
+			expectedErr: "invalid configuration: port is required",
+		},
+		{
+			config: `
+source: http
+port: aa`,
+			expectedErr: "cannot parse http datasource configuration: yaml: unmarshal errors:\n  line 3: cannot unmarshal !!str `aa` into int",
+		},
+		{
+			config: `
+source: http
+port: 8080`,
+			expectedErr: "invalid configuration: path is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: basic_auth`,
+			expectedErr: "invalid configuration: basic_auth is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers`,
+			expectedErr: "invalid configuration: headers is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: basic_auth
+basic_auth:
+  username: 132`,
+			expectedErr: "invalid configuration: password is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: basic_auth
+basic_auth:
+  password: 132`,
+			expectedErr: "invalid configuration: username is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:`,
+			expectedErr: "invalid configuration: headers is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: toto`,
+			expectedErr: "invalid configuration: at least one of tls or auth_type is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: value
+tls:
+  server_key: key`,
+			expectedErr: "invalid configuration: server_cert is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: value
+tls:
+  server_cert: cert`,
+			expectedErr: "invalid configuration: server_key is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: mtls
+tls:
+  server_cert: cert
+  server_key: key`,
+			expectedErr: "invalid configuration: ca_cert is required",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: value
+max_body_size: 0`,
+			expectedErr: "invalid configuration: max_body_size must be positive",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: value
+chunk_size: 0`,
+			expectedErr: "invalid configuration: chunk_size must be positive",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: value
+timeout: toto`,
+			expectedErr: "cannot parse http datasource configuration: yaml: unmarshal errors:\n  line 8: cannot unmarshal !!str `toto` into time.Duration",
+		},
+		{
+			config: `
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: value
+custom_status_code: 999`,
+			expectedErr: "invalid configuration: invalid HTTP status code",
+		},
+	}
+
+	subLogger := log.WithFields(log.Fields{
+		"type": "http",
+	})
+
+	for _, test := range tests {
+		h := HTTPSource{}
+		err := h.Configure([]byte(test.config), subLogger)
+		cstest.AssertErrorContains(t, err, test.expectedErr)
+	}
+}
+
+func TestGetUuid(t *testing.T) {
+	h := HTTPSource{}
+	h.Config.UniqueId = "test"
+	if h.GetUuid() != "test" {
+		t.Fatalf("expected 'test', got '%s'", h.GetUuid())
+	}
+}
+
+func TestUnmarshalConfig(t *testing.T) {
+	h := HTTPSource{}
+	err := h.UnmarshalConfig([]byte(`
+source: http
+port: 8080
+path: 15
+	auth_type: headers`))
+	cstest.AssertErrorMessage(t, err, "cannot parse http datasource configuration: yaml: line 5: found a tab character that violates indentation")
+}
+
+func TestConfigureByDSN(t *testing.T) {
+	h := HTTPSource{}
+	err := h.ConfigureByDSN("http://localhost:8080/test", map[string]string{}, log.WithFields(log.Fields{
+		"type": "http",
+	}), "test")
+	cstest.AssertErrorMessage(
+		t,
+		err,
+		"http datasource does not support command-line acquisition",
+	)
+}
+
+func TestGetMode(t *testing.T) {
+	h := HTTPSource{}
+	h.Config.Mode = "test"
+	if h.GetMode() != "test" {
+		t.Fatalf("expected 'test', got '%s'", h.GetMode())
+	}
+}
+
+func TestGetName(t *testing.T) {
+	h := HTTPSource{}
+	if h.GetName() != "http" {
+		t.Fatalf("expected 'http', got '%s'", h.GetName())
+	}
+}
+
+func SetupAndRunHTTPSource(t *testing.T, h *HTTPSource, config []byte) (chan types.Event, *tomb.Tomb) {
+	subLogger := log.WithFields(log.Fields{
+		"type": "http",
+	})
+	err := h.Configure(config, subLogger)
+	if err != nil {
+		t.Fatalf("unable to configure http source: %s", err)
+	}
+	tomb := tomb.Tomb{}
+	out := make(chan types.Event)
+	err = h.StreamingAcquisition(out, &tomb)
+	if err != nil {
+		t.Fatalf("unable to start streaming acquisition: %s", err)
+	}
+	return out, &tomb
+}
+
+func TestStreamingAcquisitionWrongHTTPMethod(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: basic_auth
+basic_auth:
+  username: test
+  password: test`))
+
+	time.Sleep(1 * time.Second)
+
+	res, err := http.Get(fmt.Sprintf("%s/test", testHTTPServerAddr))
+	if err != nil {
+		t.Fatalf("unable to get http response: %s", err)
+	}
+	if res.StatusCode != http.StatusMethodNotAllowed {
+		t.Fatalf("expected status code %d, got %d", http.StatusMethodNotAllowed, res.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+
+}
+
+func TestStreamingAcquisitionUnknownPath(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: basic_auth
+basic_auth:
+  username: test
+  password: test`))
+
+	time.Sleep(1 * time.Second)
+
+	res, err := http.Get(fmt.Sprintf("%s/unknown", testHTTPServerAddr))
+	if err != nil {
+		t.Fatalf("unable to get http response: %s", err)
+	}
+
+	if res.StatusCode != http.StatusNotFound {
+		t.Fatalf("expected status code %d, got %d", http.StatusNotFound, res.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionBasicAuth(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: basic_auth
+basic_auth:
+  username: test
+  password: test`))
+
+	time.Sleep(1 * time.Second)
+
+	client := &http.Client{}
+
+	resp, err := http.Post(fmt.Sprintf("%s/test", testHTTPServerAddr), "application/json", strings.NewReader("test"))
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("expected status code %d, got %d", http.StatusUnauthorized, resp.StatusCode)
+	}
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddr), strings.NewReader("test"))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+	req.SetBasicAuth("test", "WrongPassword")
+	resp, err = client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("expected status code %d, got %d", http.StatusUnauthorized, resp.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionBadHeaders(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: test`))
+
+	time.Sleep(1 * time.Second)
+
+	client := &http.Client{}
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddr), strings.NewReader("test"))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+	req.Header.Add("key", "wrong")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+	if resp.StatusCode != http.StatusUnauthorized {
+		t.Fatalf("expected status code %d, got %d", http.StatusUnauthorized, resp.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionMaxBodySize(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: test
+max_body_size: 5`))
+
+	time.Sleep(1 * time.Second)
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddr), strings.NewReader("testtest"))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+	req.Header.Add("key", "test")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+	if resp.StatusCode != http.StatusRequestEntityTooLarge {
+		t.Fatalf("expected status code %d, got %d", http.StatusRequestEntityTooLarge, resp.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionSuccess(t *testing.T) {
+	h := &HTTPSource{}
+	out, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: test`))
+
+	time.Sleep(1 * time.Second)
+	rawEvt := `{"test": "test"}`
+
+	client := &http.Client{}
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddr), strings.NewReader(rawEvt))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+	req.Header.Add("key", "test")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	assertEvents(out, t, rawEvt)
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionCustomStatusCodeAndCustomHeaders(t *testing.T) {
+	h := &HTTPSource{}
+	out, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: test
+custom_status_code: 201
+custom_headers:
+  success: true`))
+
+	time.Sleep(1 * time.Second)
+
+	rawEvt := `{"test": "test"}`
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddr), strings.NewReader(rawEvt))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+	req.Header.Add("key", "test")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusCreated {
+		t.Fatalf("expected status code %d, got %d", http.StatusCreated, resp.StatusCode)
+	}
+
+	if resp.Header.Get("success") != "true" {
+		t.Fatalf("expected header 'success' to be 'true', got '%s'", resp.Header.Get("success"))
+	}
+
+	assertEvents(out, t, rawEvt)
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+type slowReader struct {
+	delay time.Duration
+	body  []byte
+	index int
+}
+
+func (sr *slowReader) Read(p []byte) (int, error) {
+	if sr.index >= len(sr.body) {
+		return 0, io.EOF
+	}
+	time.Sleep(sr.delay) // Simulate a delay in reading
+	n := copy(p, sr.body[sr.index:])
+	sr.index += n
+	return n, nil
+}
+
+func assertEvents(out chan types.Event, t *testing.T, expected string) {
+	readLines := []types.Event{}
+
+	select {
+	case event := <-out:
+		readLines = append(readLines, event)
+	case <-time.After(2 * time.Second):
+		break
+	}
+
+	if len(readLines) != 1 {
+		t.Fatalf("expected 1 line, got %d", len(readLines))
+	}
+	if readLines[0].Line.Raw != expected {
+		t.Fatalf(`expected %s, got '%+v'`, expected, readLines[0].Line)
+	}
+}
+
+func TestStreamingAcquisitionTimeout(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: test
+timeout: 1s`))
+
+	time.Sleep(1 * time.Second)
+
+	slow := &slowReader{
+		delay: 2 * time.Second,
+		body:  []byte(`{"test": "delayed_payload"}`),
+	}
+
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/test", testHTTPServerAddr), slow)
+	if err != nil {
+		t.Fatalf("Error creating request: %v", err)
+	}
+	req.Header.Add("key", "test")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("Error sending request: %v", err)
+	}
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatalf("expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionTLSHTTPRequest(t *testing.T) {
+	h := &HTTPSource{}
+	_, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+tls:
+  server_cert: testdata/server.crt
+  server_key: testdata/server.key
+  ca_cert: testdata/ca.crt`))
+
+	time.Sleep(1 * time.Second)
+
+	resp, err := http.Post(fmt.Sprintf("%s/test", testHTTPServerAddr), "application/json", strings.NewReader("test"))
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusBadRequest {
+		t.Fatalf("expected status code %d, got %d", http.StatusBadRequest, resp.StatusCode)
+	}
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionTLSWithHeadersAuthSuccess(t *testing.T) {
+	h := &HTTPSource{}
+	out, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: headers
+headers:
+  key: test
+tls:
+  server_cert: testdata/server.crt
+  server_key: testdata/server.key
+`))
+
+	time.Sleep(1 * time.Second)
+
+	caCert, err := os.ReadFile("testdata/server.crt")
+	if err != nil {
+		t.Fatalf("unable to read ca cert: %s", err)
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		RootCAs: caCertPool,
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	rawEvt := `{"test": "test"}`
+
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddrTLS), strings.NewReader(rawEvt))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+	req.Header.Add("key", "test")
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	assertEvents(out, t, rawEvt)
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
+
+func TestStreamingAcquisitionMTLS(t *testing.T) {
+	h := &HTTPSource{}
+	out, tomb := SetupAndRunHTTPSource(t, h, []byte(`
+source: http
+port: 8080
+path: /test
+auth_type: mtls
+tls:
+  server_cert: testdata/server.crt
+  server_key: testdata/server.key
+  ca_cert: testdata/ca.crt`))
+
+	time.Sleep(1 * time.Second)
+
+	// init client cert
+	cert, err := tls.LoadX509KeyPair("testdata/client.crt", "testdata/client.key")
+	if err != nil {
+		t.Fatalf("unable to load client cert: %s", err)
+	}
+
+	caCert, err := os.ReadFile("testdata/ca.crt")
+	if err != nil {
+		t.Fatalf("unable to read ca cert: %s", err)
+	}
+
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      caCertPool,
+	}
+
+	client := &http.Client{
+		Transport: &http.Transport{
+			TLSClientConfig: tlsConfig,
+		},
+	}
+
+	rawEvt := `{"test": "test"}`
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf("%s/test", testHTTPServerAddrTLS), strings.NewReader(rawEvt))
+	if err != nil {
+		t.Fatalf("unable to create http request: %s", err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatalf("unable to post http request: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("expected status code %d, got %d", http.StatusOK, resp.StatusCode)
+	}
+
+	assertEvents(out, t, rawEvt)
+
+	h.Server.Close()
+	tomb.Kill(nil)
+	tomb.Wait()
+}
