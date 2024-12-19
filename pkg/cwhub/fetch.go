82b0da6496784d6d1122c318bc677d5bd5779a17
marco <marco@crowdsec.net>
2024-12-19 16:55:53 +0100

    refact content download

--- pkg/cwhub/fetch.go
+++ pkg/cwhub/fetch.go
@@ -7,6 +7,7 @@ import (
 	"crypto"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -17,47 +18,46 @@ import (
 
 )
 
-// FetchContentTo downloads the last version of the item's YAML file to the specified path.
-func (i *Item) FetchContentTo(ctx context.Context, destPath string) (bool, string, error) {
-	wantHash := i.latestHash()
-	if wantHash == "" {
-		return false, "", fmt.Errorf("%s: latest hash missing from index. The index file is invalid, please run 'cscli hub update' and try again", i.FQName())
+
+// writeEmbeddedContentTo writes the embedded content to the specified path and checks the hash.
+// If the content is base64 encoded, it will be decoded before writing. Check for item.Content
+// before calling this method.
+func (i *Item) writeEmbeddedContentTo(destPath, wantHash string) error {
+	if i.Content == "" {
+		return fmt.Errorf("no embedded content for %s", i.Name)
 	}
 
-	// Use the embedded content if available
-	if i.Content != "" {
-		// the content was historically base64 encoded
-		content, err := base64.StdEncoding.DecodeString(i.Content)
-		if err != nil {
-			content = []byte(i.Content)
-		}
-
-		dir := filepath.Dir(destPath)
-
-		if err := os.MkdirAll(dir, 0o755); err != nil {
-			return false, "", fmt.Errorf("while creating %s: %w", dir, err)
-		}
-
-		// check sha256
-		hash := crypto.SHA256.New()
-		if _, err := hash.Write(content); err != nil {
-			return false, "", fmt.Errorf("while hashing %s: %w", i.Name, err)
-		}
-
-		gotHash := hex.EncodeToString(hash.Sum(nil))
-		if gotHash != wantHash {
-			return false, "", fmt.Errorf("hash mismatch: expected %s, got %s. The index file is invalid, please run 'cscli hub update' and try again", wantHash, gotHash)
-		}
-
-		if err := os.WriteFile(destPath, content, 0o600); err != nil {
-			return false, "", fmt.Errorf("while writing %s: %w", destPath, err)
-		}
-
-		i.hub.logger.Debugf("Wrote %s content from .index.json to %s", i.Name, destPath)
-
-		return true, fmt.Sprintf("(embedded in %s)", i.hub.local.HubIndexFile), nil
+	content, err := base64.StdEncoding.DecodeString(i.Content)
+	if err != nil {
+		content = []byte(i.Content)
 	}
 
+	dir := filepath.Dir(destPath)
+
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return fmt.Errorf("while creating %s: %w", dir, err)
+	}
+
+	// check sha256
+	hash := crypto.SHA256.New()
+	if _, err := hash.Write(content); err != nil {
+		return fmt.Errorf("while hashing %s: %w", i.Name, err)
+	}
+
+	gotHash := hex.EncodeToString(hash.Sum(nil))
+	if gotHash != wantHash {
+		return fmt.Errorf("hash mismatch: expected %s, got %s. The index file is invalid, please run 'cscli hub update' and try again", wantHash, gotHash)
+	}
+
+	if err := os.WriteFile(destPath, content, 0o600); err != nil {
+		return fmt.Errorf("while writing %s: %w", destPath, err)
+	}
+
+	return nil
+}
+
+// writeRemoteContentTo downloads the content to the specified path and checks the hash.
+func (i *Item) writeRemoteContentTo(ctx context.Context, destPath, wantHash string) (bool, string, error) {
 	url, err := i.hub.remote.urlTo(i.RemotePath)
 	if err != nil {
 		return false, "", fmt.Errorf("failed to build request: %w", err)
@@ -73,12 +73,36 @@ func (i *Item) FetchContentTo(ctx context.Context, destPath string) (bool, strin
 		CompareContent().
 		VerifyHash("sha256", wantHash)
 
-	// TODO: recommend hub update if hash does not match
+	hasherr := downloader.HashMismatchError{}
 
 	downloaded, err := d.Download(ctx, url)
-	if err != nil {
+
+	switch {
+	case errors.As(err, &hasherr):
+		i.hub.logger.Warnf("%s. The index file is outdated, please run 'cscli hub update' and try again", err.Error())
+	case err != nil:
 		return false, "", err
 	}
 
 	return downloaded, url, nil
+}
+
+// FetchContentTo writes the last version of the item's YAML file to the specified path.
+// Returns whether the file was downloaded, and the remote url for feedback purposes.
+func (i *Item) FetchContentTo(ctx context.Context, destPath string) (bool, string, error) {
+	wantHash := i.latestHash()
+	if wantHash == "" {
+		return false, "", fmt.Errorf("%s: latest hash missing from index. The index file is invalid, please run 'cscli hub update' and try again", i.FQName())
+	}
+
+	// Use the embedded content if available
+	if i.Content != "" {
+		if err := i.writeEmbeddedContentTo(destPath, wantHash); err != nil {
+			return false, "", err
+		}
+
+		return true, fmt.Sprintf("(embedded in %s)", i.hub.local.HubIndexFile), nil
+	}
+
+	return i.writeRemoteContentTo(ctx, destPath, wantHash)
 }
