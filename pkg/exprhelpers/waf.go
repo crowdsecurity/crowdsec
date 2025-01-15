f20e7b8c4628a9c3ac9dc1e3ecf81cb36d3561db
Sebastien Blot <sebastien@crowdsec.net>
2025-01-15 16:14:11 +0100

    add JA4H expr helper

---
+++ pkg/exprhelpers/waf.go
@@ -0,0 +1,14 @@
+package exprhelpers
+
+import (
+	"net/http"
+
+	j4ah "github.com/lum8rjack/go-ja4h"
+)
+
+// JA4H(req *http.Request) string
+func JA4H(params ...any) (any, error) {
+	req := params[0].(*http.Request)
+
+	return j4ah.JA4H(req), nil
+}
