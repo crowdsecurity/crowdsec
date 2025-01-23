5b45ebbade6d3a5820a06c212d3b48b8421ec292
Thibault Koechlin <orixxx@gmail.com>
2025-01-23 10:55:34 +0100

    fix misleading count of discarded rules

--- pkg/acquisition/modules/appsec/appsec_runner.go
+++ pkg/acquisition/modules/appsec/appsec_runner.go
@@ -35,6 +35,7 @@ type AppsecRunner struct {
 func (r *AppsecRunner) MergeDedupRules(collections []appsec.AppsecCollection, logger *log.Entry) string {
 	var rulesArr []string
 	dedupRules := make(map[string]struct{})
+	discarded := 0
 
 	for _, collection := range collections {
 		// Dedup *our* rules
@@ -43,14 +44,15 @@ func (r *AppsecRunner) MergeDedupRules(collections []appsec.AppsecCollection, lo
 				rulesArr = append(rulesArr, rule)
 				dedupRules[rule] = struct{}{}
 			} else {
+				discarded++
 				logger.Debugf("Discarding duplicate rule : %s", rule)
 			}
 		}
 		// Don't mess up with native modsec rules
 		rulesArr = append(rulesArr, collection.NativeRules...)
 	}
-	if len(rulesArr) != len(dedupRules) {
-		logger.Warningf("%d rules were discarded as they were duplicates", len(rulesArr)-len(dedupRules))
+	if discarded > 0 {
+		logger.Warningf("%d rules were discarded as they were duplicates", discarded)
 	}
 
 	return strings.Join(rulesArr, "\n")
