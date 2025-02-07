a9cc23e446221540fd5c33bf9642ef533db991fc
mmetc <92726601+mmetc@users.noreply.github.com>
2025-02-07 10:52:14 +0100

    cscli: exclude removed commands from generated docs (#3449)

--- cmd/crowdsec-cli/cliconfig/backup.go
+++ cmd/crowdsec-cli/cliconfig/backup.go
@@ -9,11 +9,13 @@ import (
 func (cli *cliConfig) newBackupCmd() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:               "backup",
+		Short: "This command has been removed. You can backup/restore the configuration by other means.",
 		DisableAutoGenTag: true,
 		RunE: func(_ *cobra.Command, _ []string) error {
 			configDir := cli.cfg().ConfigPaths.ConfigDir
 			return fmt.Errorf("'cscli config backup' has been removed, you can manually backup/restore %s instead", configDir)
 		},
+		Hidden: true,
 	}
 
 	return cmd
