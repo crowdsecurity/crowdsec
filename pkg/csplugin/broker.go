e6b28e3e5736a9680233db41419467c33c519b78
marco <marco@crowdsec.net>
2025-05-16 16:21:38 +0200

    fix + reduce error verbosity

--- pkg/csplugin/broker.go
+++ pkg/csplugin/broker.go
@@ -84,11 +84,11 @@ func (pb *PluginBroker) Init(ctx context.Context, pluginCfg *csconfig.PluginCfg,
 	pb.pluginsTypesToDispatch = make(map[string]struct{})
 
 	if err := pb.loadConfig(configPaths.NotificationDir); err != nil {
-		return fmt.Errorf("while loading plugin config: %w", err)
+		return fmt.Errorf("loading config: %w", err)
 	}
 
 	if err := pb.loadPlugins(ctx, configPaths.PluginDir); err != nil {
-		return fmt.Errorf("while loading plugin: %w", err)
+		return fmt.Errorf("loading plugin: %w", err)
 	}
 
 	pb.watcher = PluginWatcher{}
@@ -208,10 +208,6 @@ func (pb *PluginBroker) loadConfig(path string) error {
 
 		for _, pluginConfig := range pluginConfigs {
 			SetRequiredFields(&pluginConfig)
-
-			if pluginConfig.Type == "" {
-				return fmt.Errorf("plugin %s has no type", pluginConfig.Name)
-			}
 
 			if _, ok := pb.pluginConfigByName[pluginConfig.Name]; ok {
 				log.Warningf("notification '%s' is defined multiple times", pluginConfig.Name)
@@ -413,8 +409,12 @@ func ParsePluginConfigFile(path string) ([]PluginConfig, error) {
 	dec := yaml.NewDecoder(yamlFile)
 	dec.SetStrict(true)
 
+	idx := -1
+
 	for {
-		pc := PluginConfig{}
+		var pc PluginConfig
+
+		idx += 1
 
 		err = dec.Decode(&pc)
 		if err != nil {
@@ -424,9 +424,15 @@ func ParsePluginConfigFile(path string) ([]PluginConfig, error) {
 
 			return nil, fmt.Errorf("while decoding %s got error %s", path, err)
 		}
+
 		// if the yaml document is empty, skip
 		if reflect.DeepEqual(pc, PluginConfig{}) {
 			continue
+		}
+
+		if pc.Type == "" {
+			return nil, fmt.Errorf("field 'type' missing in %s (position %d)", path, idx)
+
 		}
 
 		parsedConfigs = append(parsedConfigs, pc)
