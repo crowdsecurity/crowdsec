cb8139e307156a701ca5ff238a2f1cb25ee318eb
marco <marco@crowdsec.net>
2025-06-02 15:06:12 +0200

    use github.com/cenkalti/backoff/v5 for apiclient backoff

--- pkg/apiclient/auth_retry.go
+++ pkg/apiclient/auth_retry.go
@@ -17,7 +17,6 @@ type retryRoundTripper struct {
 	maxAttempts      uint
 	retryStatusCodes []int
 	withBackOff      bool
-	onBeforeRequest  func(attempt int)
 }
 
 func (r retryRoundTripper) ShouldRetry(statusCode int) bool {
@@ -25,60 +24,58 @@ func (r retryRoundTripper) ShouldRetry(statusCode int) bool {
 }
 
 func (r retryRoundTripper) RoundTrip(req *http.Request) (*http.Response, error) {
-	maxAttempts := r.maxAttempts
+	maxAttempts := max(r.maxAttempts, 1)
 	if fflag.DisableHttpRetryBackoff.IsEnabled() {
 		maxAttempts = 1
 	}
 
-	// Build a backoff policy:
-	//
-	// - If r.withBackOff is true, use exponential + jitter.
-	// - Otherwise, use a constant backoff of 0 (i.e. no waiting)
-
 	var bo backoff.BackOff
+
 	if r.withBackOff {
+		// Use exponential + jitter; the default values are:
+		//
+		// DefaultInitialInterval     = 500 * time.Millisecond
+		// DefaultRandomizationFactor = 0.5
+		// DefaultMultiplier          = 1.5
+		// DefaultMaxInterval         = 60 * time.Second
 		exp := backoff.NewExponentialBackOff()
-		exp.InitialInterval = 200 * time.Millisecond
-		exp.MaxInterval = 20 * time.Second
+		exp.InitialInterval = 20 * time.Second
+		exp.Multiplier = 2
 		bo = exp
 	} else {
 		// backoff is disabled, policy of "no wait"
 		bo = backoff.NewConstantBackOff(0)
 	}
 
-	attempt := uint(0)
+	attemptLeft := maxAttempts
 
 	operation := func() (*http.Response, error) {
-		if r.onBeforeRequest != nil {
-			r.onBeforeRequest(int(attempt))
-		}
-
 		clonedReq := cloneRequest(req)
+
+		attemptLeft--
 
 		resp, err := r.next.RoundTrip(clonedReq)
 		if err != nil {
-			log.Errorf("error while performing request: %s; %d retries left", err, maxAttempts-attempt-1)
-			return resp, nil
+			if attemptLeft > 0 {
+				log.Errorf("while performing request: %s; %d retries left", err, attemptLeft)
+			}
+			return nil, fmt.Errorf("retryable error: %w", err)
 		}
 
-		log.Infof("retrying... (attempt %d of %d)", attempt+1, maxAttempts)
-
-		if !r.ShouldRetry(resp.StatusCode) {
-			return resp, nil
+		if r.ShouldRetry(resp.StatusCode) {
+			log.Errorf("request returned status %d: %s; %d retries left", resp.StatusCode, resp.Status, attemptLeft)
+	        	return nil, fmt.Errorf("retryable status: %d", resp.StatusCode)
 		}
 
-		resp.Body.Close()
-
-	        return nil, fmt.Errorf("retryable status: %d", resp.StatusCode)
+		return resp, nil
 	}
 
 	resp, err := backoff.Retry(req.Context(), operation,
 		backoff.WithBackOff(bo),
-		backoff.WithMaxTries(r.maxAttempts),
+		backoff.WithMaxTries(maxAttempts),
 	)
 
 	if err != nil {
-		// can it be a context cancelation?? XXX
 		return nil, err
 	}
 
