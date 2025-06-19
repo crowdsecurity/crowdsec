eb5b1648814bacd14f346910f51cf52cc4a1495d
marco <marco@crowdsec.net>
2025-06-19 10:46:50 +0200

    extracted expr.go

--- pkg/setup/detect.go
+++ pkg/setup/detect.go
@@ -6,15 +6,12 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"os"
 	"os/exec"
 	"slices"
 	"sort"
 
-	"github.com/Masterminds/semver/v3"
 	"github.com/blackfireio/osinfo"
 	"github.com/expr-lang/expr"
-	"github.com/shirou/gopsutil/v3/process"
 	log "github.com/sirupsen/logrus"
 	goccyyaml "github.com/goccy/go-yaml"
 	"gopkg.in/yaml.v3"
@@ -44,7 +41,7 @@ type ServicePlan struct {
 
 // XXX: TODO: Setup is not validated in any way. it can contain non-existent items, malformed acquisition etc.
 
-// Setup is a container for a list of ServiceSetup objects, allowing for future extensions.
+// Setup is a container for a list of ServicePlan objects, allowing for future extensions.
 type Setup struct {
 	Setup []ServicePlan `yaml:"setup"`
 }
@@ -187,8 +184,13 @@ func validateDataSource(opaqueDS DataSourceItem) error {
 		return err
 	}
 
-	// pp.Println(ds)
 	return nil
+}
+
+// Detector is generated from the detection rules (detect.yaml) and can generate the setup plan.
+type Detector struct {
+	Version string                  `yaml:"version"`
+	Detect  map[string]ServiceRules `yaml:"detect"`
 }
 
 func NewDetector(detectReader io.Reader) (*Detector, error) {
@@ -225,7 +227,7 @@ func NewDetector(detectReader io.Reader) (*Detector, error) {
 	return &d, nil
 }
 
-// ListSupported parses the configuration file and outputs a list of the supported services.
+// ListSupportedServices returns a list of the services recognized by the detector.
 func (d *Detector) ListSupportedServices() []string {
 	keys := make([]string, 0)
 	for k := range d.Detect {
@@ -240,7 +242,7 @@ func (d *Detector) ListSupportedServices() []string {
 func NewSetup(detector *Detector, opts DetectOptions) (*Setup, error) {
 	s := Setup{}
 
-	// explicitly initialize to avoid json mashaling an empty slice as "null"
+	// explicitly initialize to avoid json marshaling an empty slice as "null"
 	s.Setup = make([]ServicePlan, 0)
 
 	os := opts.ForcedOS
@@ -271,7 +273,7 @@ func NewSetup(detector *Detector, opts DetectOptions) (*Setup, error) {
 
 	env := NewExprEnvironment(opts, os)
 
-	detected, err := filterWithRules(detector, env)
+	detected, err := buildPlans(detector, env)
 	if err != nil {
 		return nil, err
 	}
@@ -296,12 +298,7 @@ func NewSetup(detector *Detector, opts DetectOptions) (*Setup, error) {
 	sort.Strings(keys)
 
 	for _, name := range keys {
-		svc := detected[name]
-
-		s.Setup = append(s.Setup, ServicePlan{
-			Name:                  name,
-			ServiceRecommendation: svc.ServiceRecommendation,
-		})
+		s.Setup = append(s.Setup, detected[name])
 	}
 
 	return &s, nil
@@ -311,38 +308,6 @@ func NewSetup(detector *Detector, opts DetectOptions) (*Setup, error) {
 type ServiceRules struct {
 	When []string         `yaml:"when"`
 	ServiceRecommendation `yaml:",inline"`
-}
-
-// Detector is generated from the detection rules (detect.yaml) and can generate the setup plan.
-type Detector struct {
-	Version string                  `yaml:"version"`
-	Detect  map[string]ServiceRules `yaml:"detect"`
-}
-
-// ExprState keeps a global state for the duration of the service detection (cache etc.)
-type ExprState struct {
-	unitsSearched map[string]bool
-	detectOptions DetectOptions
-
-	// cache
-	installedUnits map[string]bool
-	// true if the list of running processes has already been retrieved, we can
-	// avoid getting it a second time.
-	processesSearched map[string]bool
-	// cache
-	runningProcesses map[string]bool
-}
-
-// ExprServiceState keeps a local state during the detection of a single service. It is reset before each service rules' evaluation.
-type ExprServiceState struct {
-	detectedUnits []string
-}
-
-// ExprOS contains the detected (or forced) OS fields available to the rule engine.
-type ExprOS struct {
-	Family     string
-	ID         string
-	RawVersion string
 }
 
 // This is not required with Masterminds/semver
@@ -378,128 +343,6 @@ func normalizeVersion(version string) string {
 	return string(ret)
 }
 */
-
-// VersionCheck returns true if the version of the OS matches the given constraint
-func (os ExprOS) VersionCheck(constraint string) (bool, error) {
-	v, err := semver.NewVersion(os.RawVersion)
-	if err != nil {
-		return false, err
-	}
-
-	c, err := semver.NewConstraint(constraint)
-	if err != nil {
-		return false, err
-	}
-
-	return c.Check(v), nil
-}
-
-// VersionAtLeast returns true if the version of the OS is at least the given version.
-func (os ExprOS) VersionAtLeast(constraint string) (bool, error) {
-	return os.VersionCheck(">=" + constraint)
-}
-
-// VersionIsLower returns true if the version of the OS is lower than the given version.
-func (os ExprOS) VersionIsLower(version string) (bool, error) {
-	result, err := os.VersionAtLeast(version)
-	if err != nil {
-		return false, err
-	}
-
-	return !result, nil
-}
-
-// ExprEnvironment is used to expose functions and values to the rule engine.
-// It can cache the results of service detection commands, like systemctl etc.
-type ExprEnvironment struct {
-	OS ExprOS
-
-	_serviceState *ExprServiceState
-	_state        *ExprState
-}
-
-// NewExprEnvironment creates an environment object for the rule engine.
-func NewExprEnvironment(opts DetectOptions, os ExprOS) ExprEnvironment {
-	return ExprEnvironment{
-		_state: &ExprState{
-			detectOptions: opts,
-
-			unitsSearched:  make(map[string]bool),
-			installedUnits: make(map[string]bool),
-
-			processesSearched: make(map[string]bool),
-			runningProcesses:  make(map[string]bool),
-		},
-		_serviceState: &ExprServiceState{},
-		OS:            os,
-	}
-}
-
-// PathExists returns true if the given path exists.
-func (e ExprEnvironment) PathExists(path string) bool {
-	_, err := os.Stat(path)
-
-	return err == nil
-}
-
-// UnitFound returns true if the unit is listed in the systemctl output.
-// Whether a disabled or failed unit is considered found or not, depends on the
-// systemctl parameters used.
-func (e ExprEnvironment) UnitFound(unitName string) (bool, error) {
-	// fill initial caches
-	if len(e._state.unitsSearched) == 0 {
-		if !e._state.detectOptions.SnubSystemd {
-			units, err := systemdUnitList()
-			if err != nil {
-				return false, err
-			}
-
-			for _, name := range units {
-				e._state.installedUnits[name] = true
-			}
-		}
-
-		for _, name := range e._state.detectOptions.ForcedUnits {
-			e._state.installedUnits[name] = true
-		}
-	}
-
-	e._state.unitsSearched[unitName] = true
-	if e._state.installedUnits[unitName] {
-		e._serviceState.detectedUnits = append(e._serviceState.detectedUnits, unitName)
-
-		return true, nil
-	}
-
-	return false, nil
-}
-
-// ProcessRunning returns true if there is a running process with the given name.
-func (e ExprEnvironment) ProcessRunning(processName string) (bool, error) {
-	if len(e._state.processesSearched) == 0 {
-		procs, err := process.Processes()
-		if err != nil {
-			return false, fmt.Errorf("while looking up running processes: %w", err)
-		}
-
-		for _, p := range procs {
-			name, err := p.Name()
-			if err != nil {
-				return false, fmt.Errorf("while looking up running processes: %w", err)
-			}
-
-			e._state.runningProcesses[name] = true
-		}
-
-		for _, name := range e._state.detectOptions.ForcedProcesses {
-			e._state.runningProcesses[name] = true
-		}
-	}
-
-	e._state.processesSearched[processName] = true
-
-	return e._state.runningProcesses[processName], nil
-}
 
 // applyRules checks if the 'when' expressions are true and returns a ServiceRules struct,
 // augmented with default values and anything that might be useful later on
@@ -544,8 +387,8 @@ func applyRules(svc ServiceRules, env ExprEnvironment) (ServiceRules, bool, erro
 
 // filterWithRules decorates a DetectConfig map by filtering according to the when: clauses,
 // and applying default values or whatever useful to the Service items.
-func filterWithRules(detector *Detector, env ExprEnvironment) (map[string]ServiceRules, error) {
-	ret := make(map[string]ServiceRules)
+func buildPlans(detector *Detector, env ExprEnvironment) (map[string]ServicePlan, error) {
+	ret := make(map[string]ServicePlan)
 
 	for name := range detector.Detect {
 		//
@@ -570,64 +413,13 @@ func filterWithRules(detector *Detector, env ExprEnvironment) (map[string]Servic
 
 		log.Tracef("  Detected %s", name)
 
-		ret[name] = svc
+		ret[name] = ServicePlan{
+			Name:                  name,
+			ServiceRecommendation: svc.ServiceRecommendation,
+		}
 	}
 
 	return ret, nil
-}
-
-// return units that have been forced but not searched yet.
-func (e ExprEnvironment) unsearchedUnits() []string {
-	ret := []string{}
-
-	for _, unit := range e._state.detectOptions.ForcedUnits {
-		if !e._state.unitsSearched[unit] {
-			ret = append(ret, unit)
-		}
-	}
-
-	return ret
-}
-
-// return processes that have been forced but not searched yet.
-func (e ExprEnvironment) unsearchedProcesses() []string {
-	ret := []string{}
-
-	for _, proc := range e._state.detectOptions.ForcedProcesses {
-		if !e._state.processesSearched[proc] {
-			ret = append(ret, proc)
-		}
-	}
-
-	return ret
-}
-
-// checkConsumedForcedItems checks if all the "forced" options (units or processes) have been evaluated during the service detection.
-func checkConsumedForcedItems(e ExprEnvironment) error {
-	unconsumed := e.unsearchedUnits()
-
-	unitMsg := ""
-	if len(unconsumed) > 0 {
-		unitMsg = fmt.Sprintf("unit(s) required but not supported: %v", unconsumed)
-	}
-
-	unconsumed = e.unsearchedProcesses()
-
-	procsMsg := ""
-	if len(unconsumed) > 0 {
-		procsMsg = fmt.Sprintf("process(es) required but not supported: %v", unconsumed)
-	}
-
-	join := ""
-	if unitMsg != "" && procsMsg != "" {
-		join = "; "
-	}
-
-	if unitMsg != "" || procsMsg != "" {
-		return fmt.Errorf("%s%s%s", unitMsg, join, procsMsg)
-	}
-
-	return nil
 }
 
 // DetectOptions contains parameters for the Detect function.
@@ -640,4 +432,3 @@ type DetectOptions struct {
 	SkipServices    []string
 	SnubSystemd     bool
 }
-
