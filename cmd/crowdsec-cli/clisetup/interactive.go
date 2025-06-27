301975201aa74dcec06a1fb8a7561160f4b3dc9b
marco <marco@crowdsec.net>
2025-06-27 13:50:45 +0200

    examples

--- cmd/crowdsec-cli/clisetup/interactive.go
+++ cmd/crowdsec-cli/clisetup/interactive.go
@@ -20,9 +20,21 @@ func (cli *cliSetup) newInteractiveCmd() *cobra.Command {
 		Use:               "interactive",
 		Short:             "Interactive setup",
 		Long:              "Detect services and generate configuration, with user prompts",
+		Example: `# Detect running services, install the appropriate collections and acquisition configuration.
+# Prompt the user for confirmation at each step.
+cscli setup interactive
+
+# Write the acquisition configuration to a specific directory.
+cscli setup interactive --acquis-dir /path/to/acquis.d
+
+# Use a different detection configuration file.
+cscli setup interactive --detect-config /path/to/detact.yaml
+
+# Force the OS to be detected as 'ubuntu 25.04'
+cscli setup interactive --force-os-family linux --force-os-id ubuntu --force-os-version 25.04
+`,
 		DisableAutoGenTag: true,
 		Args:              args.NoArgs,
-		// XXX: TODO: examples!
 		RunE: func(cmd *cobra.Command, _ []string) error {
 			detector, _, err := df.detector()
 			if err != nil {
