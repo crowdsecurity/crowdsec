33eb5df5b8b2e6b0ac11cacca7f28fbc20d8c3d9
Thibault Koechlin <orixxx@gmail.com>
2025-08-08 09:17:50 +0200

    when we fail to invoke nuclei, just fatal

--- pkg/hubtest/nucleirunner.go
+++ pkg/hubtest/nucleirunner.go
@@ -51,10 +51,10 @@ func (nc *NucleiConfig) RunNucleiTemplate(testName string, templatePath string, 
 	}
 
 	if err != nil {
-		log.Warningf("Error running nuclei: %s", err)
 		log.Warningf("Stdout saved to %s", outputPrefix+"_stdout.txt")
 		log.Warningf("Stderr saved to %s", outputPrefix+"_stderr.txt")
 		log.Warningf("Nuclei generated output saved to %s", outputPrefix+".json")
+		log.Fatalf("Error running nuclei: %s", err)
 		return err
 	} else if out.String() == "" {
 		log.Warningf("Stdout saved to %s", outputPrefix+"_stdout.txt")
