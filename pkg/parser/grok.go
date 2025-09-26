5476977ee14cc698467d6f09dc25942519684478
mmetc <92726601+mmetc@users.noreply.github.com>
2025-09-26 12:10:25 +0200

    refact pkg/parser: extract RuntimeGrokPattern (#3916)

--- pkg/parser/grok.go
+++ pkg/parser/grok.go
@@ -3,6 +3,7 @@ package parser
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/expr-lang/expr"
@@ -27,13 +28,13 @@ type Static struct {
 	// the source is a static value
 	Value string `yaml:"value,omitempty"`
 	// or the result of an Expression
-	ExpValue     string      `yaml:"expression,omitempty"`
+	ExpValue string `yaml:"expression,omitempty"`
 	// or an enrichment method
 	Method string `yaml:"method,omitempty"`
 }
 
 type RuntimeStatic struct {
-	Config *Static
+	Config       *Static
 	RunTimeValue *vm.Program
 }
 
@@ -84,14 +85,77 @@ type GrokPattern struct {
 	RegexpName string `yaml:"name,omitempty"`
 	// a proper grok pattern
 	RegexpValue string `yaml:"pattern,omitempty"`
-	// the runtime form of regexpname / regexpvalue
-	RunTimeRegexp grokky.Pattern `yaml:"-"` // the actual regexp
 	// the output of the expression is going to be the source for regexp
-	ExpValue     string      `yaml:"expression,omitempty"`
-	RunTimeValue *vm.Program `yaml:"-"` // the actual compiled filter
+	ExpValue string `yaml:"expression,omitempty"`
 	// a grok can contain statics that apply if pattern is successful
 	Statics []Static `yaml:"statics,omitempty"`
-	RuntimeStatics []RuntimeStatic `yaml:"-"`
+}
+
+type RuntimeGrokPattern struct {
+	Config *GrokPattern
+
+	RunTimeRegexp  grokky.Pattern // the actual regexp
+	RunTimeValue   *vm.Program    // the actual compiled filter
+	RuntimeStatics []RuntimeStatic
+}
+
+func (g *GrokPattern) Compile(pctx *UnixParserCtx, logger *log.Entry) (*RuntimeGrokPattern, error) {
+	var err error
+
+	rg := &RuntimeGrokPattern{}
+	/* load grok by name or compile in-place */
+	if g.RegexpName != "" {
+		logger.Tracef("+ Regexp Compilation %q", g.RegexpName)
+
+		rg.RunTimeRegexp, err = pctx.Grok.Get(g.RegexpName)
+		if err != nil {
+			return nil, fmt.Errorf("unable to find grok %q: %v", g.RegexpName, err)
+		}
+
+		if rg.RunTimeRegexp == nil {
+			return nil, fmt.Errorf("empty grok %q", g.RegexpName)
+		}
+
+		logger.Tracef("%s regexp: %s", g.RegexpName, rg.RunTimeRegexp.String())
+	} else if g.RegexpValue != "" {
+		if strings.HasSuffix(g.RegexpValue, "\n") {
+			logger.Debugf("Beware, pattern ends with \\n: %q", g.RegexpValue)
+		}
+
+		rg.RunTimeRegexp, err = pctx.Grok.Compile(g.RegexpValue)
+		if err != nil {
+			return nil, fmt.Errorf("failed to compile grok %q: %v", g.RegexpValue, err)
+		}
+
+		if rg.RunTimeRegexp == nil {
+			// We shouldn't be here because compilation succeeded, so regexp shouldn't be nil
+			return nil, fmt.Errorf("grok compilation failure: %s", g.RegexpValue)
+		}
+
+		logger.Tracef("%s regexp: %s", g.RegexpValue, rg.RunTimeRegexp.String())
+	}
+
+	// if grok source is an expression
+	if g.ExpValue != "" {
+		rg.RunTimeValue, err = expr.Compile(g.ExpValue,
+			exprhelpers.GetExprOptions(map[string]any{"evt": &types.Event{}})...)
+		if err != nil {
+			return nil, fmt.Errorf("while compiling grok's expression: %w", err)
+		}
+	}
+
+	/* load grok statics */
+	// compile expr statics if present
+	for _, static := range g.Statics {
+		compiled, err := static.Compile()
+		if err != nil {
+			return nil, err
+		}
+
+		rg.RuntimeStatics = append(rg.RuntimeStatics, *compiled)
+	}
+
+	return rg, nil
 }
 
 type DataCapture struct {
