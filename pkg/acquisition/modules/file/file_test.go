e61818e764bb3dfe6ad0ef59ca73a135770a2eb5
marco <marco@crowdsec.net>
2025-10-06 17:03:27 +0200

    refact file acquisition tests: prevent data race

--- pkg/acquisition/modules/file/file_test.go
+++ pkg/acquisition/modules/file/file_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sync"
 	"testing"
 	"time"
 
@@ -405,22 +406,21 @@ force_inotify: true`, testPattern),
 			}
 
 			actualLines := 0
+			var wg sync.WaitGroup
 
 			if tc.expectedLines != 0 {
-				var stopReading bool
-				defer func() { stopReading = true }()
+				wg.Add(1)
 
 				go func() {
+					defer wg.Done()
 					for {
 						select {
 						case <-out:
 							actualLines++
-						default:
-							if stopReading {
-								return
-							}
-							// Small sleep to prevent tight loop
-							time.Sleep(100 * time.Millisecond)
+						case <-tomb.Dying():
+							return
+						case <-time.After(100 * time.Millisecond):
+							// avoid tight loop
 						}
 					}
 				}()
@@ -466,6 +466,11 @@ force_inotify: true`, testPattern),
 				time.Sleep(2 * time.Second)
 
 				os.Remove(streamLogFile)
+
+				// stop acquisition and wait for tailer
+				tomb.Kill(nil)
+				wg.Wait()
+
 				assert.Equal(t, tc.expectedLines, actualLines)
 			}
 
@@ -481,8 +486,6 @@ force_inotify: true`, testPattern),
 			if tc.teardown != nil {
 				tc.teardown()
 			}
-
-			tomb.Kill(nil)
 		})
 	}
 }
