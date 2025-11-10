9f63c1c0e69aaf0eed85f9412684193ea8d87d27
mmetc <92726601+mmetc@users.noreply.github.com>
2025-11-11 00:02:45 +0100

    refact pkg/acquisition: split loki.go (#4034)

--- pkg/acquisition/modules/loki/entry.go
+++ pkg/acquisition/modules/loki/entry.go
@@ -14,8 +14,8 @@ type Entry struct {
 
 func (e *Entry) UnmarshalJSON(b []byte) error {
 	var values []string
-	err := json.Unmarshal(b, &values)
-	if err != nil {
+
+	if err := json.Unmarshal(b, &values); err != nil {
 		return err
 	}
 
@@ -27,8 +27,10 @@ func (e *Entry) UnmarshalJSON(b []byte) error {
 	if err != nil {
 		return err
 	}
+
 	e.Timestamp = time.Unix(int64(t), 0)
 	e.Line = values[1]
+
 	return nil
 }
 
@@ -57,7 +59,7 @@ type LokiQuery struct {
 type Data struct {
 	ResultType string         `json:"resultType"`
 	Result     []StreamResult `json:"result"` // Warning, just stream value is handled
-	Stats      interface{}    `json:"stats"`  // Stats is boring, just ignore it
+	Stats      any            `json:"stats"`  // Stats is boring, just ignore it
 }
 
 type StreamResult struct {
