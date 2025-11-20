dab30974a54a170e054a36a435efa6a6c16d23b9
deivid.garcia.garcia <deivid.garcia.garcia@gmail.com>
2025-11-20 19:11:43 +0100

    Fix channel wrapping issue

--- pkg/acquisition/modules/file/tailwrapper/nxadm_tail.go
+++ pkg/acquisition/modules/file/tailwrapper/nxadm_tail.go
@@ -1,12 +1,16 @@
 package tailwrapper
 
 import (
+	"sync"
+
 	"github.com/nxadm/tail"
 )
 
 // nxadmTailAdapter wraps the original tail.Tail to implement our Tailer interface
 type nxadmTailAdapter struct {
-	tail *tail.Tail
+	tail      *tail.Tail
+	linesChan <-chan *Line
+	once      sync.Once
 }
 
 // Filename returns the filename being tailed
@@ -15,23 +19,27 @@ func (a *nxadmTailAdapter) Filename() string {
 }
 
 // Lines returns a channel of lines read from the file
+// This method is safe to call multiple times - it returns the same channel
 func (a *nxadmTailAdapter) Lines() <-chan *Line {
-	// Convert tail.Line to our Line type
-	ch := make(chan *Line)
-	go func() {
-		defer close(ch)
-		for line := range a.tail.Lines {
-			if line == nil {
-				continue
+	a.once.Do(func() {
+		// Convert tail.Line to our Line type
+		ch := make(chan *Line)
+		go func() {
+			defer close(ch)
+			for line := range a.tail.Lines {
+				if line == nil {
+					continue
+				}
+				ch <- &Line{
+					Text: line.Text,
+					Time: line.Time,
+					Err:  line.Err,
+				}
 			}
-			ch <- &Line{
-				Text: line.Text,
-				Time: line.Time,
-				Err:  line.Err,
-			}
-		}
-	}()
-	return ch
+		}()
+		a.linesChan = ch
+	})
+	return a.linesChan
 }
 
 // Dying returns a channel that will be closed when the tailer is dying
