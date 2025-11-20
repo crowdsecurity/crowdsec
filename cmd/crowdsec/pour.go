23b76848d66c6691cbc74f6cd57527196153acd6
marco <marco@crowdsec.net>
2025-11-20 12:48:41 +0100

    refact pour.go

--- cmd/crowdsec/pour.go
+++ cmd/crowdsec/pour.go
@@ -13,25 +13,27 @@ import (
 	"github.com/crowdsecurity/crowdsec/pkg/pipeline"
 )
 
-func maybeGC(parsed pipeline.Event, buckets *leaky.Buckets, cConfig *csconfig.Config) error {
-	log.Infof("%d existing buckets", leaky.LeakyRoutineCount)
-	// when in forensics mode, garbage collect buckets
-	if cConfig.Crowdsec.BucketsGCEnabled {
-		if parsed.MarshaledTime != "" {
-			z := &time.Time{}
-			if err := z.UnmarshalText([]byte(parsed.MarshaledTime)); err != nil {
-				log.Warningf("Failed to parse time from event '%s': %s", parsed.MarshaledTime, err)
-			} else {
-				log.Warning("Starting buckets garbage collection ...")
+func shouldTriggerGC(count int) bool {
+	return count % 5000 == 0
+}
 
-				if err = leaky.GarbageCollectBuckets(*z, buckets); err != nil {
-					return err
-				}
-			}
-		}
+func triggerGC(parsed pipeline.Event, buckets *leaky.Buckets, cConfig *csconfig.Config) error {
+	log.Infof("%d existing buckets", leaky.LeakyRoutineCount)
+
+	// when in forensics mode, garbage collect buckets
+	if !cConfig.Crowdsec.BucketsGCEnabled || parsed.MarshaledTime == "" {
+		return nil
 	}
 
-	return nil
+	z := &time.Time{}
+	if err := z.UnmarshalText([]byte(parsed.MarshaledTime)); err != nil {
+		log.Warningf("Failed to parse time from event '%s': %s", parsed.MarshaledTime, err)
+		return nil
+	}
+
+	log.Warning("Starting buckets garbage collection...")
+
+	return leaky.GarbageCollectBuckets(*z, buckets)
 }
 
 func runPour(input chan pipeline.Event, holders []leaky.BucketFactory, buckets *leaky.Buckets, cConfig *csconfig.Config) error {
@@ -47,8 +49,8 @@ func runPour(input chan pipeline.Event, holders []leaky.BucketFactory, buckets *
 			startTime := time.Now()
 
 			count++
-			if count%5000 == 0 {
-				if err := maybeGC(parsed, buckets, cConfig); err != nil {
+			if shouldTriggerGC(count) {
+				if err := triggerGC(parsed, buckets, cConfig); err != nil {
 					return fmt.Errorf("failed to start bucket GC: %w", err)
 				}
 			}
