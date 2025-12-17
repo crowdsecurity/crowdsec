2302ec5fc0f7bb9c0a03ebd4acb590d4fb4f8299
mmetc <92726601+mmetc@users.noreply.github.com>
2025-12-17 17:21:06 +0100

    fix typos in function name, error messages, comments (#4154)

--- pkg/metrics/acquisition_cloudwatch.go
+++ pkg/metrics/acquisition_cloudwatch.go
@@ -21,7 +21,7 @@ const CloudWatchDatasourceLinesReadMetricName = "cs_cloudwatch_stream_hits_total
 var CloudWatchDatasourceLinesRead = prometheus.NewCounterVec(
 	prometheus.CounterOpts{
 		Name: CloudWatchDatasourceLinesReadMetricName,
-		Help: "Number of event read from stream.",
+		Help: "Number of events read from stream.",
 	},
 	[]string{"group", "stream", "datasource_type", "acquis_type"},
 )
