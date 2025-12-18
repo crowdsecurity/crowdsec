1da2ca2de35f87f9d4b54bfd2765239776cc8b7a
marco <marco@crowdsec.net>
2025-12-18 15:58:11 +0100

    pour.go: happy path

--- cmd/crowdsec/pour.go
+++ cmd/crowdsec/pour.go
@@ -13,21 +13,26 @@ import (
 	"github.com/crowdsecurity/crowdsec/pkg/pipeline"
 )
 
-func maybeGC(parsed pipeline.Event, buckets *leaky.Buckets, cConfig *csconfig.Config) {
+func shouldTriggerGC(count int) bool {
+	return count % 5000 == 0
+}
+
+func triggerGC(parsed pipeline.Event, buckets *leaky.Buckets, cConfig *csconfig.Config) {
 	log.Infof("%d existing buckets", leaky.LeakyRoutineCount)
 	// when in forensics mode, garbage collect buckets
-	if cConfig.Crowdsec.BucketsGCEnabled {
-		if parsed.MarshaledTime != "" {
-			z := &time.Time{}
-			if err := z.UnmarshalText([]byte(parsed.MarshaledTime)); err != nil {
-				log.Warningf("Failed to parse time from event '%s': %s", parsed.MarshaledTime, err)
-			} else {
-				log.Warning("Starting buckets garbage collection ...")
-
-				leaky.GarbageCollectBuckets(*z, buckets)
-			}
-		}
+	if !cConfig.Crowdsec.BucketsGCEnabled || parsed.MarshaledTime == "" {
+		return
 	}
+
+	z := &time.Time{}
+	if err := z.UnmarshalText([]byte(parsed.MarshaledTime)); err != nil {
+		log.Warningf("Failed to parse time from event '%s': %s", parsed.MarshaledTime, err)
+		return
+	}
+
+	log.Warning("Starting buckets garbage collection ...")
+
+	leaky.GarbageCollectBuckets(*z, buckets)
 }
 
 func runPour(ctx context.Context, input chan pipeline.Event, holders []leaky.BucketFactory, buckets *leaky.Buckets, cConfig *csconfig.Config) {
@@ -43,8 +48,8 @@ func runPour(ctx context.Context, input chan pipeline.Event, holders []leaky.Buc
 			startTime := time.Now()
 
 			count++
-			if count%5000 == 0 {
-				maybeGC(parsed, buckets, cConfig)
+			if shouldTriggerGC(count) {
+				triggerGC(parsed, buckets, cConfig)
 			}
 			// here we can bucketify with parsed
 			poured, err := leaky.PourItemToHolders(ctx, parsed, holders, buckets)
