07c95f559c2f22d3386fa405af6ab0b43104f451
mmetc <92726601+mmetc@users.noreply.github.com>
2026-02-03 12:15:33 +0100

    pkg/leakybucket: refact test loop, more explicit failures in testFile() (#4281)
    * bucketstore_test.go: replace bool return with explicit failure
    
    * pkg/leakybucket: refact test loop

--- pkg/leakybucket/bucketstore_test.go
+++ pkg/leakybucket/bucketstore_test.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path/filepath"
 	"reflect"
+	"slices"
 	"testing"
 	"time"
 
@@ -125,12 +126,36 @@ func testOneBucket(t *testing.T, hub *cwhub.Hub, dir string) {
 	holders, response, err := LoadBuckets(cscfg, hub, scenarios, bucketStore, false)
 	require.NoError(t, err)
 
-	if !testFile(t, filepath.Join(dir, "test.json"), holders, response, bucketStore) {
-		t.Fatalf("tests from %s failed", dir)
-	}
+	testFile(t, filepath.Join(dir, "test.json"), holders, response, bucketStore)
 }
 
-func testFile(t *testing.T, file string, holders []BucketFactory, response chan pipeline.Event, bucketStore *BucketStore) bool {
+func matchOverflow(got, expected pipeline.RuntimeAlert) bool {
+	// both empty
+	if got.Alert == nil && expected.Alert == nil {
+		return true
+	}
+
+	// one empty, one not
+	if got.Alert == nil || expected.Alert == nil {
+		return false
+	}
+
+	if *got.Alert.Scenario != *expected.Alert.Scenario {
+		return false
+	}
+
+	if *got.Alert.EventsCount != *expected.Alert.EventsCount {
+		return false
+	}
+
+	if !reflect.DeepEqual(got.Sources, expected.Sources) {
+		return false
+	}
+
+	return true
+}
+
+func testFile(t *testing.T, file string, holders []BucketFactory, response chan pipeline.Event, bucketStore *BucketStore) {
 	var results []pipeline.Event
 
 	yamlFile, err := os.Open(file)
@@ -211,75 +236,39 @@ POLL_AGAIN:
 	*/
 	for {
 		if len(tf.Results) == 0 && len(results) == 0 {
-			log.Warning("Test is successful")
-			return true
+			return
 		}
-		log.Warningf("%d results to check against %d expected results", len(results), len(tf.Results))
-		if len(tf.Results) != len(results) {
-			log.Errorf("results / expected count doesn't match results = %d / expected = %d", len(results), len(tf.Results))
-			return false
-		}
-	checkresultsloop:
-		for eidx, out := range results {
-			for ridx, expected := range tf.Results {
-				log.Tracef("Checking next expected result.")
 
-				// empty overflow
-				if out.Overflow.Alert == nil && expected.Overflow.Alert == nil {
-					// match stuff
-				} else {
-					if out.Overflow.Alert == nil || expected.Overflow.Alert == nil {
-						log.Info("Here ?")
-						continue
-					}
+		require.Len(t, results, len(tf.Results))
 
-					// Scenario
-					if *out.Overflow.Alert.Scenario != *expected.Overflow.Alert.Scenario {
-						log.Errorf("(scenario) %v != %v", *out.Overflow.Alert.Scenario, *expected.Overflow.Alert.Scenario)
-						continue
-					}
-					log.Infof("(scenario) %v == %v", *out.Overflow.Alert.Scenario, *expected.Overflow.Alert.Scenario)
+		matched := false
 
-					// EventsCount
-					if *out.Overflow.Alert.EventsCount != *expected.Overflow.Alert.EventsCount {
-						log.Errorf("(EventsCount) %d != %d", *out.Overflow.Alert.EventsCount, *expected.Overflow.Alert.EventsCount)
-						continue
-					}
-					log.Infof("(EventsCount) %d == %d", *out.Overflow.Alert.EventsCount, *expected.Overflow.Alert.EventsCount)
+		for i := range results {
+			out := results[i]
 
-					// Sources
-					if !reflect.DeepEqual(out.Overflow.Sources, expected.Overflow.Sources) {
-						log.Errorf("(Sources %s != %s)", spew.Sdump(out.Overflow.Sources), spew.Sdump(expected.Overflow.Sources))
-						continue
-					}
-					log.Infof("(Sources: %s == %s)", spew.Sdump(out.Overflow.Sources), spew.Sdump(expected.Overflow.Sources))
+			matchIdx := -1
+
+			for j := range tf.Results {
+				if matchOverflow(out.Overflow, tf.Results[j].Overflow) {
+					matchIdx = j
+					break
 				}
-				// Events
-				// if !reflect.DeepEqual(out.Overflow.Alert.Events, expected.Overflow.Alert.Events) {
-				// 	log.Errorf("(Events %s != %s)", spew.Sdump(out.Overflow.Alert.Events), spew.Sdump(expected.Overflow.Alert.Events))
-				// 	valid = false
-				// 	continue
-				// } else {
-				// 	log.Infof("(Events: %s == %s)", spew.Sdump(out.Overflow.Alert.Events), spew.Sdump(expected.Overflow.Alert.Events))
-				// }
-
-				// CheckFailed:
-
-				log.Warningf("The test is valid, remove entry %d from expects, and %d from t.Results", eidx, ridx)
-				// don't do this at home : delete current element from list and redo
-				results[eidx] = results[len(results)-1]
-				results = results[:len(results)-1]
-				tf.Results[ridx] = tf.Results[len(tf.Results)-1]
-				tf.Results = tf.Results[:len(tf.Results)-1]
-				goto checkresultsloop
 			}
+
+			if matchIdx < 0 {
+				continue
+			}
+
+			results = slices.Delete(results, i, i+1)
+			tf.Results = slices.Delete(tf.Results, matchIdx, matchIdx+1)
+
+			matched = true
+			break // return to outer loop because the slices have changed
 		}
-		if len(results) != 0 && len(tf.Results) != 0 {
-			log.Errorf("mismatching entries left")
-			log.Errorf("we got: %s", spew.Sdump(results))
-			log.Errorf("we expected: %s", spew.Sdump(tf.Results))
-			return false
+
+		if !matched {
+			require.Failf(t, "no matching pairs remain",
+				"leftover results: %s\nleftover expected: %s", spew.Sdump(results), spew.Sdump(tf.Results))
 		}
-		log.Warning("entry valid at end of loop")
 	}
 }
