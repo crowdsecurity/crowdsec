caf88733c7424156e0ae4aa9bb70ae2effcc04f0
mmetc <92726601+mmetc@users.noreply.github.com>
2026-02-20 16:19:40 +0100

    cmd/notification-*: don't provide the same context twice for request (#4316)

--- cmd/notification-http/main.go
+++ cmd/notification-http/main.go
@@ -144,7 +144,7 @@ func (s *HTTPPlugin) Notify(ctx context.Context, notification *protobufs.Notific
 
 	logger.Debug(fmt.Sprintf("making HTTP %s call to %s with body %s", cfg.Method, cfg.URL, text))
 
-	resp, err := cfg.Client.Do(request.WithContext(ctx))
+	resp, err := cfg.Client.Do(request)
 	if err != nil {
 		logger.Error(fmt.Sprintf("Failed to make HTTP request : %s", err))
 		return nil, err
