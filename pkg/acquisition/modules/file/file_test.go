8f4737200f828d1567ecd86d1f45903b97dbc698
mmetc <92726601+mmetc@users.noreply.github.com>
2026-02-24 14:40:29 +0100

    acquisition/file: minimal fix for data race in tests (#4326)

--- pkg/acquisition/modules/file/file_test.go
+++ pkg/acquisition/modules/file/file_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"sync/atomic"
 	"testing"
 	"time"
 
@@ -355,19 +356,19 @@ force_inotify: true`, testPattern),
 				tc.afterConfigure()
 			}
 
-			actualLines := 0
+			var actualLines atomic.Int32
 
 			if tc.expectedLines != 0 {
-				var stopReading bool
-				defer func() { stopReading = true }()
+				var stopReading atomic.Bool
+				defer func() { stopReading.Store(true) }()
 
 				go func() {
 					for {
 						select {
 						case <-out:
-							actualLines++
+							actualLines.Add(1)
 						default:
-							if stopReading {
+							if stopReading.Load() {
 								return
 							}
 							// Small sleep to prevent tight loop
@@ -417,7 +418,7 @@ force_inotify: true`, testPattern),
 				time.Sleep(2 * time.Second)
 
 				os.Remove(streamLogFile)
-				assert.Equal(t, tc.expectedLines, actualLines)
+				assert.Equal(t, tc.expectedLines, int(actualLines.Load()))
 			}
 
 			if tc.expectedOutput != "" {
