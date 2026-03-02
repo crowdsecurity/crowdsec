e22ff8df98dd3bcd31279051892292c9cc091994
marco <marco@crowdsec.net>
2026-03-02 14:15:08 +0100

    lint

--- pkg/parser/node_test.go
+++ pkg/parser/node_test.go
@@ -12,36 +12,36 @@ func TestParserConfigs(t *testing.T) {
 		t.Fatalf("unable to load patterns : %s", err)
 	}
 
-	/*the actual tests*/
+	// the actual tests
 	var CfgTests = []struct {
 		cfg      NodeConfig
 		compiles bool
 		valid    bool
 	}{
-		//valid node with grok pattern
+		// valid node with grok pattern
 		{NodeConfig{Debug: true, Stage: "s00", Grok: GrokPattern{RegexpValue: "^x%{DATA:extr}$", TargetField: "t"}}, true, true},
-		//bad filter
+		// bad filter
 		{NodeConfig{Debug: true, Stage: "s00", Filter: "ratata"}, false, false},
-		//empty node
+		// empty node
 		{NodeConfig{Debug: true, Stage: "s00", Filter: "true"}, false, false},
-		//bad subgrok
+		// bad subgrok
 		{NodeConfig{Debug: true, Stage: "s00", SubGroks: yaml.MapSlice{{Key: string("FOOBAR"), Value: string("[a-$")}}}, false, true},
-		//valid node with grok pattern
+		// valid node with grok pattern
 		{NodeConfig{Debug: true, Stage: "s00", SubGroks: yaml.MapSlice{{Key: string("FOOBAR"), Value: string("[a-z]")}}, Grok: GrokPattern{RegexpValue: "^x%{FOOBAR:extr}$", TargetField: "t"}}, true, true},
-		//bad node success
+		// bad node success
 		{NodeConfig{Debug: true, Stage: "s00", OnSuccess: "ratat", Grok: GrokPattern{RegexpValue: "^x%{DATA:extr}$", TargetField: "t"}}, false, false},
-		//ok node success
+		// ok node success
 		{NodeConfig{Debug: true, Stage: "s00", OnSuccess: "continue", Grok: GrokPattern{RegexpValue: "^x%{DATA:extr}$", TargetField: "t"}}, true, true},
-		//valid node with grok sub-pattern used by name
+		// valid node with grok sub-pattern used by name
 		{NodeConfig{Debug: true, Stage: "s00", SubGroks: yaml.MapSlice{{Key: string("FOOBARx"), Value: string("[a-z] %{DATA:lol}$")}}, Grok: GrokPattern{RegexpName: "FOOBARx", TargetField: "t"}}, true, true},
-		//node with unexisting grok pattern
+		// node with unexisting grok pattern
 		{NodeConfig{Debug: true, Stage: "s00", Grok: GrokPattern{RegexpName: "RATATA", TargetField: "t"}}, false, true},
-		//node with grok pattern dependencies
+		// node with grok pattern dependencies
 		{NodeConfig{Debug: true, Stage: "s00", SubGroks: yaml.MapSlice{
 			{Key: string("SUBGROK"), Value: string("[a-z]")},
 			{Key: string("MYGROK"), Value: string("[a-z]%{SUBGROK}")},
 		}, Grok: GrokPattern{RegexpValue: "^x%{MYGROK:extr}$", TargetField: "t"}}, true, true},
-		//node with broken grok pattern dependencies
+		// node with broken grok pattern dependencies
 		{NodeConfig{Debug: true, Stage: "s00", SubGroks: yaml.MapSlice{
 			{Key: string("SUBGROKBIS"), Value: string("[a-z]%{MYGROKBIS}")},
 			{Key: string("MYGROKBIS"), Value: string("[a-z]")},
