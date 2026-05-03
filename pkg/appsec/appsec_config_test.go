7a9123bdb4069713a60dac662ee1a4a6439b950c
Sebastien Blot <sebastien@crowdsec.net>
2026-05-03 21:48:40 +0200

    add test for escaping

--- pkg/appsec/appsec_config_test.go
+++ pkg/appsec/appsec_config_test.go
@@ -326,6 +326,27 @@ func TestBuildNilPhaseConfig(t *testing.T) {
 	assert.Empty(t, rt.OutOfBandHooks.OnMatch)
 }
 
+func TestLoadAPISchemaRejectsPathTraversal(t *testing.T) {
+	cases := []string{
+		"../escape.yaml",
+		"../../etc/passwd",
+		"/etc/passwd",
+	}
+
+	for _, filename := range cases {
+		t.Run(filename, func(t *testing.T) {
+			rt := &AppsecRuntimeConfig{
+				Logger:  log.NewEntry(log.StandardLogger()),
+				DataDir: t.TempDir(),
+			}
+
+			err := rt.LoadAPISchemaWithName("ref", filename)
+			require.Error(t, err)
+			require.Contains(t, err.Error(), "must be relative to schemas")
+		})
+	}
+}
+
 func TestBuildOnLoadStaysOutOfPhaseHooks(t *testing.T) {
 	cfg := AppsecConfig{
 		Logger:             log.NewEntry(log.StandardLogger()),
