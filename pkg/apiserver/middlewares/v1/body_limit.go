54a0dfe6c16b7687b0da6634e0b19ef0f5d9bb30
blotus <sebastien@crowdsec.net>
2026-05-07 15:51:13 +0200

    Merge commit from fork
    * lapi: enforce a maximum body size
    
    * add tests

---
+++ pkg/apiserver/middlewares/v1/body_limit.go
@@ -0,0 +1,23 @@
+package v1
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Maximum body size for LAPI queries
+// Applies to the decompressed body if it's gzipped
+const (
+	UnauthenticatedBodyLimit int64 = 2 * 1024 * 1024  // 2 MiB
+	AuthenticatedBodyLimit   int64 = 50 * 1024 * 1024 // 50 MiB
+)
+
+func BodyLimit(max int64) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Body != nil {
+			c.Request.Body = http.MaxBytesReader(c.Writer, c.Request.Body, max)
+		}
+		c.Next()
+	}
+}
