e2ba6e0bb12cc9a8a72ef3119ae9cac657b48d7e
Sebastien Blot <sebastien@crowdsec.net>
2026-07-23 17:26:12 +0200

    apiclient: return nil response if we couldnt connect to LAPI

--- pkg/apiclient/client_http_test.go
+++ pkg/apiclient/client_http_test.go
@@ -11,6 +11,8 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/crowdsecurity/go-cs-lib/cstest"
+
+	"github.com/crowdsecurity/crowdsec/pkg/fflag"
 )
 
 func TestNewRequestInvalid(t *testing.T) {
@@ -44,6 +46,33 @@ func TestNewRequestInvalid(t *testing.T) {
 
 	_, _, err = client.Alerts.List(ctx, AlertsListOpts{})
 	cstest.RequireErrorContains(t, err, "building request: BaseURL must have a trailing slash, but ")
+}
+
+// A transport error (here: nothing listening) must not yield a non-nil *Response
+// wrapping a nil http.Response, or callers checking "resp != nil" would panic.
+func TestNewRequestUnreachable(t *testing.T) {
+	ctx := t.Context()
+
+	// otherwise the client retries the failing dial for a few minutes
+	require.NoError(t, fflag.DisableHttpRetryBackoff.Set(true))
+	t.Cleanup(func() { _ = fflag.DisableHttpRetryBackoff.Set(false) })
+
+	_, urlx, teardown := setup()
+	teardown()
+
+	apiURL, err := url.Parse(urlx + "/")
+	require.NoError(t, err)
+
+	client := NewClient(&Config{
+		MachineID:     "test_login",
+		Password:      "test_password",
+		URL:           apiURL,
+		VersionPrefix: "v1",
+	})
+
+	_, resp, err := client.UsageMetrics.Add(ctx, nil)
+	require.Error(t, err)
+	assert.Nil(t, resp)
 }
 
 func TestNewRequestTimeout(t *testing.T) {
